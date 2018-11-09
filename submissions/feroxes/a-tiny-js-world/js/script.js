834b2814dcfbfe09459d23d26768ab91fb34c003
feroxes <feroxes@gmail.com>
2018-11-09 16:33:20 +0200

    add files

---
+++ submissions/feroxes/a-tiny-js-world/js/script.js
@@ -0,0 +1,39 @@
+class Creature {
+    constructor(whoIs, gender, name, say, legs){
+        this.whoIs = whoIs;
+        this.gender = gender;
+        this.name = name;
+        this.say = say;
+        this.legs = legs;
+    }
+    toSay(){
+            console.log(this.say);
+        }
+    print(){
+        let info = [];
+        
+        for(let key in this){
+           info.push(this[key]);
+        }
+        console.log(info);
+    }
+}
+class Person extends Creature{
+     constructor(whoIs, gender, name, say, legs, hands, friends){
+        super(whoIs, gender, name, say, legs)
+        this.hands = hands;
+        this.friends = friends;
+     }
+}
+
+
+
+let nick = new Person('human', 'male', 'Nick','Hellooooo...',2 ,2, ['Kate', 'Jane']);
+
+nick.print();
+nick.toSay();
+
+let bars = new Creature('cat', 'female', 'Bars', 'Meuuuu', 4, 0);
+
+bars.print();
+bars.toSay();
