eb8d9ee2b693dfe088ec6ea7ff4ede65d2ba308c
IrynaY <yrchuk.iryna@gmail.com>
2018-11-19 19:01:34 +0200

    fixes

--- submissions/IrynaY/js-dom/index.js
+++ submissions/IrynaY/js-dom/index.js
@@ -1,23 +1,26 @@
 window.onload = function() {
-  const menuList = document.getElementById("menu");
   const pageContent = document.getElementById("page_content")
-  const menuContainer = document.getElementById("menu_container")
   const menuButton = document.getElementById("menu_btn")
+  const menuContainer = document.getElementById("menu_container")
+  const ul = document.createElement("ul")
 
-  pageContent.innerHTML = `<img src="${content.cat.img}" class="pic">` + content.cat.content
-
-  Object.keys(content).forEach(el => {
+  ul.id = "menu"
+  сharacters.forEach(el => {
     const li = document.createElement("li")
-    li.innerHTML = `${content[el].title}`
-    li.addEventListener("click", function () {
-      pageContent.innerHTML = `<img src="${content[el].img}" class="pic">` + content[el].content
-    })
-    menuList.appendChild(li)
+    li.innerHTML = `${el.id}`
+    ul.appendChild(li)
+  })
+  menuContainer.appendChild(ul)
+
+  ul.addEventListener("click", (evt) => {
+    if (evt.target.tagName != "LI") return
+    let element = сharacters.filter(el => {return el.id === evt.target.textContent})
+    pageContent.innerHTML = `<img src="${element[0].img}" class="pic">` + element[0].content
   })
 
-  menuButton.addEventListener("click", function () {
-    menuContainer.style.display !== "flex" ?  
-      menuContainer.style.display = "flex" : menuContainer.style.display = "none"    
+  menuButton.addEventListener("click", () => {
+    ul.className === "visible" ? ul.className = "disable" : ul.className = "visible"
   })
 
+  pageContent.innerHTML = `<img src="${сharacters[0].img}" class="pic">` + сharacters[0].content
 }
