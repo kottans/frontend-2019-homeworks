170c65947e6d7fecf860b398b8a607ce9df37d33
Oodmincheg <Vlad Vorobyov>
2018-11-23 15:51:57 +0200

    fix ternary operation, use numbers

--- submissions/Oodmincheg/js-tiny-world/index.js
+++ submissions/Oodmincheg/js-tiny-world/index.js
@@ -7,26 +7,49 @@
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
-const Inhabition = function(specie, name, gender, legs, hands, saying){
-    this.specie = specie;
-    this.name = name;
-    this.gender = gender;
-    this.legs = legs;
-    this.hands = hands;
-    this.saying = saying
-    this.hands == 0 ? this.handsString = "no" : this.handsString = this.hands
-    this.hiString = `"${this.saying}"\n Hi. I'm ${this.name}. I'm a ${this.specie}. I have ${this.handsString} hands. I have ${this.legs} legs. And I'm a ${this.gender}\n `
-    
-}
+const Inhabition = function(specie, name, gender, legs, hands, saying) {
+  this.specie = specie;
+  this.name = name;
+  this.gender = gender;
+  this.legs = legs;
+  this.saying = saying;
+  hands === 0 ? (this.handsString = "no") : (this.handsString = this.hands);
+  this.hiString = `"${this.saying}"\n Hi. I'm ${this.name}. I'm a ${
+    this.specie
+  }. I have ${this.handsString} hands. I have ${this.legs} legs. And I'm a ${
+    this.gender
+  }\n `;
+};
 
-const dog = new Inhabition('dog', 'Mr. Pickles', 'male', 4, 0, "MRRRR. PIIICLEEESSSS GOOOOD BOOOOOYYYY")
-const cat = new Inhabition('cat', 'Felix', 'male', '4', '0', 'Felix the cat. The wonderful, wonderful cat!')
-const man = new Inhabition('human', 'Luntik', 'male', '2', '2', 'Ya rodilsya')
-const woman = new Inhabition('human', 'Eva', 'female', '2', '2', 'Ya sozdana iz rebra')
+const dog = new Inhabition(
+  "dog",
+  "Mr. Pickles",
+  "male",
+  4,
+  0,
+  "MRRRR. PIIICLEEESSSS GOOOOD BOOOOOYYYY"
+);
+const cat = new Inhabition(
+  "cat",
+  "Felix",
+  "male",
+  "4",
+  "0",
+  "Felix the cat. The wonderful, wonderful cat!"
+);
+const man = new Inhabition("human", "Luntik", "male", 2, 2, "Ya rodilsya");
+const woman = new Inhabition(
+  "human",
+  "Eva",
+  "female",
+  2,
+  2,
+  "Ya sozdana iz rebra"
+);
 print(cat.hiString);
 print(dog.hiString);
 print(man.hiString);
-print(woman.hiString)
+print(woman.hiString);
 
 // ======== OUTPUT ========
 /* Use print(message) for output.
@@ -46,5 +69,3 @@ print(woman.hiString)
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
-
-
