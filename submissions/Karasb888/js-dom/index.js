da72faa4430e8a73da669158ac5bc134ea6e4a85
karasb888 <Ivan.mazurenko99@gmail.com>
2018-11-26 21:43:44 +0200

    Code changing after review

--- submissions/Karasb888/js-dom/index.js
+++ submissions/Karasb888/js-dom/index.js
@@ -1,80 +1,93 @@
 document.addEventListener("DOMContentLoaded", function(){
-  var menu = document.getElementById('menu');
-  var menuItems = document.querySelectorAll('.menu__item');
-  var contentItems = document.querySelectorAll('.content__item');
-  var activeMenu = menuItems[0];
-  var activeContent = contentItems[0];
+  const menu = document.getElementById('menu');
+  const content = document.getElementById('content');
+  var menuItems = [];
+  var contentItems = [];
 
-  var activeMobile = 0;
+//наполняю страницу
+  for(var index in data){
+    //меню
+    let itemM = document.createElement('div');
+    itemM.classList.add('menu__item');
+    itemM.innerHTML=data[index]["menu"];
+    menu.appendChild(itemM);
+    menuItems.push(itemM);
+    //контент
+    let itemC = document.createElement('div');
+    itemC.classList.add("content__item", data[index]['contentClass']);
+    let childC = document.createElement('div');
+    childC.classList.add("content__" + data[index]['contentClass']);
+    childC.innerHTML=data[index]["text"];
+    itemC.appendChild(childC);
+    content.appendChild(itemC);
+    contentItems.push(itemC);
+  }
+
+var activeMenu = menuItems[0];
+var activeContent = contentItems[0];
+
+for(var i = 1; i < contentItems.length; i++){
+  contentItems[i].classList.add('hidden');
+}
+
+//номер активного меню (и контента)
+  var mobileActiveItem = 0;
+  //логика меню на мобильных девайсах
   if(document.documentElement.clientWidth <= 768){
 
-      var leftArrow = document.createElement('div');
+      let leftArrow = document.createElement('div');
       menu.appendChild(leftArrow);
       leftArrow.className = 'leftArrow';
-      leftArrow.style.display='none';
+      leftArrow.classList.add('hidden');
 
-      var rightArrow = document.createElement('div');
+      let rightArrow = document.createElement('div');
       menu.appendChild(rightArrow);
       rightArrow.className = 'rightArrow';
 
       for(var i = 1; i < menuItems.length; i++){
-        menuItems[i].style.display='none';
+        menuItems[i].classList.add('hidden');
       };
 
       leftArrow.addEventListener('click', function(){
-        console.log('left  ' + activeMobile);
-        if(activeMobile > 0){
-          menuItems[activeMobile].style.display='none';
-          menuItems[activeMobile - 1].style.display='block';
-          contentItems[activeMobile].style.display='none';
-          contentItems[activeMobile - 1].style.display='block';
+
+        if(mobileActiveItem > 0){
+          menuItems[mobileActiveItem].classList.add('hidden');
+          menuItems[mobileActiveItem - 1].classList.remove('hidden');
+          contentItems[mobileActiveItem].classList.add('hidden');
+          contentItems[mobileActiveItem - 1].classList.remove('hidden');
         }
 
-        activeMobile--;
+        mobileActiveItem--;
+        //на первом слайде убираю стрелку влево, на последнем стрелку вправо
+        mobileActiveItem === 0 ? leftArrow.classList.add('hidden') : leftArrow.classList.remove('hidden');
+        mobileActiveItem  === menuItems.length - 1 ? rightArrow.classList.add('hidden') : rightArrow.classList.remove('hidden');
 
-        if(activeMobile  === 0){
-          leftArrow.style.display='none';
-        }else{
-          leftArrow.style.display='block';
-        }
-        if(activeMobile  === menuItems.length - 1){
-          rightArrow.style.display='none';
-      }else{
-        rightArrow.style.display='block';
-      }
 
     });
 
     rightArrow.addEventListener('click', function(){
-      console.log('right   ' + activeMobile);
-      if(activeMobile < menuItems.length - 1){
-        menuItems[activeMobile].style.display='none';
-        menuItems[activeMobile + 1].style.display='block';
-        contentItems[activeMobile].style.display='none';
-        contentItems[activeMobile + 1].style.display='block';
+  
+      if(mobileActiveItem < menuItems.length - 1){
+        menuItems[mobileActiveItem].classList.add('hidden');
+        menuItems[mobileActiveItem + 1].classList.remove('hidden');
+        contentItems[mobileActiveItem].classList.add('hidden');
+        contentItems[mobileActiveItem + 1].classList.remove('hidden');
       }
 
-      activeMobile++;
+      mobileActiveItem++;
+      //на первом слайде убираю стрелку влево, на последнем стрелку вправо
+      mobileActiveItem  === menuItems.length - 1 ? rightArrow.classList.add('hidden') : rightArrow.classList.remove('hidden') ;
+      mobileActiveItem  === 0 ? leftArrow.classList.add('hidden') : leftArrow.classList.remove('hidden') ;
 
-      if(activeMobile  === menuItems.length - 1){
-        rightArrow.style.display='none';
-      }else{
-        rightArrow.style.display='block';
-      }
-      if(activeMobile  === 0){
-        leftArrow.style.display='none';
-      }else{
-        leftArrow.style.display='block';
-      }
     });
-
+//меню на десктопе
   }else{
 
   function menuClick(menu, content) {
     menu.classList.add('active');
     activeMenu.classList.remove('active');
-    activeContent.style.display="none";
-    content.style.display='block';
+    activeContent.classList.add('hidden');
+    content.classList.remove('hidden');
     activeMenu = menu;
     activeContent = content;
   };
