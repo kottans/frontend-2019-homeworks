8116b088b64c993b0dd0900b82dd6a046ad06bb1
Oodmincheg <Vlad Vorobyov>
2018-11-26 20:52:12 +0200

    delete comments

--- submissions/Oodmincheg/js-tiny-world/index.js
+++ submissions/Oodmincheg/js-tiny-world/index.js
@@ -50,22 +50,3 @@ print(cat.hiString);
 print(dog.hiString);
 print(man.hiString);
 print(woman.hiString);
-
-// ======== OUTPUT ========
-/* Use print(message) for output.
-     Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-  
-     Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-     However, please, REFRAIN from improving visuals at least until your code is reviewed
-     so code reviewers might focus on a single file that is index.js.
-     */
-
-/* Print examples:
-     print('ABC');
-     print('<strong>ABC</strong>');
-     print('<strong>ABC</strong>', 'div');
-  
-     print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-     print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-     print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-     */
