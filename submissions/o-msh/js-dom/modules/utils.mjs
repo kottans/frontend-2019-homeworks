492c645f9b5fa8c3bdfa103622a9bac8d12a6588
Oleh <shtepa.oleh.m@gmail.com>
2018-11-27 11:21:53 +0200

    Split functions of getting data from API

--- submissions/o-msh/js-dom/modules/utils.mjs
+++ submissions/o-msh/js-dom/modules/utils.mjs
@@ -1,6 +1,10 @@
 import data from './data.mjs';
 
-const generateMenu = (data, contentElement) => {
+const navigationElement = document.querySelector(".navigation");
+const mobileIconElement = document.querySelector(".mobile_icon");
+const contentElement = document.querySelector(".content");
+
+const generateMenu = data => {
     let defaultFlag = false,
         menu = document.createElement("ul");
     data.forEach(el => {
@@ -23,7 +27,7 @@ const generateMenu = (data, contentElement) => {
     return menu;
 }
 
-const menuHandlerClick = (e) => {
+const menuHandlerClick = e => {
     e.preventDefault();
     let current = e.target;
     document.querySelector(".menu_item.active").classList.remove("active");
@@ -31,103 +35,111 @@ const menuHandlerClick = (e) => {
     if (current.tagName !== "A") {
         current = getParentNode(current, "A");
     }
-    document.querySelector(".content").innerHTML = getContent(current.getAttribute("href"));
+    contentElement.innerHTML = getContent(current.getAttribute("href"));
     window.location.hash = current.getAttribute("href");
     let btnGetData = document.querySelector(".btn_get_data");
-        btnGetData ? btnGetData.addEventListener("click", e => btnHandlerClick(e)) : null;
-    document.querySelector(".navigation").classList.toggle("active");
-    document.querySelector(".mobile_icon").classList.toggle("change");
+    btnGetData ? btnGetData.addEventListener("click", btnHandlerClick) : null;
+    navigationElement.classList.toggle("active");
+    mobileIconElement.classList.toggle("change");
 }
 
 const btnHandlerClick = e => {
     e.preventDefault();
     let type = e.target.dataset.type;
-    document.querySelector(".loading_div").classList.add("show");
-    let userDiv = document.querySelector(".user_div");
-    let currencyDiv = document.querySelector(".currency_div");
-    userDiv ? userDiv.innerHTML = "" : currencyDiv ? currencyDiv.innerHTML = "" : false;
+    clearContainers();
     switch (type) {
         case "random_user":
-            fetch("https://randomuser.me/api/?results=5").then(response => {
-                if (response.ok) {
-                    return response.json();
-                }
-                throw new Error("Network response was not ok.");
-            }).then(user => {
-                if (window.location.hash === "#random_user") {
-                    let div = document.querySelector("div.user_div");
-                    if (!div) {
-                        div = document.createElement("div");
-                    } else {
-                        div.innerHTML = "";
-                    }
-                    div.classList.add("user_div");
-                    user.results.forEach(el => {
-                        let userCard = document.createElement("div");
-                        userCard.classList.add("user_card");
-                        let img = document.createElement("img");
-                        img.src = el.picture.large;
-                        userCard.appendChild(img);
-                        let userInfo = document.createElement("div");
-                        userInfo.classList.add("user_info");
-                        userInfo.insertAdjacentHTML("beforeend", `<ul>
-                                <li>${el.name.first.charAt(0).toUpperCase() + el.name.first.slice(1)} ${el.name.last.charAt(0).toUpperCase() + el.name.last.slice(1)}, ${el.dob.age}</li>
-                                <li>${el.email}</li>
-                                <li>${el.cell}</li>
-                            </ul>`);
-                        userCard.appendChild(userInfo);
-                        div.appendChild(userCard);
-                    });
-                    document.querySelector(".loading_div").classList.remove("show");
-                    document.querySelector(".content").appendChild(div);
-                }
-            }).catch(e => {
-                document.querySelector(".loading_div").classList.remove("show");
-                console.log('There has been a problem with your fetch operation: ' + e.message);
-            });
+            fetchData("https://randomuser.me/api/?results=5")
+                .then(data => makeRandomUserContent(data));
             break;
         case "currency":
-            fetch("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json").then(response => {
-                if (response.ok) {
-                    return response.json();
-                }
-                throw new Error("Network response was not ok.");
-            }).then(currency => {
-                if (window.location.hash === "#currency") {
-                    let div = document.querySelector("div.currency_div");
-                    if (!div) {
-                        div = document.createElement("div");
-                    } else {
-                        div.innerHTML = "";
-                    }
-                    div.classList.add("currency_div");
-                    currency.forEach(el => {
-                        let currencyCard = document.createElement("div");
-                        currencyCard.classList.add("currency_card");
-                        currencyCard.insertAdjacentHTML("beforeend", `<ul>
-                                <li>${el.txt}</li>
-                                <li>${el.cc}</li>
-                                <li>${el.rate}</li>
-                            </ul>`);
-                        div.appendChild(currencyCard);
-                    });
-                    document.querySelector(".loading_div").classList.remove("show");
-                    document.querySelector(".content").appendChild(div);
-                }
-            }).catch(e => {
-                document.querySelector(".loading_div").classList.remove("show");
-                console.log('There has been a problem with your fetch operation: ' + e.message);
-            });
+            fetchData("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json")
+                .then(data => makeCurrencyContent(data));
             break;
         default:
             break;
     }
 }
 
-const getContent = link => {
-    let dataElement = data.filter(el => el.link === link);
-    return dataElement[0].content;
+const clearContainers = () => {
+    let userDiv = document.querySelector(".user_div"),
+        currencyDiv = document.querySelector(".currency_div");
+    userDiv ? userDiv.innerHTML = "" : currencyDiv ? currencyDiv.innerHTML = "" : false;
 }
+
+const fetchData = url => {
+    let loading_div = document.querySelector(".loading_div");
+    loading_div.classList.add("show");
+    return fetch(url).then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error("Network response was not ok.");
+    }).then(data => {
+        loading_div.classList.remove("show");
+        return data;
+    })
+    .catch(e => {
+        loading_div.classList.remove("show");
+        console.log('There has been a problem with your fetch operation: ' + e.message);
+    });
+}
+
+const makeRandomUserContent = users => {
+    if (window.location.hash === "#random_user" && users) {
+        let div = document.querySelector("div.user_div");
+        if (!div) {
+            div = document.createElement("div");
+        } else {
+            div.innerHTML = "";
+        }
+        div.classList.add("user_div");
+        users.results.forEach(el => {
+            let userCard = document.createElement("div");
+            userCard.classList.add("user_card");
+            let img = document.createElement("img");
+            img.src = el.picture.large;
+            userCard.appendChild(img);
+            let userInfo = document.createElement("div");
+            userInfo.classList.add("user_info");
+            userInfo.insertAdjacentHTML("beforeend", `<ul>
+                    <li>${el.name.first.charAt(0).toUpperCase() + el.name.first.slice(1)} ${el.name.last.charAt(0).toUpperCase() + el.name.last.slice(1)}, ${el.dob.age}</li>
+                    <li>${el.email}</li>
+                    <li>${el.cell}</li>
+                </ul>`);
+            userCard.appendChild(userInfo);
+            div.appendChild(userCard);
+        });
+        document.querySelector(".loading_div").classList.remove("show");
+        contentElement.appendChild(div);
+    }
+}
+
+const makeCurrencyContent = currencies => {
+    if (window.location.hash === "#currency" && currencies) {
+        let div = document.querySelector("div.currency_div");
+        if (!div) {
+            div = document.createElement("div");
+        } else {
+            div.innerHTML = "";
+        }
+        div.classList.add("currency_div");
+        currencies.forEach(el => {
+            let currencyCard = document.createElement("div");
+            currencyCard.classList.add("currency_card");
+            currencyCard.insertAdjacentHTML("beforeend", `<ul>
+                    <li>${el.txt}</li>
+                    <li>${el.cc}</li>
+                    <li>${el.rate}</li>
+                </ul>`);
+            div.appendChild(currencyCard);
+        });
+        document.querySelector(".loading_div").classList.remove("show");
+        contentElement.appendChild(div);
+    }
+}
+
+const getContent = link => { return data.filter(el => el.link === link)[0].content }
 
 const getParentNode = (node, parentTag) => {
     if (node.parentNode.tagName === parentTag) {
@@ -138,9 +150,6 @@ const getParentNode = (node, parentTag) => {
 }
 
 const init = () => {
-    const navigationElement = document.querySelector(".navigation");
-    const mobileIconElement = document.querySelector(".mobile_icon");
-    const contentElement = document.querySelector(".content");
     const isValidData = data.every(menuItem => Object.values(menuItem).every(value => value));
     if (isValidData) {
         navigationElement.appendChild(generateMenu(data, contentElement));
