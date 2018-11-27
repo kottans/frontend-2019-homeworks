759366a7725b63c343d7a632d3749842bf11cdd9
Maks_Ovsianiuk <maksovsanyuk@gmail.com>
2018-11-27 11:00:45 +0200

    mend

--- submissions/maxovsanyuk/js-dom/js/data.js
+++ submissions/maxovsanyuk/js-dom/js/data.js
@@ -1,86 +1,110 @@
 let newItems = ['General Basics','Front-End Basics', 'Advanced Topics',];
 let myNode = document.getElementById("content");
 
-let clickDel = function (){
+const cleanChildren = () => {
     while (myNode.firstChild) {
         myNode.removeChild(myNode.firstChild);
     }
    } 
 
+const contentTitle = () =>{
+    document.createElement("h1")
+    myNode.insertAdjacentHTML('afterbegin','<h1>Kottans Front-End Course 2019</h1>');
+    document.querySelector("h1").className = "tittle"
+}
+
 newItems.forEach(function(item){
     let topNav = document.createElement('div');
     topNav.innerHTML = "<span>" + item + "</span>";
-    menu.appendChild(topNav);
-    topNav.className = "new-item"
-
-    topNav.addEventListener("mouseover",function(){
-        topNav.classList.add('items-mouseover');
-    });
-    topNav.addEventListener("mouseout",function(){
-        topNav.classList.remove('items-mouseover');
-    });
+    menu.appendChild(topNav);   
 });
 
-let titleKottans = document.createElement("h1")
-document.getElementById("content").insertAdjacentHTML('beforeend','<h1>Kottans Front-End Course 2019</h1>');
+menu.children[0].className = "generalBasics";
+menu.children[1].className = "frontendBasics";       
+menu.children[2].className = "advancedTopics"; 
 
-let generalBasics = document.querySelectorAll("nav >div.new-item")[0];
+
+let titleKottans = document.createElement("h1")
+myNode.insertAdjacentHTML('beforeend','<h1>Kottans Front-End Course 2019</h1>');
+
+let generalBasics = document.querySelector(".generalBasics");
 generalBasics.addEventListener("click",function(){
        
-    if(document.body.classList.contains("children0") === false){
-        document.body.classList.add("children0");
-        document.body.classList.remove("children1","children2");
+    if(document.body.classList.contains("children-first") === false){
+        document.body.classList.add("children-first");
+        document.body.classList.remove("children-second","children-third");
         
-        clickDel();
+        cleanChildren();
         
         let generalContent = document.createElement("div");
         generalContent.className = "main-content";
-        generalContent.innerHTML = "<h1>General Basics</h1><ol><li><a>Git Basics</a></li><li><a>Linux CLI and Networking</a></li><li><a>VCS (hello gitty)GitHub and Collaboration</a></li></ol>"
+        generalContent.innerHTML = `
+        <h1>General Basics</h1>
+        <ol>
+           <li>Git Basics</li>
+           <li>Linux CLI and Networking</li>
+           <li>VCS (hello gitty)GitHub and Collaboration</li>
+        </ol>`
         content.appendChild(generalContent);
-        let titleKottans = document.createElement("h1")
-        myNode.insertAdjacentHTML('afterbegin','<h1>Kottans Front-End Course 2019</h1>');
-        document.querySelector("h1").className = "tittle"
+        
+        contentTitle();
     }
 });
     
-let frontendBasics = document.querySelectorAll("nav >div.new-item")[1];
+let frontendBasics = document.querySelector(".frontendBasics");
 
 frontendBasics.addEventListener("click",function(){
 
-    if(document.body.classList.contains("children1") === false){
-        document.body.classList.add("children1");
-        document.body.classList.remove("children0","children2");
+    if(document.body.classList.contains("children-second") === false){
+        document.body.classList.add("children-second");
+        document.body.classList.remove("children-first","children-third");
 
-        clickDel();
+        cleanChildren();
         
         let frontendContent = document.createElement("div");
         frontendContent.className = "main-content"
-        frontendContent.innerHTML = "<h1>Front-End Basics</h1><ol><li><a>Intro to HTML & CSS</a></li><li><a>Responsive Web Design</a></li><li><a>JavaScript Basics</a></li><li><a>Document Object Model - practice></a></li></ol>"
+        frontendContent.innerHTML = `
+        <h1>Front-End Basics</h1>
+        <ol>
+            <li>Intro to HTML & CSS</li>
+            <li>Responsive Web Design</li>
+            <li>JavaScript Basics></li>
+            <li>Document Object Model - practice</li>
+        </ol>`
         content.appendChild(frontendContent);
-    
-        let titleKottans = document.createElement("h1")
-        myNode.insertAdjacentHTML('afterbegin','<h1>Kottans Front-End Course 2019</h1>');
-        document.querySelector("h1").className = "tittle"
+        
+        contentTitle();
     }
 });
 
-let advancedTopics = document.querySelectorAll("nav >div.new-item")[2];
+let advancedTopics = document.querySelector(".advancedTopics");
 advancedTopics.addEventListener("click",function(){
        
-    if(document.body.classList.contains("children2") === false){
-        document.body.classList.add("children2");
-        document.body.classList.remove("children0","children1");
+    if(document.body.classList.contains("children-third") === false){
+        document.body.classList.add("children-third");
+        document.body.classList.remove("children-first","children-second");
 
-        clickDel();
+        cleanChildren();
         
         let advancedContent = document.createElement("div");
         advancedContent.className = "main-content"
-        advancedContent.innerHTML = "<h1>Advanced Topics</h1><ol><li><a>Building a Tiny JS World (pre-OOP) - practice</a></li><li><a>Object oriented JS - practice</a></li><li><a>OOP exercise - practice</a></li><li><a>Offline Web Applications</a></li><li><a>Offline Web Applications</a></li><li><a>Offline Web Applications</a></li><li><a>Offline Web Applications</a></li><li><a>Memory pair game — real project!</a></li><li><a>Website Performance Optimization</a></li><li><a>Friends App - real project!</a></li></ol>"
+        advancedContent.innerHTML = `
+        <h1>Advanced Topics</h1>
+        <ol>
+            <li>Building a Tiny JS World (pre-OOP) - practice</li>
+            <li>Object oriented JS - practice</li>
+            <li>OOP exercise - practice</li>
+            <li>Offline Web Applications</li>
+            <li>Offline Web Applications</li>
+            <li>Offline Web Applications</li>
+            <li>Offline Web Applications</li>
+            <li>Memory pair game — real project!</li>
+            <li>Website Performance Optimization</li>
+            <li>Friends App - real project!</li>
+        </ol>`
         content.appendChild(advancedContent);
-
-        let titleKottans = document.createElement("h1")
-        myNode.insertAdjacentHTML('afterbegin','<h1>Kottans Front-End Course 2019</h1>');
-        document.querySelector("h1").className = "tittle"
+        
+        contentTitle();
     }   
 });
     
