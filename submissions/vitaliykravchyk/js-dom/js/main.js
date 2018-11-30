f3f25bb2ebeca6dd5006bc62a226a4e83580a668
vitaliykravchyk <vetal.kravchik@gmail.com>
2018-11-30 20:31:24 +0200

    Fixing comments

--- submissions/vitaliykravchyk/js-dom/js/main.js
+++ submissions/vitaliykravchyk/js-dom/js/main.js
@@ -1,4 +1,4 @@
-var Luntik = {
+let Luntik = {
     data: [
         {name: "Лунтик", image: "img/luntik.jpg", description: "Лунтик — главный персонаж российского мультфильма «Приключения Лунтика и его друзей» (Также фигурирует как главный персонаж в некоторых компьютерных играх). Доподлинно неизвестно, каким существом является Лунтик, однако, он свалился с Луны на Землю, и представился жителям поляны как лунная пчела. Добр, наивен, правдив. Имеет множество друзей. Умеет прощать и дружить. Лучшие друзья — Кузя, Мила и Пчелёнок. Дата рождения Лунтика — 12 апреля 2006."},
         {name: "Кузя", image: "img/kuzia.jpg", description: "Кузя — зелёный кузнечик, который постоянно ищет приключения на свою голову. Кузя стал первым другом Лунтика. Кузнечик Кузя зелёный, но туловище и ладони у него жёлтые. Из одежды у Кузи тёмно-зеленая курточка на пуговицах и жёлтые ботинки. Кузя очень любопытен, деятелен, отличается редкостным жизнелюбием, но в то же время он – хвастун, выдумщик и очень любит лесть. Благодаря непоседливости Кузи, друзья постоянно попадают в передряги, из которых вместе ищут выход, в процессе чего познают окружающий мир."},
@@ -6,17 +6,18 @@ var Luntik = {
         {name: "Пчелёнок", image: "img/bee.jpg", description: "Пчелёнок — друг Лунтика, который ходит в школу пчёл и учится базовым навыкам пчелы. Пчелёнок трудолюбив и любознателен, но при этом, очень стеснителен. Он раньше считал, что ему незачем ходить в школу, ведь он и так всё знает, но после серии «Прогулял» он понял, что знания, полученные в школе, пригодятся. Самый умный среди своих детских персонажей. Иногда рассказывает своим друзьям о том, что он сегодня узнал. Мечта Пчелёнка — стать героем и спасти мир. Во время событий сериала смог перебороть страх темноты."}
     ],
     init: function(){
-        let createItemFunction = this.createItem.bind(this);
-        let removeContentFunction = this.removeContent.bind(this);
-        let removeStylesFunction = this.removeStyles.bind(this);
-        document.querySelector('#friend-list').addEventListener('click', function(){
-            removeContentFunction();
-            removeStylesFunction();
+        let changeItemFunction = this.changeItem.bind(this);
+        document.querySelector('#friend-list').addEventListener('click', changeItemFunction); 
+    },
+    changeItem: function(){
+        if(event.target.hasAttribute('data-item')){
+            this.removeContent();
+            this.removeStyles();
             //ADD CONTENT AND STYLES
             event.target.classList.toggle('person');
             document.querySelector('main').classList.add('mainActive');
-            createItemFunction(event.target.dataset.item);
-        }); 
+            this.createItem(event.target.dataset.item);
+        }
     },
     createItem: function(n){
         let infoBlock = document.createElement("div");
@@ -37,20 +38,18 @@ var Luntik = {
         document.querySelector('main .content').appendChild(infoBlock);
 
     },
-    removeContent: function(){
-        if(event.target.hasAttribute('data-item')){            
-            if(document.querySelector('main .content').childNodes.length > 0){
-                if(document.querySelector('main .content').childNodes[0].classList.contains('infoBlock')){
-                    document.querySelector('main .content').removeChild(document.querySelector('.infoBlock'));
-                };
-            }
+    removeContent: function(){         
+        let content = document.querySelector('main .content');
+        if(content.childNodes.length > 0){
+            if(content.childNodes[0].classList.contains('infoBlock')){
+                content.removeChild(document.querySelector('.infoBlock'));
+            };
         }
     },
     removeStyles: function(){
-        for(let i = 0; i < document.querySelectorAll('#friend-list li').length;i++){
-            if(document.querySelectorAll('#friend-list li')[i].classList.contains('person')){
-                document.querySelectorAll('#friend-list li')[i].classList.remove('person');
-            }
+        let person = document.querySelector('.person');
+        if(person){
+            person.classList.remove('person');
         }
     }
 }
