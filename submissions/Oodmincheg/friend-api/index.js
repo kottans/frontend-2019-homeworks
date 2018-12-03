aed481d4f43d1c0c20e51fe5da8afcf87cddc7f1
Oodmincheg <Vlad Vorobyov>
2018-12-03 10:52:43 +0200

    fix request changes

--- submissions/Oodmincheg/friend-api/index.js
+++ submissions/Oodmincheg/friend-api/index.js
@@ -7,11 +7,14 @@ const createCardOfFreind = friend => {
   image.setAttribute("src", friend.picture.large);
   const caption = document.createElement("figcaption");
   //prettier-ignore
-  caption.textContent = `${upFirstLetter(friend.name.first)} ${upFirstLetter(friend.name.last)}, age ${friend.dob.age}`;
+  let name  = document.createElement("p")
+  name.textContent = `${friend.name.first} ${friend.name.last}`;
+  let age = document.createElement("p");
+  age.textContent = `age ${friend.dob.age}`;
   let email = document.createElement("a");
   let br = document.createElement("br");
   email.textContent = `${friend.email}`;
-  caption.append(br, email);
+  caption.append(name, age, email);
   friendProfile.append(image, caption);
   return friendProfile;
 };
@@ -40,10 +43,6 @@ const asc = (a, b) => {
   }
 };
 
-const upFirstLetter = string => {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-};
-
 const filterByString = (workList, string) => {
   return workList.filter(friend =>
     `${friend.name.first} ${friend.name.last} ${friend.email}`.includes(string)
@@ -54,51 +53,51 @@ const resetSortButtons = () => {
   document
     .querySelectorAll("input[type=radio]")
     .forEach(radio => (radio.checked = false));
-  document.getElementById("bothSexes").checked = true;
+  document.getElementById("both-sexes").checked = true;
 };
 
 const resetSearchInput = () => {
-  document.getElementById("searchByName").value = "";
+  document.getElementById("search-by-name").value = "";
 };
 
 const renderPageWithListeners = originList => {
   let workList = [...originList];
   renderListOfFriends(originList);
-  const buttonSortByNameDesc = document.getElementById("sortByNameDesc");
+  const buttonSortByNameDesc = document.getElementById("sort-by-name-desc");
   buttonSortByNameDesc.addEventListener("change", () =>
     //prettier-ignore
     renderListOfFriends(workList.sort((a, b) => desc(a.name.first, b.name.first)))
   );
-  const buttonSortByNameAsc = document.getElementById("sortByNameAsc");
+  const buttonSortByNameAsc = document.getElementById("sort-by-name-asc");
   buttonSortByNameAsc.addEventListener("change", () =>
     renderListOfFriends(
       workList.sort((a, b) => asc(a.name.first, b.name.first))
     )
   );
-  const buttonSortByAgeDesc = document.getElementById("sortByAgeDesc");
+  const buttonSortByAgeDesc = document.getElementById("sort-by-age-desc");
   buttonSortByAgeDesc.addEventListener("change", () =>
     renderListOfFriends(workList.sort((a, b) => desc(a.dob.age, b.dob.age)))
   );
-  const buttonSortByAgeAsc = document.getElementById("sortByAgeAsc");
+  const buttonSortByAgeAsc = document.getElementById("sort-by-age-asc");
   buttonSortByAgeAsc.addEventListener("change", () =>
     renderListOfFriends(workList.sort((a, b) => asc(a.dob.age, b.dob.age)))
   );
-  const buttonSortByMale = document.getElementById("sortByMale");
+  const buttonSortByMale = document.getElementById("sort-by-male");
   buttonSortByMale.addEventListener("change", () => {
     renderListOfFriends(originList.filter(friend => friend.gender === "male"));
   });
-  const buttonSortByFemale = document.getElementById("sortByFemale");
+  const buttonSortByFemale = document.getElementById("sort-by-female");
   buttonSortByFemale.addEventListener("change", () => {
     renderListOfFriends(
       originList.filter(friend => friend.gender === "female")
     );
   });
-  const buttonBothSexes = document.getElementById("bothSexes");
+  const buttonBothSexes = document.getElementById("both-sexes");
   buttonBothSexes.addEventListener("change", () => {
     renderListOfFriends(originList);
   });
 
-  const inputSearchByName = document.getElementById("searchByName");
+  const inputSearchByName = document.getElementById("search-by-name");
   inputSearchByName.addEventListener("input", event => {
     resetSortButtons();
     workList = filterByString(originList, event.target.value);
