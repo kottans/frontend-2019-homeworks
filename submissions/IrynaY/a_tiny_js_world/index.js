7f986eedd476f90a0fa8f11aefe55c641caf232d
IrynaY <yrchuk.iryna@gmail.com>
2018-12-04 08:07:48 +0200

    refactor

--- submissions/IrynaY/a_tiny_js_world/index.js
+++ submissions/IrynaY/a_tiny_js_world/index.js
@@ -14,38 +14,44 @@ class Inhabitant{
 }
 
 class Human extends Inhabitant{
-    constructor(name, gender, hands = 2, lags = 2, speak, friends){
+    constructor(name, gender, speak, friends, hands = 2, lags = 2){
         super("human", name, gender, hands, lags, speak, friends)
     }
 }
 
 class Dog extends Inhabitant{
-    constructor(name, gender, hands, lags = 4, friends){
+    constructor(name, gender, friends, hands, lags = 4){
         super("dog", name, gender, hands , lags, "Woof-woof!", friends)
     }
 }
 
 class Cat extends Inhabitant{
-    constructor(name, gender, lags = 4, friends, hands){
+    constructor(name, gender, friends, hands, lags = 4){
         super("cat", name, gender, hands, lags, "Meeeeooow!", friends)
     }
 }
 
 class CatWoman extends Cat{
-    constructor(name, gender, hands = 2, lags = 2, friends){
-        super(name, gender, hands, lags, friends)
+    constructor(name, gender, friends, hands = 2, lags = 2){
+        super(name, gender, friends, hands, lags)
         this.species = "Cat-woman"
     }
 }
 
-const man = new Human("Jack", "male", 2, 2,  "AAAAaaaargh!!!", ["Tom", "Bob"])
-const woman = new Human("Lucy", "female", 2, 2,  "Holla!", ["Kittie", "Bob"])
-const cat = new Cat("Fluffie", "female", 4, ["Lucy"])
-const dog = new Dog("Tim", "male", 0, 4, ["Jack"])
-const cat_woman = new CatWoman("Kittie", "female", 2, 2, ["Kittie", "Fluffie"])
+const inhabitant_1 = new Human("Jack", "male", "AAAAaaaargh!!!")
+const inhabitant_2 = new Human("Lucy", "female", "Holla!")
+const inhabitant_3 = new Cat("Fluffie", "female")
+const inhabitant_4 = new Dog("Tim", "male")
+const inhabitant_5 = new CatWoman("Kittie", "female")
 
-print(man.getInfo())
-print(woman.getInfo())
-print(dog.getInfo())
-print(cat.getInfo())
-print(cat_woman.getInfo())
+inhabitant_1.friends = [inhabitant_2.name]
+inhabitant_2.friends = [inhabitant_3.name, inhabitant_5.name]
+inhabitant_3.friends = [inhabitant_2.name, inhabitant_5.name]
+inhabitant_4.friends = [inhabitant_1.name, inhabitant_2.name, inhabitant_3.name, inhabitant_5.name]
+inhabitant_5.friends = [inhabitant_2.name, inhabitant_3.name]
+
+print(inhabitant_1.getInfo())
+print(inhabitant_2.getInfo())
+print(inhabitant_3.getInfo())
+print(inhabitant_4.getInfo())
+print(inhabitant_5.getInfo())
