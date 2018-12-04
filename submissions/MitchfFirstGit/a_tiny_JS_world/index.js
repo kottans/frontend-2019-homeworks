3c07864710758a8cef549db6f5d79917a891ab22
MitchfFirstGit <44033501+MitchfFirstGit@users.noreply.github.com>
2018-12-05 02:24:05 +0300

    Fixed things

--- submissions/MitchfFirstGit/a_tiny_JS_world/index.js
+++ submissions/MitchfFirstGit/a_tiny_JS_world/index.js
@@ -8,35 +8,49 @@
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
 class Inhabitant {
-  constructor(species, name, gender, legs, hands, saying, ...friends ) {
-  this.species = species;
-  this.name = name;
-  this.gender = gender;
-  this.legs = legs;
-  this.hands = hands;
-  this.saying = saying;
-  this.friends =friends;
-  }
-  say(){
-    if(!this.friends.length) this.friends = `I have no friends, I'm lonely in this world`;
-    if(typeof(this.saying)==='object'){
-      print(`species: ${this.species}; name: ${this.name}; gender: ${this.gender}; legs: ${this.legs}; hands: ${this.hands}; saying: ${this.saying.saying}; friends: ${this.friends}`, 'div');
-    }
-    else{ 
-      print(`species: ${this.species}; name: ${this.name}; gender: ${this.gender}; legs: ${this.legs}; hands: ${this.hands}; saying: ${this.saying}; friends: ${this.friends}`, 'div');
-    }
+  constructor(name, gender) {
+    this.name = name;
+    this.gender = gender;
+    this.saying = `Hi, my name is ${this.name}. What about you?`;
   }
 }
-const dog = new Inhabitant('dog', 'Toby', 'male', 4, 0, 'woof-woof', 'man', 'woman');
-const cat = new Inhabitant('cat', 'Milka', 'female', 4, 0, 'meow-meow', 'man', 'woman');
-const woman = new Inhabitant('woman', 'Emma', 'female', 2, 2, 'Hello everyone!', 'man', 'cat', 'dog');
-const man = new Inhabitant('man', 'Alex', 'male',2, 2, 'Hello everyone!', 'dog', 'cat', 'woman');
-const catWoman = new Inhabitant('catWoman', 'Mia', 'female',2, 2, cat);
-dog.say();
-cat.say();
-woman.say();
-man.say();
-catWoman.say();
+class Dog extends Inhabitant {
+  constructor(name, gender) {
+    super(name, gender);
+    this.specie = "Dog"; 
+    this.legs = 4;
+  }
+}
+class Cat extends Inhabitant {
+  constructor(name, gender) {
+    super(name, gender);
+    this.specie = "Cat";
+    this.legs = 4;
+  }
+}
+class Man extends Inhabitant {
+  constructor(name, gender) {
+    super(name, gender);
+    this.specie = "Man";
+    this.hands = 2;
+    this.legs = 2;
+  }
+}
+class Woman extends Inhabitant {
+  constructor(name, gender) {
+    super(name, gender);
+    this.specie = "Woman";
+    this.hands = 2;
+    this.legs = 2;
+  }
+}
+const inhabitantsArray = [ 
+  new Dog('Toby', 'male'),
+  new Cat('Milka', 'female'),
+  new Man('Alex', 'male'),
+  new Woman('Emma','female')
+ ];
+inhabitantsArray.forEach( inhabitant => print(`specie: ${inhabitant.specie}; name: ${inhabitant.name}; gender: ${inhabitant.gender}; hands: ${inhabitant.hands? inhabitant.hands: "Oh my God, I don't feel my hands, but wait a minute, I don't have them and it's okey for me hah"}; legs: ${inhabitant.legs}; saying: ${inhabitant.saying}`, 'div') );
 // ======== OUTPUT ========
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
