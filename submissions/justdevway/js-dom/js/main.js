f8bff001f2e2351722b80bd03dcfd7db567e5fe3
jdw <justdevway@gmail.com>
2018-12-05 23:25:19 +0200

    Restructured code after review

--- submissions/justdevway/js-dom/js/main.js
+++ submissions/justdevway/js-dom/js/main.js
@@ -26,49 +26,15 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
 
 
-
     // TODO: write preloader
 
     function buildContent(data) {
         var data = data.results;
-        var container = document.getElementsByClassName('js-results')[0];
+        var container = document.querySelector('.js-results');
         container.innerHTML = '';
 
-        data.forEach(function (el, index) {
-            var item = document.createElement('article');
-            var content = document.createElement('section');
-            var header = document.createElement('header');
-            var footer = document.createElement('footer');
-            var title = document.createElement('h2');
-            var titleLink = document.createElement('a');
-            var poster = document.createElement('img');
-            var overview = document.createElement('div');
-            var date = document.createElement('time');
-            item.classList.add('result', 'js-result', 'js-result--first', 'isActive');
-            header.classList.add('result__title');
-            footer.classList.add('result__footer');
-            titleLink.classList.add('result__title__link', 'js-result__title__link');
-            titleLink.setAttribute('src', '#');
-            titleLink.append(document.createTextNode(el.title));
-            title.append(titleLink);
-            content.classList.add('result__info', 'cf');
-            poster.classList.add('result__img', 'js-result__img');
-            poster.setAttribute('data-src', 'https://image.tmdb.org/t/p/w500' + el.poster_path);
-            poster.setAttribute('src', './img/1.jpg');
-            poster.setAttribute('alt', el.title);
-            overview.classList.add('result__description');
-            overview.append(document.createTextNode(el.overview));
-            date.classList.add('result__data');
-            date.append(document.createTextNode('Дата релиза: ' + el.release_date));
-            date.dateTime = el.release_date
-            header.append(title);
-            item.append(header);
-            content.append(poster);
-            content.append(overview);
-            footer.append(date);
-            item.append(content);
-            item.append(footer);
-            item.append(document.createElement('hr'));
+        data.forEach(function (el) {
+            let item = makeResultItem(el);
             container.append(item);
         });
         console.log('FINISH buildContent SCRIPT');
@@ -79,29 +45,63 @@ document.addEventListener("DOMContentLoaded", function (event) {
         });
     }
 
+    showModal();
+    initModal();
+    appendContent(links.genre, buildNav);
+    appendContent(links.popular, buildContent);
+
+    function makeResultItem(el) {
+        var item = document.createElement('article');
+        var content = document.createElement('section');
+        var header = document.createElement('header');
+        var footer = document.createElement('footer');
+        var title = document.createElement('h2');
+        var titleLink = document.createElement('a');
+        var poster = document.createElement('img');
+        var overview = document.createElement('div');
+        var date = document.createElement('time');
+        item.classList.add('result', 'js-result', 'js-result--first', 'isActive');
+        header.classList.add('result__title');
+        footer.classList.add('result__footer');
+        titleLink.classList.add('result__title__link', 'js-result__title__link');
+        titleLink.setAttribute('src', '#');
+        titleLink.innerText = el.title;
+        title.append(titleLink);
+        content.classList.add('result__info', 'cf');
+        poster.classList.add('result__img', 'js-result__img');
+        poster.setAttribute('data-src', 'https://image.tmdb.org/t/p/w500' + el.poster_path);
+        poster.setAttribute('src', './img/1.jpg');
+        poster.setAttribute('alt', el.title);
+        overview.classList.add('result__description');
+        overview.append(document.createTextNode(el.overview));
+        date.classList.add('result__data');
+        date.append(document.createTextNode('Дата релиза: ' + el.release_date));
+        date.dateTime = el.release_date;
+        header.append(title);
+        content.append(poster, overview);
+        footer.append(date);
+        item.append(header, content, footer, document.createElement('hr'));
+        return item;
+    }
+
     function buildNav(data) {
         var data = data.genres;
         var menuLimit = 8;
-        var container = document.getElementsByClassName('js-list')[0];
-        var showLess = showLessButton();
+        var container = document.querySelector('.js-list');
+        var showLess = makeShowLessButton();
+        var showMore = makeShowMoreButton();
 
-        data.forEach(function (el, index) {
-            var item = document.createElement('li');
-            var link = document.createElement('a');
-            link.classList.add('item__link', 'js-item__link');
-            link.setAttribute('data-link', index);
-            link.setAttribute('data-menu-id', el.id);
-            link.append(document.createTextNode(el.name));
-            item.append(link);
+
+        data.slice(0, 10).forEach(function (el, index) {
+            let item = makeNavItem(el, index);
             container.append(item);
-            item.classList.add('item');
-            if (index === menuLimit) {
-                var showMore = showMoreButton();
-                container.append(showMore);
-            }
-            if (index > menuLimit) {
-                item.classList.add('isHide');
-            }
+        });
+
+        container.append(showMore);
+
+        data.slice(10).forEach(function (el, index) {
+            let item = makeNavItem(el, index);
+            container.append(item);
         });
 
         container.append(showLess);
@@ -110,7 +110,20 @@ document.addEventListener("DOMContentLoaded", function (event) {
         initNavLinks();
     }
 
-    function  showMoreButton () {
+    function makeNavItem(el, index) {
+        var item = document.createElement('li');
+        var link = document.createElement('a');
+        link.classList.add('item__link', 'js-item__link');
+        link.setAttribute('data-link', index);
+        link.setAttribute('data-menu-id', el.id);
+        link.append(document.createTextNode(el.name));
+        item.append(link);
+        item.classList.add('item');
+        return item;
+    }
+
+
+    function makeShowMoreButton() {
         var showMore = document.createElement('li');
         var tempLink = document.createElement('a');
         showMore.classList.add('item');
@@ -120,9 +133,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
         return showMore;
     }
 
-    function showLessButton () {
+    function makeShowLessButton() {
         var showLess = document.createElement('li');
-        showLess.classList.add('isHide');
         var tempLink = document.createElement('a');
         showLess.classList.add('item');
         tempLink.classList.add('item__link', 'item__link--showLess', 'js-item__link--showLess');
@@ -132,7 +144,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
 
     function changeImgs(cb) {
-        var imgs = Array.prototype.slice.call(document.getElementsByClassName('js-result__img'));
+        var imgs = document.querySelectorAll('.js-result__img');
 
         imgs.forEach(function (el, i) {
             var src = el.getAttribute('data-src');
@@ -143,9 +155,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
 
     function initBurger() {
-        var burger = document.getElementsByClassName('js-toggle__burger')[0];
+        var burger = document.querySelector('.js-toggle__burger');
         burger.addEventListener('click', function (e) {
-            var nav = document.getElementsByClassName('js-nav')[0];
+            var nav = document.querySelector('.js-nav');
             if (this.classList.contains('isActive')) {
                 this.classList.remove('isActive');
                 nav.classList.remove('isActive');
@@ -158,79 +170,67 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     function initNavButton() {
         console.log('START BUTTON SCRIPT');
-        var showMoreButton = document.getElementsByClassName('js-item__link--showMore')[0];
-        var showLessButton = document.getElementsByClassName('js-item__link--showLess')[0];
+        var showMoreButton = document.querySelector('.js-item__link--showMore');
+        var showLessButton = document.querySelector('.js-item__link--showLess');
 
-        if(showMoreButton) {
+        if (showMoreButton) {
             showMoreButton.addEventListener('click', function (e) {
                 e.preventDefault();
-                var items = document.getElementsByClassName('item');
-                Array.prototype.slice.call(items).forEach(function (el, index) {
-                    if (index > 9) {
-                        el.classList.remove('isHide');
-                    }
-                });
+                var list = document.querySelector('.js-list');
+                list.classList.add('showItems');
                 this.parentElement.classList.add('isHide');
             });
         }
 
-        if(showLessButton) {
+        if (showLessButton) {
             showLessButton.addEventListener('click', function (e) {
                 e.preventDefault();
-                var items = document.getElementsByClassName('item');
-                Array.prototype.slice.call(items).forEach(function (el, index) {
-                    if (index === 10) {
-                        el.classList.remove('isHide');
-                    }
-                    if (index > 10) {
-                        el.classList.add('isHide');
-                    }
-                });
+                var list = document.querySelector('.js-list');
+                var showMoreButton = document.querySelector('.js-item__link--showMore');
+                showMoreButton.parentElement.classList.remove('isHide');
+                list.classList.remove('showItems');
             });
         }
     }
 
     function initTitleButton() {
-        var titleLinks = Array.prototype.slice.call(document.getElementsByClassName('js-result__title__link'));
-        var modalWindow = document.getElementsByClassName('js-modal')[0];
-        titleLinks.forEach(function (el, i) {
-            el.addEventListener('click', function (e) {
-                e.preventDefault();
+        var list = document.querySelector('.js-results');
+        list.addEventListener('click', function(e) {
+            e.preventDefault();
+            var modalWindow = document.querySelector('.js-modal');
+            var target = e.target;
+            console.log(target);
+            if(target.classList.contains('js-result__title__link')) {
                 modalWindow.classList.add('isActive');
-                modalWindow.getElementsByClassName('js-modal__text')[0].classList.add('isActive');
-                // modalWindow.getElementsByClassName('js-modal__preloader')[0].classList.remove('isActive');
-            });
+                modalWindow.querySelector('.js-modal__text').classList.add('isActive');
+                return;
+            }
         });
     }
 
     function initModal() {
-        var modalWindow = document.getElementsByClassName('js-modal')[0];
+        var modalWindow = document.querySelector('.js-modal');
         modalWindow.addEventListener('click', function (e) {
             this.classList.remove('isActive');
-            this.getElementsByClassName('js-modal__text')[0].classList.remove('isActive');
+            this.querySelector('.js-modal__text').classList.remove('isActive');
         });
     }
 
     function showModal() {
-        var modalWindow = document.getElementsByClassName('js-modal')[0];
+        var modalWindow = document.querySelector('.js-modal');
         modalWindow.style.display = 'block';
-        modalWindow.getElementsByClassName('js-modal__preloader')[0].style.display = 'block';
+        modalWindow.querySelector('.js-modal__preloader').style.display = 'block';
     }
 
     // TODO: make fadeIn
     function hideModal() {
-        var modalWindow = document.getElementsByClassName('js-modal')[0];
+        var modalWindow = document.querySelector('.js-modal');
         modalWindow.style.display = '';
-        modalWindow.getElementsByClassName('js-modal__preloader')[0].style.display = 'none';
+        modalWindow.querySelector('.js-modal__preloader').style.display = 'none';
     }
 
-    showModal();
-    initModal();
-    appendContent(links.genre, buildNav);
-    appendContent(links.popular, buildContent);
-
     function initNavLinks() {
-        var navLinks = Array.prototype.slice.call(document.getElementsByClassName('js-item__link'));
+        var navLinks = document.querySelectorAll('.js-item__link');
         navLinks.forEach(function (el, i) {
             el.addEventListener('click', function (e) {
                 e.preventDefault();
