5644dc9e9962e3dc169f69544103d3b23675cb47
vitaliykravchyk <vetal.kravchik@gmail.com>
2018-12-05 21:35:58 +0200

    fix names of variables

--- submissions/vitaliykravchyk/a-tiny-js-world-2/index.js
+++ submissions/vitaliykravchyk/a-tiny-js-world-2/index.js
@@ -16,8 +16,8 @@ let Creature = function(species, name, hands, legs, gender, phrase){
     this.phrase = phrase;
     this.friendList = [];
 }
-Creature.prototype.friends = function(...friend){
-    friend.forEach(element => {
+Creature.prototype.addFriends = function(...friends){
+    friends.forEach(element => {
                 this.friendList.push(element.name);
             });
 }
@@ -27,20 +27,20 @@ const dog = new Creature('dog', 'Garold', 0, 4, 'male', 'Woof-woof');
 const cat = new Creature('cat', 'Jinny', 0, 4, 'female', 'Meeeeooow');
 const catWomen = new Creature('catwoman', 'Emma', 2, 2, 'female', cat.phrase);
 
-man.friends(women, dog);
-women.friends(man, cat);
-cat.friends(women, catWomen);
-dog.friends(man);
-catWomen.friends(cat);
-function info(obj){
+man.addFriends(women, dog);
+women.addFriends(man, cat);
+cat.addFriends(women, catWomen);
+dog.addFriends(man);
+catWomen.addFriends(cat);
+function getPrintableInfo(obj){
     return obj.species + ';' + obj.name + ';' + obj.hands + ';' + obj.legs + ';' + obj.gender + ';' + obj.phrase + ';' + obj.friendList;
 }
 // ======== OUTPUT ========
-print(info(man));
-print(info(women));
-print(info(dog));
-print(info(cat));
-print(info(catWomen));
+print(getPrintableInfo(man));
+print(getPrintableInfo(women));
+print(getPrintableInfo(dog));
+print(getPrintableInfo(cat));
+print(getPrintableInfo(catWomen));
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
 
