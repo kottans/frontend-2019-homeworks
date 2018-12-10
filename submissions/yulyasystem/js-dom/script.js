f045c283701359e3f8a7654bd174b767c28f1407
Yulia Bekeniova <yulyasystem@gmail.com>
2018-12-10 18:57:02 +0300

    delete global variables, inconsistent indentations and take event listener outside the loop

--- submissions/yulyasystem/js-dom/script.js
+++ submissions/yulyasystem/js-dom/script.js
@@ -1,62 +1,40 @@
-var selectedLink;
-var data;
-var index = 0;
-fetch("/data.json")
-  .then(function(response) {
+let selectedLink;
+let data;
+let index = 0;
+fetch("./data.json")
+  .then(function (response) {
     return response.json();
   })
-  .then(function(json) {
+  .then(function (json) {
     data = json;
     makeSideBar(data);
     makePageContent(0);
   })
-  .catch(alert); 
-  
+  .catch(alert);
 
-  function makeSideBar(data){
-     var menuItem = document.getElementsByTagName("li");
-     var nav = document.getElementById("sideMenu");
-      for(var i in data){
-        
-        menuItem[i].textContent = data[i].heading;
-        menuItem[i].id = data[i].id
-
-        console.log(i);
-
-        
-        nav.addEventListener("click",function(event){
-          
-          
-          var target = event.target; //catch
-          
-          content = target.getAttribute("data-content");
-          var arr = target.id.split('/');
-          var index = arr[arr.length - 1];
-          console.log(arr);
-          if(content){
-            
-            console.log(index); 
-            makePageContent(index);
-            
-             //target.className = content;
-          }
-        }
-      );
-      }
-      
-
+function makeSideBar(data) {
+  let menuItem = document.getElementsByTagName("li");
+  let nav = document.getElementById("sideMenu");
+  for (let i in data) {
+    menuItem[i].textContent = data[i].heading;
+    menuItem[i].id = data[i].id
   }
-
-  function makePageContent(index){
-    console.log(index);
-    var main = document.querySelector(".main");
-    var div = document.createElement("div");
-    main.innerHTML = "";
-    div.innerHTML = data[index].text;
-    main.appendChild(div);
-
+  nav.addEventListener("click", function (event) {
+    let target = event.target; //catch
+    content = target.getAttribute("data-content");
+    let arr = target.id.split('/');
+    let index = arr[arr.length - 1];
+    if (content) {
+      makePageContent(index);
+    }
   }
+  );
+}
 
-  
-
-  
+function makePageContent(index) {
+  let main = document.querySelector(".main");
+  let div = document.createElement("div");
+  main.innerHTML = "";
+  div.innerHTML = data[index].text;
+  main.appendChild(div);
+}
