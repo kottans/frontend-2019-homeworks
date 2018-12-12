98c0277b94e18817937b0eb9e7764e1c4ca0378b
Денис Лук'яненко <denlyk1@gmail.com>
2018-12-12 23:22:19 +0200

    Clean directory and update for requested changes

--- submissions/denislukianenko/js-post-oop/index.js
+++ submissions/denislukianenko/js-post-oop/index.js
@@ -1,73 +1,93 @@
 // ======== CLASSES ==========
-function Animal(species, name, gender, saying) {
-  this.species = species;
-  this.name = name;
-  this.gender = gender;
-  this.saying = saying;
+function Creature(name, gender, saying, legs) {
+  this.name = name ? name : "Creature";
+  this.gender = gender ? gender : "No gender";
+  this.saying = saying ? saying : "Que-qa";
+  this.legs = legs ? legs : 4;
 }
-Animal.prototype.printInfo = function() {
-  print(
+Creature.prototype.formPrintString = function() {
+  this.printStr =
     '<span style="font-weight: bold; font-size: 23px">' +
-      this.name +
-      "</span>" +
-      '<span style="color: gray"> ' +
-      this.species +
-      "</span><br>" +
-      "  Gender: " +
-      this.gender +
-      "<br>" +
-      "  Says: " +
-      this.saying
-  );
-  if (this.printAdditional) this.printAdditional();
-};
-
-function HumanBeing(species, name, gender, saying, legs, hands, friends) {
-  Animal.apply(this, arguments);
-  this.legs = legs;
-  this.hands = hands;
-  this.friends = friends;
-}
-HumanBeing.prototype = Object.create(Animal.prototype);
-HumanBeing.prototype.printAdditional = function() {
-  print(
+    this.name +
+    "</span>" +
+    '<span style="color: gray"> ' +
+    this.species +
+    "</span><br>" +
+    "  Gender: " +
+    this.gender +
+    "<br>" +
+    "  Says: " +
+    this.saying +
+    "<br>" +
     "  Legs: " +
-      this.legs +
-      "<br>" +
-      "  Hands: " +
-      this.hands +
-      "<br>" +
-      "  Friends: " +
-      this.friends.join(", ")
-  );
+    this.legs;
 };
-HumanBeing.prototype.constructor = HumanBeing;
+Creature.prototype.printInfo = function() {
+  this.formPrintString();
+  print(this.printStr);
+};
 
-function CatWoman(species, name, gender, saying, legs, hands, friends) {
-  HumanBeing.apply(this, arguments);
+//-----------------------------------------------------------------------------------
+function Cat(name, gender, saying, legs) {
+  Creature.apply(this, arguments);
+  this.species = "cat";
+}
+Cat.prototype = Object.create(Creature.prototype);
+Cat.prototype.constructor = Cat;
+
+//-----------------------------------------------------------------------------------
+function Dog(name, gender, saying, legs) {
+  Creature.apply(this, arguments);
+  this.species = "dog";
+}
+Dog.prototype = Object.create(Creature.prototype);
+Dog.prototype.constructor = Dog;
+
+//------------------------------------------------------------------------------------
+function Human(name, gender, saying, legs, hands, friends) {
+  Creature.apply(this, arguments);
+  this.species = "human";
+  this.hands = hands ? hands : 2;
+  this.friends = friends ? friends : [];
+}
+Human.prototype = Object.create(Creature.prototype);
+Human.prototype.constructor = Human;
+Human.prototype.printInfo = function() {
+  Creature.prototype.formPrintString.apply(this);
+  this.printStr +=
+    "<br>" +
+    "  Hands: " +
+    this.hands +
+    "<br>" +
+    "  Friends: " +
+    this.friends
+      .map(function(element) {
+        return element.name;
+      })
+      .join(", ");
+  print(this.printStr);
+};
+//-------------------------------------------------------------------------------------
+function CatWoman(name, gender, saying, legs, hands, friends) {
+  Human.apply(this, arguments);
+  this.species = "cat-woman";
   this.saying = cat.saying;
 }
-CatWoman.prototype = Object.create(HumanBeing.prototype);
+CatWoman.prototype = Object.create(Human.prototype);
 CatWoman.prototype.constructor = CatWoman;
 
 // ======== OBJECTS CREATION ============
-var dog = new Animal("dog", "Toby", "male", "Woff-woff!");
-var cat = new Animal("cat", "Tihon", "male", "Meow-ow");
-var woman = new HumanBeing("human", "Alya", "female", "Hello, there :)", 2, 2, [
-  "Julia",
-  "George",
-  "Denis",
-  "Vlad"
-]);
-var catWoman = new CatWoman("cat-woman", "Julia", "female", "", 2, 2, [
-  "Alya",
-  "George",
-  "Denis",
-  "Vlad"
-]);
+let dog = new Dog("Toby", "male", "Woff-woff!", 4);
+let cat = new Cat("Tihon", "male", "Meow-ow", 4);
+let woman = new Human("Alya", "female", "Hello, there :)", 2, 2, [dog, cat]);
+let man = new Human("George", "male", "Hey-ho!", 2, 2, [woman, dog, cat]);
+let catWoman = new CatWoman("Julia", "female", "", 2, 2, [woman, man, dog]);
 
+let allInhabitants = [];
+allInhabitants.push(dog, cat, woman, man, catWoman);
 // ======= OUTPUT =========
 dog.printInfo();
 cat.printInfo();
 woman.printInfo();
+man.printInfo();
 catWoman.printInfo();
