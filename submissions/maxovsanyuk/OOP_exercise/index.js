17d80025879373f8912ef303233ad332cbcc392b
Maks_Ovsianiuk <maksovsanyuk@gmail.com>
2018-12-12 16:28:00 +0200

    Employ default parameters

--- submissions/maxovsanyuk/OOP_exercise/index.js
+++ submissions/maxovsanyuk/OOP_exercise/index.js
@@ -1,41 +1,40 @@
 class Inhabitant {
-    constructor(name, gender, specie, saying) {
+    constructor(name, gender, legs) {
         this.name  `${name.charAt(0).toUpperCase() + name.slice(1)}`;
         this.gender = gender;
-        this.specie = specie;
-        this.saying = saying;
+        this.legs = legs;
     }
     introduce(){
       print(`<b>General information</b> ( ${this.specie}, ${this.gender} ). Hello my name is: <b>${this.name}</b>, I have: <b>${this.legs}</b> legs and <b>${this.hands? this.hands: "I don't have hands"}</b> hands. About myself: <b>${this.introduce}</b>`);
     }
   }
   class Dog extends Inhabitant {
-    constructor(name, gender) {
-      super(name, gender, 'Dog');
-      this.legs = 4;
+    constructor(name, gender, legs=4) {
+      super(name, gender, legs);
+      this.specie = 'Dog';
       this.saying = 'I am a dog.';
     }
   }
   class Cat extends Inhabitant {
-    constructor(name, gender) {
-      super(name, gender, 'Cat');
-      this.legs = 4;
+    constructor(name, gender, legs=4) {
+      super(name, gender);
+      this.specie = 'cat';
       this.saying = 'I am a cat.';
     }
   }
   class Man extends Inhabitant {
-    constructor(name, gender) {
-      super(name, gender, 'Man');
+    constructor(name, gender, legs=2) {
+      super(name, gender);
       this.hands = 2;
-      this.legs = 2;
+      this.specie = 'man'
       this.saying = 'I am a drummer.';
     }
   }
   class Woman extends Inhabitant {
-    constructor(name, gender) {
-      super(name, gender, 'Woman');
+    constructor(name, gender, legs=2) {
+      super(name, gender);
       this.hands = 2;
-      this.legs = 2;
+      this.specie = 'woman';
       this.saying = 'I am a teacher.';
     }
   }
