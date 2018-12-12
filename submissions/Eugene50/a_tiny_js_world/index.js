51e553af1c4fc882b3fa859149cceb2990c6ac1e
unknown <nova_alf@ukr.net>
2018-12-12 16:50:38 +0200

    correcting words

--- submissions/Eugene50/a_tiny_js_world/index.js
+++ submissions/Eugene50/a_tiny_js_world/index.js
@@ -1,9 +1,9 @@
 class Citizens {
-    constructor (species, gender, name, lags ='', hands = '', say){
+    constructor (species, gender, name, legs = '', hands = '', say){
      this.species = species;
      this.gender = gender;
      this.name = name;
-     this.lags = lags;
+     this.legs = legs;
      this.hands = hands;
      this.say = say;   
     }
@@ -11,10 +11,10 @@ class Citizens {
 
 let dog = new Citizens('dog','male','Sharik', 2, 4,'Gaf!!!');
 let cat = new Citizens('cat', 'male', 'Murzik', 4, 0,'Mjau)' );
-let man = new Citizens('human','male','Alex','2','2','Hello to All!!!');
-let woman = new Citizens('human','female','Maria','2','2','I want coffee!!!');
+let man = new Citizens('human','male','Alex', 2, 2, 'Hello to All!!!');
+let woman = new Citizens('human','female','Maria', 2, 2, 'I want coffee!!!');
 
-print(`${dog.species}; ${dog.gender}; ${dog.name};  ${dog.lags}; ${dog.hands}; ${dog.say}`);
-print(`${cat.species}; ${cat.gender}; ${cat.name};  ${cat.lags}; ${cat.hands}; ${cat.say}`);
-print(`${man.species}; ${man.gender}; ${man.name};  ${man.lags}; ${man.hands}; ${man.say}`);
-print(`${woman.species}; ${woman.gender}; ${woman.name};  ${woman.lags}; ${woman.hands}; ${woman.say}`);
+print(`${dog.species}; ${dog.gender}; ${dog.name};  ${dog.legs}; ${dog.hands}; ${dog.say}`);
+print(`${cat.species}; ${cat.gender}; ${cat.name};  ${cat.legs}; ${cat.hands}; ${cat.say}`);
+print(`${man.species}; ${man.gender}; ${man.name};  ${man.legs}; ${man.hands}; ${man.say}`);
+print(`${woman.species}; ${woman.gender}; ${woman.name};  ${woman.legs}; ${woman.hands}; ${woman.say}`);
