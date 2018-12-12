073d5976b2effd7ad41a415a7de72e16ef2808e1
unknown <nova_alf@ukr.net>
2018-12-12 16:08:48 +0200

    delete needless spaces

--- submissions/Eugene50/a_tiny_js_world/index.js
+++ submissions/Eugene50/a_tiny_js_world/index.js
@@ -1,5 +1,5 @@
 class Citizens {
-    constructor(species, gender, name, lags ='', hands = '', say){
+    constructor (species, gender, name, lags ='', hands = '', say){
      this.species = species;
      this.gender = gender;
      this.name = name;
@@ -9,13 +9,12 @@ class Citizens {
     }
 };
 
+let dog = new Citizens('dog','male','Sharik', 2, 4,'Gaf!!!');
+let cat = new Citizens('cat', 'male', 'Murzik', 4, 0,'Mjau)' );
+let man = new Citizens('human','male','Alex','2','2','Hello to All!!!');
+let woman = new Citizens('human','female','Maria','2','2','I want coffee!!!');
 
- let dog = new Citizens('dog','male','Sharik',2, 4,'Gaf!!!');
- let cat = new Citizens('cat', 'male', 'Murzik', 4, 0,'Mjau)' );
- let man = new Citizens('human','male','Alex','2','2','Hello to All!!!');
- let woman = new Citizens('human','female','Maria','2','2','I want coffee!!!');
-
-print(`${dog.species}; ${dog.gender}; ${dog.name};  ${dog.lags}; ${dog.hands}; ${dog.say} `);
-print(`${cat.species}; ${cat.gender}; ${cat.name};  ${cat.lags}; ${cat.hands}; ${cat.say} `);
-print(`${man.species}; ${man.gender}; ${man.name};  ${man.lags}; ${man.hands}; ${man.say} `);
-print(`${woman.species}; ${woman.gender}; ${woman.name};  ${woman.lags}; ${woman.hands}; ${woman.say} `);
+print(`${dog.species}; ${dog.gender}; ${dog.name};  ${dog.lags}; ${dog.hands}; ${dog.say}`);
+print(`${cat.species}; ${cat.gender}; ${cat.name};  ${cat.lags}; ${cat.hands}; ${cat.say}`);
+print(`${man.species}; ${man.gender}; ${man.name};  ${man.lags}; ${man.hands}; ${man.say}`);
+print(`${woman.species}; ${woman.gender}; ${woman.name};  ${woman.lags}; ${woman.hands}; ${woman.say}`);
