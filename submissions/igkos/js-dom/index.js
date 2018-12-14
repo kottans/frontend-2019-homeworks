92565a50701c145257c2b99722486408c4bbf958
ig.kos <ig.fire@gmail.com>
2018-12-14 18:56:16 +0200

    code duplication removed

--- submissions/igkos/js-dom/index.js
+++ submissions/igkos/js-dom/index.js
@@ -1,5 +1,4 @@
 window.onload = function() {
-
   const initialArraySort = 0;
 
   const ul = document.createElement("ul");
@@ -13,6 +12,20 @@ window.onload = function() {
   const sortingName = document.querySelector(".content__sortingName");
   const menuContainer = document.getElementById("drawer");
 
+  const selectSort = sortName => {
+    const selectedSort = arraySorts.find(
+      arraySort => arraySort.name === sortName
+    );
+    pageImg.innerHTML = `<img src="${selectedSort.img}"  alt="${
+      selectedSort.name
+    }">`;
+    prosCons.innerText = selectedSort.prosConsSorting;
+    pageContent.innerHTML = selectedSort.content;
+    sortingName.innerHTML = selectedSort.name;
+
+    hljs.initHighlighting.called = false;
+    hljs.initHighlighting();
+  };
 
   ul.classList.add("nav__list");
   arraySorts.forEach(arr => {
@@ -27,18 +40,7 @@ window.onload = function() {
   ul.addEventListener("click", evt => {
     if (evt.target && evt.target.nodeName.toLowerCase() === "li") {
       console.log(evt.target.textContent);
-      const selectedSort = arraySorts.find(
-        arraySort => arraySort.name === evt.target.textContent
-      );
-      pageImg.innerHTML = `<img src="${selectedSort.img}"  alt="${
-        selectedSort.name
-      }">`;
-      prosCons.innerText = selectedSort.prosConsSorting;
-      pageContent.innerHTML = selectedSort.content;
-      sortingName.innerHTML = selectedSort.name;
-      
-      hljs.initHighlighting.called = false;
-      hljs.initHighlighting();
+      selectSort(evt.target.textContent);
     }
   });
 
@@ -50,13 +52,5 @@ window.onload = function() {
     drawer.classList.remove("open");
   });
 
-  pageImg.innerHTML = `<img src="${arraySorts[initialArraySort].img}"  alt="${
-    arraySorts[initialArraySort].name
-  }">`;
-  prosCons.innerText = arraySorts[initialArraySort].prosConsSorting;
-  pageContent.innerHTML = arraySorts[initialArraySort].content;
-  sortingName.innerHTML = arraySorts[initialArraySort].name;
-
-  hljs.initHighlighting.called = false;
-  hljs.initHighlighting();
+  selectSort(arraySorts[initialArraySort].name);
 };
