0c221323dcd4c66bc48085a87327d5303939f78f
Aldegid <aldegid@gmail.com>
2018-12-19 19:30:23 +0200

    fix some bugs and implement recommendations

--- submissions/Aldegid/js-dom/js/common.js
+++ submissions/Aldegid/js-dom/js/common.js
@@ -1,31 +1,30 @@
 window.onload = function () {
 
-	const addContent = [ {
+	const content = [ {
 		title: 'HTML',
-		description: "<p>Hypertext Markup Language is the standard markup language for creating web pages and web applications. With Cascading Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.</p><p>Web browsers receive HTML documents from a web server or from local storage and render the documents into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.</p>",
+		description: "Hypertext Markup Language is the standard markup language for creating web pages and web applications. With Cascading Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web. Web browsers receive HTML documents from a web server or from local storage and render the documents into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.",
 		image: "img/HTML5_logo.png"
 	},
 	{
 		title: 'CSS',
-		description: "<p>Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.</p><p>CSS is designed to enable the separation of presentation and content, including layout, colors, and fonts. This separation can improve content accessibility, provide more flexibility and control in the specification of presentation characteristics, enable multiple web pages to share formatting by specifying the relevant CSS in a separate .css file, and reduce complexity and repetition in the structural content.<p>",
+		description: "Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.CSS is designed to enable the separation of presentation and content, including layout, colors, and fonts. This separation can improve content accessibility, provide more flexibility and control in the specification of presentation characteristics, enable multiple web pages to share formatting by specifying the relevant CSS in a separate .css file, and reduce complexity and repetition in the structural content.",
 		image: "img/CSS3_logo.png"
 	},
 	{
 		title: 'Javascript',
-		description: "<p>JavaScript often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification. It is a language which is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm</p><p>Alongside HTML and CSS, JavaScript is one of the three core technologies of the World Wide Web. JavaScript enables interactive web pages and thus is an essential part of web applications. The vast majority of websites use it, and all major web browsers have a dedicated JavaScript engine to execute it.</p>",
+		description: "JavaScript often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification. It is a language which is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm Alongside HTML and CSS, JavaScript is one of the three core technologies of the World Wide Web. JavaScript enables interactive web pages and thus is an essential part of web applications. The vast majority of websites use it, and all major web browsers have a dedicated JavaScript engine to execute it.",
 		image: "img/JavaScript_logo.png"
 	},
 	{
 		title: 'React',
-		description: "<p>AngularJS (also written as Angular.js) is a JavaScript-based open-source front-end web application framework mainly maintained by Google and by a community of individuals and corporations to address many of the challenges encountered in developing single-page applications. The JavaScript components complement Apache Cordova, a framework used for developing cross-platform mobile apps. It aims to simplify both the development and the testing of such applications by providing a framework for client-side model–view–controller (MVC) and model–view–viewmodel (MVVM) architectures, along with components commonly used in rich Internet applications. (This flexibility has led to the acronym MVW, which stands for and may also encompass model–view–presenter and model–view–adapter.) In 2014, the original AngularJS team began working on the Angular application platform.</p>",
+		description: "In computing, React (also known as React.js or ReactJS) is a JavaScript library[3] for building user interfaces. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications. Complex React applications usually require the use of additional libraries for state management, routing, and interaction with an API",
 		image: "img/React-icon.png"
 	},
 	{
 		title: 'Angular',
-		description: "<p>In computing, React (also known as React.js or ReactJS) is a JavaScript library[3] for building user interfaces. It is maintained by Facebook and a community of individual developers and companies</p><p>React can be used as a base in the development of single-page or mobile applications. Complex React applications usually require the use of additional libraries for state management, routing, and interaction with an API</p>",
+		description: "AngularJS (also written as Angular.js) is a JavaScript-based open-source front-end web application framework mainly maintained by Google and by a community of individuals and corporations to address many of the challenges encountered in developing single-page applications. The JavaScript components complement Apache Cordova, a framework used for developing cross-platform mobile apps. It aims to simplify both the development and the testing of such applications by providing a framework for client-side model–view–controller (MVC) and model–view–viewmodel (MVVM) architectures, along with components commonly used in rich Internet applications.",
 		image: "img/angular-logo.png"
 	}		
-		
 	]; 
 
 	var menu = document.querySelector('.nav__list');
@@ -37,24 +36,20 @@ window.onload = function () {
 	var ul = document.querySelector('.nav__list');
 	var txt = document.querySelector('.text');
 	var img = document.querySelector('.main__img-item');
-	console.log(img);
 
 	var currLang = null;
 
 	ul.addEventListener('click', function(e){
-		for(var i = 0; i < addContent.length; i++){
+		for(var i = 0; i < content.length; i++){
 			currLang = e.target.textContent;
-			if(currLang === addContent[i].title){
-				h2.textContent = addContent[i].title;
-				txt.innerHTML = addContent[i].description;
-				img.src = addContent[i].image;
+			if(currLang === content[i].title){
+				h2.textContent = content[i].title;
+				txt.textContent = content[i].description;
+				img.src = content[i].image;
 				break;
 			}
 		}
 	});
-
-
-
 
 	menu.addEventListener('click', function(e){
 		document.querySelector(".nav__item-active").classList.remove("nav__item-active");
@@ -66,8 +61,4 @@ window.onload = function () {
 		container.classList.toggle('column');
 		e.stopPropagation();
 	});
-
-
-	
-
-}
+};
