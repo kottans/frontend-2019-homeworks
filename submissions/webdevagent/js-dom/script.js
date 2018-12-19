ce249277af9cb2facd4bb81acb122a4098168592
webdevagent <44728742+webdevagent@users.noreply.github.com>
2018-12-19 14:17:10 +0200

    Replace let to const, loops to Array methods

--- submissions/webdevagent/js-dom/script.js
+++ submissions/webdevagent/js-dom/script.js
@@ -1,23 +1,20 @@
-let listItems = document.querySelectorAll('.listItem');
-let descriptions = document.querySelectorAll('.description');
-let openTab = (evt) => {
-  let tab = evt.target;
-  for (let i = 0; i < listItems.length; i++) {
-    if (listItems[i] != tab) {
-      listItems[i].style.backgroundColor = '#80dfff';
+const listItems = document.querySelectorAll('.listItem');
+const descriptions = document.querySelectorAll('.description');
+const replaceClass = (array, tObject, className) => {
+  array.forEach(item => {
+    if (item == tObject) {
+      tObject.classList.add(className);
     } else {
-      tab.style.backgroundColor = 'white';
+      item.classList.remove(className);
     }
-  }
-  for (let i = 0; i < descriptions.length; i++) {
-    if (descriptions[i] == tab.descriptionItem) {
-      tab.descriptionItem.style.display = 'inline-block';
-    } else {
-      descriptions[i].style.display = 'none';
-    }
-  }
+  });
 };
-for (let i = 0; i < listItems.length; i++) {
-  listItems[i].descriptionItem = descriptions[i];
-  listItems[i].addEventListener('click', openTab);
+const openTab = ({target}) => {
+  replaceClass(listItems, target, 'selected');
+  replaceClass(descriptions, target.descriptionItem, 'showTab');
 };
+listItems.forEach((item, i) => {
+  item.descriptionItem = descriptions[i];
+  item.addEventListener('click', openTab);
+});
+
