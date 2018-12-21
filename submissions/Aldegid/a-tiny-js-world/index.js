5df1ffe6263a8e5d37c035d567c1f7bc5eef97c3
Aldegid <aldegid@gmail.com>
2018-12-21 17:21:02 +0200

    add a-tiny-JS-world task

---
+++ submissions/Aldegid/a-tiny-js-world/index.js
@@ -0,0 +1,89 @@
+/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
+   Complete the below for code reviewers' convenience:
+
+   Code repository: _put repo URL here_
+   Web app: _put project's github pages URL here_
+   */
+
+// ======== OBJECTS DEFINITIONS ========
+// Define your objects here
+function outCreature(obj) {
+  var msg = [];
+  for (let key of Object.keys(obj)) {
+    msg.push(obj[key]);
+  }  
+  return  `Hello i am a <strong>${msg[0]}</strong>. My name is <strong>${msg[1]}</strong> and i am a ${msg[2]}. I have <strong>${msg[3]}</strong> legs and <strong>${msg[4]}</strong> hands and i saying: <em>${msg[5]}</em><hr>`;  
+}
+
+const dog = {
+  species: 'dog',
+  name: 'Toby',
+  gender: 'male',
+  legs: 4,
+  hands: 0,
+  saying: 'woof-woof!'
+};
+
+const cat = {
+  species: 'cat',
+  name: 'Murka',
+  gender: 'female',
+  legs: 4,
+  hands: 0,
+  saying: 'meow-meow!'
+};
+
+const man = {
+  species: 'human',
+  name: 'Vasiliy',
+  gender: 'male',
+  legs: 2,
+  hands: 2,
+  saying: 'I am hungry!'
+};
+
+const woman = {
+  species: 'human',
+  name: 'Marusiya',
+  gender: 'female',
+  legs: 2,
+  hands: 2,
+  saying: 'listen to me!'
+};
+
+const catWoman = {
+  species: cat.species + "-" + woman.species,
+  name: "Selina Kyle",
+  gender: "female",
+  legs: 2,
+  hands: 2,
+  saying: woman.saying + " and sometimes " + cat.saying 
+};
+
+
+
+
+
+// ======== OUTPUT ========
+/* Use print(message) for output.
+   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
+
+   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
+   However, please, REFRAIN from improving visuals at least until your code is reviewed
+   so code reviewers might focus on a single file that is index.js.
+   */
+  print(outCreature(dog));
+  print(outCreature(cat));
+  print(outCreature(man));
+  print(outCreature(woman));
+  print(outCreature(catWoman));
+
+/* Print examples:
+   print('ABC');
+   print('<strong>ABC</strong>');
+   print('<strong>ABC</strong>', 'div');
+
+   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
+   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
+   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
+   */
