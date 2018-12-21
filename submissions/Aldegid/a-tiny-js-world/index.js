b3488751b0dda39e658a86b77f5408b0b2b07218
Aldegid <aldegid@gmail.com>
2018-12-21 21:45:12 +0200

    one more try ot change function name

--- submissions/Aldegid/a-tiny-js-world/index.js
+++ submissions/Aldegid/a-tiny-js-world/index.js
@@ -7,12 +7,12 @@
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
-function creature(obj) {
+function makeCreatureMessage(obj) {
   var msg = [];
   for (let key of Object.keys(obj)) {
     msg.push(obj[key]);
-  }  
-  return  `Hello i am a <strong>${msg[0]}</strong>. My name is <strong>${msg[1]}</strong> and i am a ${msg[2]}. I have <strong>${msg[3]}</strong> legs and <strong>${msg[4]}</strong> hands and i saying: <em>${msg[5]}</em><hr>`;  
+  }
+  return  `Hello i am a <strong>${msg[0]}</strong>. My name is <strong>${msg[1]}</strong> and i am a ${msg[2]}. I have <strong>${msg[3]}</strong> legs and <strong>${msg[4]}</strong> hands and i saying: <em>${msg[5]}</em><hr>`;
 }
 
 const dog = {
@@ -57,20 +57,11 @@ const catWoman = {
   gender: "female",
   legs: 2,
   hands: 2,
-  saying: woman.saying + " and sometimes " + cat.saying 
+  saying: woman.saying + " and sometimes " + cat.saying
 };
 
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-  print(creature(dog));
-  print(creature(cat));
-  print(creature(man));
-  print(creature(woman));
-  print(creature(catWoman));
-  
+  print(makeCreatureMessage(dog));
+  print(makeCreatureMessage(cat));
+  print(makeCreatureMessage(man));
+  print(makeCreatureMessage(woman));
+  print(makeCreatureMessage(catWoman));
