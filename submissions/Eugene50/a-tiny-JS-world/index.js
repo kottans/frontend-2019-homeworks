15e349c4b7a34090e1f3d62be6a9641326114b72
unknown <nova_alf@ukr.net>
2018-12-21 16:48:29 +0200

    update code

--- submissions/Eugene50/a-tiny-JS-world/index.js
+++ submissions/Eugene50/a-tiny-JS-world/index.js
@@ -9,12 +9,12 @@ class Citizens {
     }
 };
 
-let dog = new Citizens('dog','male','Sharik', 2, 4,'Gaf!!!');
-let cat = new Citizens('cat', 'male', 'Murzik', 4, 0,'Mjau)' );
+let dog = new Citizens('dog','male','Sharik', 2, 4, 'Gaf!!!');
+let cat = new Citizens('cat', 'male', 'Murzik', 4, 0, 'Mjau)' );
 let man = new Citizens('human','male','Alex', 2, 2, 'Hello to All!!!');
 let woman = new Citizens('human','female','Maria', 2, 2, 'I want coffee!!!');
 
-const all  = [man, woman, cat, dog];
+const all = [man, woman, cat, dog];
 all.forEach(element => {
     print(`${element.species}; ${element.gender}; ${element.name}; ${element.legs}; ${element.hands}; ${element.say}`);
 })
