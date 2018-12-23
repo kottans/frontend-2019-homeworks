2b1fad0a731d8f7276ee5e27996196c21d6cc464
IrynaY <yrchuk.iryna@gmail.com>
2018-12-23 16:48:05 +0200

    refactor

--- submissions/IrynaY/memory-pare-game/index.js
+++ submissions/IrynaY/memory-pare-game/index.js
@@ -3,61 +3,64 @@ const BODY = document.querySelector("body")
 const CONGRATULATION = document.createElement("div")
 const WON_STRING = document.createElement("p")
 const RESTART = document.createElement("button")
+const TIMEOUT = 700
 
 function renderCongratulation(){
     CONGRATULATION.classList = "ho"
     WON_STRING.innerHTML = "🎉 YOU WON! 🎉"
     RESTART.id = "restart"
     RESTART.innerHTML = "Play again 🎅🏻"
-    RESTART.onclick = () => {
+    RESTART.addEventListener("click", () => {
         BODY.removeChild(CONGRATULATION)
         initGame()
-    }
-    CONGRATULATION.appendChild(WON_STRING)
-    CONGRATULATION.appendChild(RESTART)
+    })
+    CONGRATULATION.append(WON_STRING)
+    CONGRATULATION.append(RESTART)
     BODY.removeChild(document.getElementById("field"))
-    BODY.appendChild(CONGRATULATION)
+    BODY.append(CONGRATULATION)
 }
 
 function initGame(){
-    const FIELD = document.createElement("div")
-    FIELD.id = "field"
-    
-    let images = FILES.concat(FILES)
-    images.sort(() => 0.5 - Math.random())
-
     let openCards = []
 
-    for(let i=0; i<12; i++){
+    const FIELD = document.createElement("div")
+    FIELD.id = "field"
+
+    let images = FILES.concat(FILES)
+    images.sort(() => 0.5 - Math.random())
+    images.forEach(imageSrc => {
         const IMAGE = document.createElement("img")
-        IMAGE.classList = "back"
-        IMAGE.src = images.shift()
+        IMAGE.classList = "card back"
+        IMAGE.src = imageSrc
 
         const FRONT = document.createElement("div")
         FRONT.classList = "front"
         
         const FLIPPER = document.createElement("div")
         FLIPPER.classList = "flipper"
-        FLIPPER.addEventListener("click", (event) => {
-            if(event.target.tagName === "DIV" && openCards.length<2){
-                FLIPPER.classList.toggle("flip")
-                openCards.push(FLIPPER)
-                if(openCards.length === 2)
-                    setTimeout(() => {
-                        openCards[0].children[0].src === openCards[1].children[0].src ? 
-                            openCards.forEach(element => element.children[0].classList.toggle("hide")) :
-                            openCards.forEach(element => element.classList.toggle("flip"))
-                        openCards = []
-                        if(document.getElementsByClassName("hide").length === 12)
-                            renderCongratulation()
-                    }, 700)
-            }
-        })
-        FLIPPER.appendChild(IMAGE)
-        FLIPPER.appendChild(FRONT)
-        FIELD.appendChild(FLIPPER)
-    }
-    BODY.appendChild(FIELD)
+
+        FLIPPER.append(IMAGE)
+        FLIPPER.append(FRONT)
+        FIELD.append(FLIPPER)
+    })
+
+    FIELD.addEventListener("click", (event) => {
+        if(event.target.classList.value === "front" && openCards.length<2){
+            event.target.parentNode.classList.toggle("flip")
+            openCards.push(event.target.parentNode.querySelector("img"))
+
+            if(openCards.length === 2)
+                setTimeout(() => {
+                    openCards[0].src === openCards[1].src ? 
+                        openCards.forEach(img => img.classList.toggle("hide")) :
+                        openCards.forEach(img => img.parentNode.classList.toggle("flip"))
+                    openCards = []
+                    if(document.getElementsByClassName("hide").length === images.length)
+                        renderCongratulation()
+                }, TIMEOUT)
+        }
+    })
+    BODY.append(FIELD)
 }
 
 initGame()
