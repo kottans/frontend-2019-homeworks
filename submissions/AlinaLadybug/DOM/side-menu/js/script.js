d633df113eb98c5a34c3ad9f3cc562acfd798d6b
AlinaLadybug <alinaladybug23@gmail.com>
2018-12-23 13:28:00 +0200

    refactor after review

--- submissions/AlinaLadybug/DOM/side-menu/js/script.js
+++ submissions/AlinaLadybug/DOM/side-menu/js/script.js
@@ -3,24 +3,24 @@ document.addEventListener("DOMContentLoaded", ready);
 //start function
 function ready() {
     var contents = document.getElementById('content-wrapper').children;
-    for (var i = 1; i < contents.length; i++) {
-        contents[i].style.display = "none";
-    }
-    contents[0].style.display = "block";
-
+    
+    var li_elements = Array.from(document.getElementsByClassName('menu')[0].children);
+    li_elements.forEach(element => {
+        element.addEventListener("mouseover", function () { hover(element) });
+        element.addEventListener("mouseout", function () { unhover(element) });
+        element.addEventListener("click", function () { changeContent(element) });
+    });
 }
-
-//hover & unhover nav elements
 function hover(element) {
     var image = document.getElementsByClassName(element.className)[0].children[0];
     var oldSrc = image.getAttribute('src');
     image.setAttribute('src', oldSrc.replace('before', 'after'));
 }
 function unhover(element) {
-    if (element.className.includes('active')) {
+    if (element.classList.contains('active')) {
         return;
     }
-    var image = document.getElementsByClassName(element.className)[0].children[0];
+    var image = document.querySelector("."+element.classList[0]+" img");
     var oldSrc = image.getAttribute('src');
     image.setAttribute('src', oldSrc.replace('after', 'before'));
 }
@@ -29,24 +29,24 @@ function unhover(element) {
 
 function changeContent(element) {
     //disable all contents
-    var contents = document.getElementById('content-wrapper').children;
-    for (var i = 0; i < contents.length; i++) {
-        contents[i].style.display = "none";
-    }
+    var contents = Array.from(document.getElementById('content-wrapper').children);
+    contents.forEach(element => {
+        element.classList.remove('active-content');
+    });
 
     //disable active classes
-    var active = document.getElementsByClassName('active');
-    for (var i = 0; i < active.length; ++i) {
-        var className = active[i].className.split(' ')[0];
-        var image = active[i].children[0];
+    var active = Array.from(document.getElementsByClassName('active'));
+    active.forEach(el=> {
+        var image = document.querySelector("."+el.classList[0]+" img");
         var oldSrc = image.getAttribute('src');
         image.setAttribute('src', oldSrc.replace('after', 'before'));
-        active[i].className = className;
-    }
+        el.classList.toggle('active');
+    });
 
     hover(element);
     //enable content
-    document.getElementById(element.className).style.display = "block";
-
-    element.className = element.className + ' active';
+    
+    var content = document.querySelector("#content-wrapper ."+element.className);
+    content.classList.toggle('active-content');
+    element.classList.toggle('active');
 }
