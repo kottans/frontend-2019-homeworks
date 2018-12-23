c3113a5f9fa60bb67c0137536da3f436e0d2f334
Eugene <35635032+Eugene50@users.noreply.github.com>
2018-12-24 05:49:05 +0900

    js_pre_oop (#111)
    * add new file
    
    * correcting code
    
    * update code
    
    * refactoring code
    
    * refactoring code (update)
    
    * add empty line at the end of file
    
    * defined default params

---
+++ submissions/Eugene50/a-tiny-JS-world/index.js
@@ -0,0 +1,20 @@
+class Citizens {
+    constructor (species, gender, name, say, legs = 0, hands = null ){
+     this.species = species;
+     this.gender = gender;
+     this.name = name;
+     this.legs = legs;
+     this.hands = hands;
+     this.say = say;
+    }
+};
+
+let dog = new Citizens('dog','male','Sharik','Gaf!!!', 4);
+let cat = new Citizens('cat', 'male', 'Murzik','Mjau)', 4 );
+let man = new Citizens('human','male','Alex','Hello to All!!!', 2, 2);
+let woman = new Citizens('human','female','Maria','I want coffee!!!', 2, 2);
+
+const all = [man, woman, cat, dog];
+all.forEach(element => {
+    print([element.species, element.gender, element.name, element.say, element.legs, element.hands].join('; '));
+})
