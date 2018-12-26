981802ec36fd32fa73b369d3ec5c1e0049f95d48
Aldegid <aldegid@gmail.com>
2018-12-26 17:39:28 +0200

    implement OOP code organization

--- submissions/Aldegid/js-oop-frogger/js/app.js
+++ submissions/Aldegid/js-oop-frogger/js/app.js
@@ -1,138 +1,137 @@
-const playerStepY = 101;
-const playerStepX = 83;
+const BLOCK_WIDTH = 101;
+const BLOCK_HEIGHT = 83;
+const FIELD_WIDTH = 505;
+const START_POSITION_X = 202;
+const START_POSITION_Y = 404;
+
+const WIN_CONTAINER = document.querySelector('.wins');
+const LIVES_CONTAINER = document.querySelector('.lives');
+const GAME_OVER = document.querySelector('.game-over');
+
+const ENEMY_CONF = {
+    sprite: 'images/enemy-bug.png',
+    startX: -50,
+    startY: 63,
+    spaceBetween: 20
+};
+const PLAYER_CONF = {
+    sprite: 'images/char-boy.png',
+    spriteWidth: 65,
+    spriteHeight: 75,
+    spriteIsRip: 'images/rip.png'
+};
+
 let wins = 0;
 let lives = 3;
-let winContainer = document.querySelector('.wins');
-let livesContainer = document.querySelector('.lives');
-let gameOver = document.querySelector('.game-over');
-// const startPlayerPosition =
-// Enemies our player must avoid
-var Enemy = function(x,y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+let Unit = function (x, y, sprite) {
+    this.sprite = sprite;
     this.x = x;
     this.y = y;
-    this.speed = speed;
 };
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+
+Unit.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+let Enemy = function (x, y, player) {
+    Unit.call(this, x, y, ENEMY_CONF.sprite);
+    this.speed = this.generteSpeed();
+    this.player = player;
+    console.log(this.player);
+};
+
+Enemy.prototype = Object.create(Unit.prototype);
+Enemy.prototype.constructor = Enemy;
+Enemy.prototype.generteSpeed = function () {
+    return 100 + Math.floor(Math.random() * 200);
+};
+
+Enemy.prototype.update = function (dt) {
     this.x += this.speed * dt;
-    if(this.x > 505){
-        this.x = -100;
+    if (this.x > FIELD_WIDTH) {
+        this.x = -BLOCK_WIDTH;
         this.speed = 100 + Math.floor(Math.random() * 200);
     }
     this.checkCollisions();
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-Enemy.prototype.checkCollisions = function() {
-    if( player.x < this.x + 75 &&
-        player.x + 75 > this.x &&
-        player.y < this.y + 65 &&
-        player.y + 65 > this.y){
-            player.goToStartPosition();
-            lives --;
-            updateResult();
-            if(lives === 0){
-                //console.log('rip');
-                allEnemies = [];
-                gameOver.setAttribute("style", "display: block; margin: 0; font-size: 40px; color: red;");
-                player.sprite = rip.sprite;
-            }
-
-        }
-};
-
-var RipPlayer = function(){
-    this.sprite = 'images/rip.png';
-};
-
-RipPlayer.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-var Player = function(){
-    this.x = 202;
-    this.y = 404;
-    this.stepX = 101;
-    this.stepY = 83;
-    this.sprite = 'images/char-boy.png';
-
-}
-
-Player.prototype.update = function(){
-
-};
-
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-Player.prototype.goToStartPosition = function() {
-    this.x = 202;
-    this.y = 404;
-
-};
-
-Player.prototype.handleInput = function(dir) {
-    if(lives !== 0){
-        switch (dir) {
-            case 'left':
-            this.x >= this.stepX ? this.x -= this.stepX : this.x -= 0;
-                break;
-            case 'right':
-            this.x <= (this.stepX * 3) ? this.x += this.stepX : this.x += 0;
-                break;
-            case 'up':
-            this.y -= this.stepY;
-            if(this.y < 72){
-                wins ++;
-                updateResult();
-                this.goToStartPosition();
-            }
-                break;
-            case 'down':
-            this.y <= (this.stepY * 4) ? this.y += this.stepY : this.y += 0;
-                break;
-
+Enemy.prototype.checkCollisions = function () {
+    if (this.player.x < this.x + PLAYER_CONF.spriteWidth &&
+        this.player.x + PLAYER_CONF.spriteWidth > this.x &&
+        this.player.y < this.y + PLAYER_CONF.spriteHeight &&
+        this.player.y + PLAYER_CONF.spriteHeight > this.y) {
+        player.goToStartPosition();
+        lives--;
+        updateResult();
+        if (lives === 0) {
+            allEnemies = [];
+            GAME_OVER.setAttribute("style", "display: block; margin: 0; font-size: 40px; color: red;");
+            player.sprite = rip.sprite;
         }
     }
+};
 
+let RipPlayer = function (x, y) {
+    Unit.call(this, x, y, PLAYER_CONF.spriteIsRip);
+};
+
+RipPlayer.prototype = Object.create(Unit.prototype);
+RipPlayer.prototype.constructor = RipPlayer;
+
+let Player = function (x, y) {
+    Unit.call(this, x, y, PLAYER_CONF.sprite);
+    this.stepX = BLOCK_WIDTH;
+    this.stepY = BLOCK_HEIGHT;
+    this.sprite = PLAYER_CONF.sprite;
+};
+Player.prototype = Object.create(Unit.prototype);
+Player.prototype.constructor = Player;
+
+Player.prototype.update = function () {
+};
+
+Player.prototype.goToStartPosition = function () {
+    this.x = START_POSITION_X;
+    this.y = START_POSITION_Y;
+};
+
+Player.prototype.handleInput = function (dir) {
+    if (lives !== 0) {
+        switch (dir) {
+            case 'left':
+                this.x >= this.stepX ? this.x -= this.stepX : this.x -= 0;
+                break;
+            case 'right':
+                this.x <= (this.stepX * 3) ? this.x += this.stepX : this.x += 0;
+                break;
+            case 'up':
+                this.y -= this.stepY;
+                if (this.y < 72) {
+                    wins++;
+                    updateResult();
+                    this.goToStartPosition();
+                }
+                break;
+            case 'down':
+                this.y <= (this.stepY * 4) ? this.y += this.stepY : this.y += 0;
+                break;
+        }
+    }
     //console.log(this.x);
     //console.log(this.y);
 };
 
-function updateResult(){
-    winContainer.innerHTML = wins;
-    livesContainer.innerHTML = lives;
+function updateResult() {
+    WIN_CONTAINER.innerHTML = wins;
+    LIVES_CONTAINER.innerHTML = lives;
 }
 
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-let player = new Player();
-let allEnemies = [new Enemy(0, 220, 50), new Enemy(-50, 140, 80), new Enemy(-100, 60, 100)];
+let player = new Player(START_POSITION_X, START_POSITION_Y);
+let allEnemies = [1, 2, 3].map(coordsY => new Enemy(ENEMY_CONF.startX, (coordsY * BLOCK_HEIGHT) - ENEMY_CONF.spaceBetween, player));
 let rip = new RipPlayer();
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
