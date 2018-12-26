186f6f88f018b3ef914e3ad894775a7262e996fc
unknown <nova_alf@ukr.net>
2018-12-26 18:21:22 +0200

    refactoring code (replace peaces)

--- submissions/Eugene50/js_oop_frogger/js/app.js
+++ submissions/Eugene50/js_oop_frogger/js/app.js
@@ -9,40 +9,6 @@ function enemyRandomSpeed(max, min) {
     return Math.floor(Math.random() * (max - min)) + min;
   };
 
-  class Enemy {
-    constructor(speed, x, y, width, height, sprite = imgBug) {
-      this.speed = speed;
-      this.x = x;
-      this.y = y;
-      this.width = width;
-      this.height = height;
-      this.sprite = sprite;
-    }
-  }
-
-  Enemy.prototype.update = function (dt) {
-    if (this.x < widthField) {
-      this.x += (this.speed * dt);
-    } else {
-      this.x = 0;
-    }
-  };
-
-  Enemy.prototype.render = function () {
-    let XColl = false;
-    let YColl = false;
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y, this.width, this.height);
-
-    if (((this.x + 10) + (this.width - 50) >= (player.x + 10)) && ((this.x + 10) <= (player.x + 10) + (player.width - 50))) XColl = true;
-    if (((this.y + 20) + (this.height - 120) >= player.y + 10) && ((this.y + 10) <= (player.y + 10) + player.height - 120)) YColl = true;
-
-  // back to the start point
-    if (XColl && YColl) {
-      player.x = playerPositionX;
-      player.y = playerPositionY;
-    };
-  };
-
   class Player {
     constructor(x, y, width, height, sprite = imgBoy) {
       this.x = x;
@@ -75,13 +41,48 @@ function enemyRandomSpeed(max, min) {
       this.y += 80;
     }
   };
+
+  const player = new Player(200, 380, 101, 171);
+
+  class Enemy {
+    constructor(speed, x, y, width, height, sprite = imgBug, boy = player) {
+      this.speed = speed;
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+      this.sprite = sprite;
+      this.boy  = boy;
+    }
+  }
+
+  Enemy.prototype.update = function (dt) {
+    if (this.x < widthField) {
+      this.x += (this.speed * dt);
+    } else {
+      this.x = 0;
+    }
+  };
+
+  Enemy.prototype.render = function () {
+    let XColl = false;
+    let YColl = false;
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y, this.width, this.height);
+
+    if (((this.x + 10) + (this.width - 50) >= (this.boy.x + 10)) && ((this.x + 10) <= (this.boy.x + 10) + (this.boy.width - 50))) XColl = true;
+    if (((this.y + 20) + (this.height - 120) >= this.boy.y + 10) && ((this.y + 10) <= (this.boy.y + 10) + this.boy.height - 120)) YColl = true;
+
+  // back to the start point
+    if (XColl && YColl) {
+      this.boy.x = playerPositionX;
+      this.boy.y = playerPositionY;
+    };
+  };
   //------------------------------------------------------
   const enemy = new Enemy(enemyRandomSpeed(70, 200), 0, 63, 101, 171);
   const enemyJack = new Enemy(enemyRandomSpeed(90, 200), 0, 145, 101, 171);
   const enemyMarty = new Enemy(enemyRandomSpeed(40, 250), 0, 228, 101, 171);
   const enemyBuba = new Enemy(enemyRandomSpeed(68, 150), 0, 63, 101, 171);
-
-  const player = new Player(200, 380, 101, 171);
   //------------------------------------------------------
   const allEnemies = [enemy, enemyJack, enemyMarty, enemyBuba];
   //------------------------------------------------------
