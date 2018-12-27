3a10b5a82137553ed35fcd13d5f0bbe53dae497f
linkqwd <linkqwd@gmail.com>
2018-12-27 22:44:29 +0200

    Code update after review

--- submissions/linkqwd/task_11_friendsApp/main.js
+++ submissions/linkqwd/task_11_friendsApp/main.js
@@ -1,7 +1,3 @@
-const settings = {
-    friendsCount: 50,
-}
-
 const DOM = {
     friendHolder: 'b-friends-list',
     friendCard: 'b-friends-list__friend-item',
@@ -38,31 +34,59 @@ class Friend {
 }
 
 function buildFriendsList() {
-    fetch(`https://randomuser.me/api/?results=${settings.friendsCount}`, { method: "GET" })
+    fetch(`https://randomuser.me/api/?results=50`)
         .then(data => data.json())
-        .then((json) => {
-            json.results.forEach((friend, i) => {
-                friendsList.push(new Friend({ photo: friend.picture.large, name: friend.name.first, lastName: friend.name.last, gender: friend.gender, cell: friend.cell, age: friend.dob.age, id: i + 100 }));
-            });
-        })
-        .then(() => {
-            renderHTMLFriendsCards(friendsList);
+        .then((friendsData) => friendsList = friendsData.results.map(buildFriend))
+        .then(() => renderHTMLFriendsCards(friendsList));
+
+    function buildFriend(friend) {
+        return new Friend({
+            photo: friend.picture.large,
+            name: friend.name.first,
+            lastName: friend.name.last,
+            gender: friend.gender,
+            cell: friend.cell,
+            age: friend.dob.age,
+            id: friend.login.uuid,
         });
+    };
 };
 
-let ucFirst = (str) => str.charAt(0).toUpperCase() + str.slice(1); // UpperCase For first letter
+let capitalizeFirstLetter = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 
 function renderHTMLFriendsCards(friendsArray) {
-    let friendsHTMLCards = [];
-    for (let i = 0; i < friendsArray.length; i++) {
-        friendsHTMLCards.push(`<li class="${DOM.friendCard} ${!friendsArray[i].isPresentInList ? 'hidden' : ''}" id="${friendsArray[i].id}"><div class="b-friends-list__friend-item-wrapper"><div class="b-friends-list__img-wrapper"><img class="b-friends-list__image" src="${friendsArray[i].photo}" alt="test"></div><div class="b-friends-list__info-wrapper"><div class="b-friends-list__friend-name b-friends-list__info-row"><span>${ucFirst(friendsArray[i].name)} ${ucFirst(friendsArray[i].lastName)}</span></div><div class="b-friends-list__friend-age b-friends-list__info-row"><span>Age: ${friendsArray[i].age}</span></div><div class="b-friends-list__friend-age b-friends-list__info-row"><span>Gender: ${friendsArray[i].gender}</span></div><div class="b-friends-list__friend-phone b-friends-list__info-row"><span>Phone: ${friendsArray[i].cell}</span></div></div></div></li>`);
+    let friendsHTMLCards = friendsArray.map(buildHTMLCard);
+
+    function buildHTMLCard(friend) {
+        return `
+        <li class="${DOM.friendCard} ${!friend.isPresentInList ? 'hidden' : ''}" id="${friend.id}">
+            <div class="b-friends-list__friend-item-wrapper">
+                <div class="b-friends-list__img-wrapper">
+                    <img class="b-friends-list__image" src="${friend.photo}" alt="test">
+                </div>
+                <div class="b-friends-list__info-wrapper">
+                    <div class="b-friends-list__friend-name b-friends-list__info-row">
+                        <span>${capitalizeFirstLetter(friend.name)} ${capitalizeFirstLetter(friend.lastName)}</span>
+                    </div>
+                    <div class="b-friends-list__friend-age b-friends-list__info-row">
+                        <span>Age: ${friend.age}</span>
+                    </div>
+                    <div class="b-friends-list__friend-gender b-friends-list__info-row">
+                        <span>Gender: ${friend.gender}</span>
+                        </div>
+                    <div class="b-friends-list__friend-phone b-friends-list__info-row">
+                        <span>Phone: ${friend.cell}</span>
+                    </div>
+                </div>
+            </div>
+        </li>`;
     }
 
     DOM.friendHolderSelector.innerHTML = friendsHTMLCards.join('');
 }
 
 function filteringByName() {
-    friendsList.forEach((friend) => ~friend.fullName.indexOf(this.value.toLowerCase()) ? friend.showInList() : friend.removeFromList());
+    friendsList.forEach((friend) => friend.fullName.search(new RegExp(this.value.toLowerCase())) != -1 ? friend.showInList() : friend.removeFromList());
 }
 
 function sortingOptions(e) {
