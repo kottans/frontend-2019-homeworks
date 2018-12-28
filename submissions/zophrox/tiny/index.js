940d4a4b9d35d5ac2cc27613b1fd7d3cdf728e76
zophrox <zophrox@gmail.com>
2018-12-28 20:27:35 +0200

    Tiny JS World (add join,delete extra files, changed if to ?)

--- submissions/zophrox/tiny/index.js
+++ submissions/zophrox/tiny/index.js
@@ -1,14 +1,46 @@
-var dog = {kind:"animal", who:"dog", legs:"4", hands:"0", name:"Rex" ,gender:"male" , say:"woof-woof!", friend:"Mary, Jon"};
-var cat = {kind:"animal", who:"cat", legs:"4", hands:"0", name:"Lisa" ,gender:"female" , say:"meow!"};
-var woman = {kind:"human", who:"woman",legs:"2", hands:"2", name:"Mary" ,gender:"female" , say:"Hello Jon!", friend:"Mary, Rex"};
-var man = {kind:"human", who:"man",legs:"2", hands:"2", name:"Jon" ,gender:"male" , say:"Hello Mary!", friend:"Jon, Rex"};
+var dog = { kind:"animal",
+            who:"dog",
+            legs:"4",
+            hands:"0",
+            name:"Rex",
+            gender:"male",
+            say:"woof-woof!",
+            friend:"Mary, Jon"};
+
+var cat = { kind:"animal",
+            who:"cat",
+            legs:"4",
+            hands:"0",
+            name:"Lisa",
+            gender:"female",
+            say:"meow!"};
+
+var woman = { kind:"human",
+              who:"woman",
+              legs:"2",
+              hands:"2",
+              name:"Mary",
+              gender:"female",
+              say:"Hello Jon!",
+              friend:"Mary, Rex"};
+
+var man = { kind:"human",
+            who:"man",
+            legs:"2",
+            hands:"2",
+            name:"Jon",
+            gender:"male",
+            say:"Hello Mary!",
+            friend:"Jon, Rex"};
+
 var  inhabitantes = [dog, cat, woman, man];
+
 inhabitantes.forEach(function(element){
-    var person = "";
-    person += element.kind + ";" + element.who + ";" + element.legs + ";"
-    + element.hands + ";" + element.name + ";" + element.gender + ";" + element.say;
-    if(element.friend !== undefined){
-        person += "; " + element.friend;
-    }
-    console.log(person);
+    var personArr = [];
+    var personStr = "";
+    personArr.push(element.kind, element.who, element.legs, element.hands, element.name, element.gender, element.say);
+    personStr = personArr.join(";");
+    personStr += (element.friend !== undefined) ?  ("; " + element.friend): "";
+
+    print(personStr);
 });
