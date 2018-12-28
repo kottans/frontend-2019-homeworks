02562e2fd33b6eb04c578c580231c91c483a1a5e
Vitalii Vitaminenko <vitamin@ukr.net>
2018-12-28 21:30:46 +0200

    Add some improvements.

--- submissions/Vitaminvp/js-oop-frogger/app.js
+++ submissions/Vitaminvp/js-oop-frogger/app.js
@@ -1,31 +1,47 @@
-const cellWidth = 100;
-const cellHeight = 80;
-const enemyWidth = 80;
-const enemyHeight = 60;
-const initialX = 200;
-const initialY = 400;
-const SPRITES = [
-    'images/char-boy.png',
-    'images/char-cat-girl.png',
-    'images/char-horn-girl.png',
-    'images/char-pink-girl.png',
-    'images/char-princess-girl.png'
-];
+const CELL = {
+    width: 100,
+    height: 80
+};
+const ENEMY = {
+    width: 80,
+    height: 60
+};
+const INITIAL = {
+    x: 200,
+    y: 400
+};
+
+const SPRITES = {
+    player: 'images/char-boy.png',
+    enemy: 'images/enemy-bug.png'
+};
+
 function getRandomSpeed() {
     return Math.round(Math.random() * 600);
 }
 
-// Enemies our player must avoid
-var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+const Creature = function(x, y, sprite){
     this.x = x;
     this.y = y;
+    this.sprite = sprite;
+};
+
+Creature.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Enemies our player must avoid
+const Enemy = function(x, y, speed, player) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+    Creature.call(this, x, y, SPRITES.enemy);
     this.speed = speed;
+    this.player = player;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
 };
+
+Enemy.prototype = Object.create(Creature.prototype);
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -37,63 +53,59 @@ Enemy.prototype.update = function(dt) {
     // all computers.
     this.x += this.speed * dt;
 
-    if (this.x > cellWidth * 5) {
-        this.x = -cellWidth;
-        this.speed = getRandomSpeed();
-    }
+    handleCollision(this.player, this);
 
-    if (player.x < this.x + enemyWidth  && player.x    + enemyWidth > this.x &&
-        player.y < this.y + enemyHeight && enemyHeight + player.y   > this.y) {
-        player.x = initialX;
-        player.y = initialY;
+};
+
+function handleCollision(player, enemy) {
+    if (enemy.x > CELL.width * 5) {
+        enemy.x = -CELL.width;
+        enemy.speed = getRandomSpeed();
     }
+    if (player.x < enemy.x + ENEMY.width && player.x + ENEMY.width > enemy.x &&
+        player.y < enemy.y + ENEMY.height && ENEMY.height + player.y > enemy.y){
+        this.player.x = INITIAL.x;
+        this.player.y = INITIAL.y;
+    };
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
-var Player = function(x, y) {
-    this.player = SPRITES[0];//"images/char-boy.png";
-    this.x = x;
-    this.y = y;
+const Player = function(x, y) {
+    Creature.call(this, x, y, SPRITES.player);
 };
+
+Player.prototype = Object.create(Creature.prototype);
 
 Player.prototype.update = function() {};
-
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.player), this.x, this.y);
-};
 
 Player.prototype.handleInput = function(key) {
     switch (key){
         case'up':
-            this.y = (this.y > 0) ? this.y- cellHeight : initialY;
+            (this.y > 0) ? this.y = this.y- CELL.height : this.y = INITIAL.y;
             break;
         case'down':
-            this.y = (this.y < cellHeight * 5) ? this.y + cellHeight : this.y;
+            (this.y < CELL.height * 5) ? this.y = this.y + CELL.height : null;
             break;
         case'left':
-            this.x = (this.x > 1) ? this.x - cellWidth : this.x;
+            (this.x > 1) ? this.x =  this.x - CELL.width : null;
             break;
         case'right':
-            this.x = (this.x < cellWidth * 4) ? this.x + cellWidth : this.x;
+            (this.x < CELL.width * 4) ? this.x = this.x + CELL.width : null;
             break;
     }
 };
 
-var player = new Player(initialX, initialY);
-var allEnemies = [];
+const player = new Player(INITIAL.x, INITIAL.y);
 
-[cellHeight , cellHeight * 2, cellHeight * 3].forEach(function(y) {
-    var enemy = new Enemy(0, y, getRandomSpeed());
-    allEnemies.push(enemy);
-});
+const allEnemies = [1, 2, 3]
+    .map(fieldRow => fieldRow * CELL.height)
+    .map(y => new Enemy(0, y, getRandomSpeed(), player));
+
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
@@ -103,7 +115,7 @@ var allEnemies = [];
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    const allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
