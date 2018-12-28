954a379eeef775ca77391de28346aab6ea015f7e
feroxes <feroxes@gmail.com>
2018-12-28 12:42:16 +0200

    fixed accorind to review

--- submissions/feroxes/a-tiny-js-world/script.js
+++ submissions/feroxes/a-tiny-js-world/script.js
@@ -1,38 +1,43 @@
 class Creature {
-    constructor(whoIs, gender, name, say, legs){
+    constructor(whoIs, gender, name, say) {
         this.whoIs = whoIs;
         this.gender = gender;
         this.name = name;
         this.say = say;
-        this.legs = legs;
     }
-    toSay(){
-            console.log(this.say);
-        }
-    print(){
+
+    toSay() {
+        console.log(this.say);
+    }
+
+    print() {
         let info = [];
-        
-        for(let key in this){
-           info.push(this[key]);
+        for (let key in this) {
+            info.push(this[key]);
         }
         let str = info.join('; ');
         console.log(str);
     }
 }
-class Person extends Creature{
-     constructor(whoIs, gender, name, say, legs, hands, friends){
-        super(whoIs, gender, name, say, legs)
-        this.hands = hands;
-        this.friends = friends;
-     }
+
+class Animal extends Creature {
+    constructor(whoIs, gender, name, say) {
+        super(whoIs, gender, name, say)
+        this.legs = 4;
+    }
+}
+
+class Human extends Creature {
+    constructor(whoIs, gender, name, say) {
+        super(whoIs, gender, name, say)
+        this.hands = 2;
+        this.legs = 2;
+    }
 }
 
 
+let nick = new Human('human', 'male', 'Nick', 'Hellooooo...');
+let helen = new Human('human', 'female', 'Helen', 'Hi!');
 
-let nick = new Person('human', 'male', 'Nick','Hellooooo...',2 ,2, ['Kate', 'Jane']);
-let helen = new Person('human', 'female', 'Helen', 'Hi!', 2,2)
-
-let bars = new Creature('cat', 'female', 'Bars', 'Meuuuu', 4, 0);
-let skot = new Creature('dog', 'male', 'Skot', 'Auuuuu!', 4,0);
-
-// I think it`s nothing to change:)
+let bars = new Animal('cat', 'female', 'Bars', 'Meuuuu');
+let skot = new Animal('dog', 'male', 'Skot', 'Auuuuu!');
