e5491e28b886f44769f93d94fa901f2090777ae9
igor Kostiuk <ig.fire@gmail.com>
2018-12-29 05:10:47 +0200

    change logic after first  review

--- submissions/igkos/friends-app/index.js
+++ submissions/igkos/friends-app/index.js
@@ -1,6 +1,7 @@
 window.onload = () => {
   let data = [];
-  let sort = { text: ``, gender: `` };
+  let sort = { order: null, gender: null, text: null };
+  let sortedData = data;
   const cardContainer = document.querySelector(".container");
   fetch("https://randomuser.me/api/?results=50")
     .then(function(response) {
@@ -14,23 +15,25 @@ window.onload = () => {
         data = Array.from(users.results);
         //console.log(data);
         render(data);
+        sortedData = data;
       });
     })
     .catch(function(err) {
       console.log("Fetch Error :-S", err);
     });
+
   const render = data => {
     cardContainer.innerHTML = "";
     let fragment = document.createDocumentFragment();
-    data.forEach(arr => {
+    data.forEach(user => {
       let card = document.createElement("div");
-      card.classList.add(`card`, `${arr.gender}`);
+      card.classList.add(`card`, `${user.gender}`);
       card.innerHTML = `
-      <img class="gravatar" src=${arr.picture.large} alt=${arr.gender}>
-      <h3 >${arr.name.first}</h3><h3>${arr.name.last}</h3>
-      <h5 class="age">${arr.dob.age}</h5>
-      <p class="cell">${arr.cell}</p>
-      <p class="email">${arr.email}</p>`;
+      <img class="gravatar" src=${user.picture.large} alt=${user.gender}>
+      <h3 >${user.name.first}</h3><h3>${user.name.last}</h3>
+      <h5 class="age">${user.dob.age}</h5>
+      <p class="cell">${user.cell}</p>
+      <p class="email">${user.email}</p>`;
       fragment.appendChild(card);
     });
     cardContainer.appendChild(fragment);
@@ -39,54 +42,54 @@ window.onload = () => {
   const ascending = (a, b) => (a > b ? 1 : a < b ? -1 : 0);
   const descending = (a, b) => (b > a ? 1 : b < a ? -1 : 0);
 
-  const sortData = sortType => {
-    let sortedData = data;
-    switch (sortType) {
+  const orderingData = order => {
+    switch (order) {
       case "ascendingAge":
-        render(sortedData.sort((a, b) => ascending(a.dob.age, b.dob.age)));
+        sortedData = sortedData.sort((a, b) => ascending(a.dob.age, b.dob.age));
         break;
       case "descendingAge":
-        render(sortedData.sort((a, b) => descending(a.dob.age, b.dob.age)));
+        sortedData = sortedData.sort((a, b) =>
+          descending(a.dob.age, b.dob.age)
+        );
         break;
       case "ascendingFirstName":
-        render(
-          sortedData.sort((a, b) => ascending(a.name.first, b.name.first))
+        sortedData = sortedData.sort((a, b) =>
+          ascending(a.name.first, b.name.first)
         );
         break;
       case "descendingFirstName":
-        render(
-          sortedData.sort((a, b) => descending(a.name.first, b.name.first))
+        sortedData = sortedData.sort((a, b) =>
+          descending(a.name.first, b.name.first)
         );
-        break;
-      default:
-        render(data);
         break;
     }
   };
 
+  const filteringData = () => {
+    let filteredData = sortedData;
+    if (sort.text) {
+      filteredData = sortedData.filter(user =>
+        `${user.name.first}${user.name.last}${user.dob.age}${user.cell}${user.email}`.includes(sort.text)
+      );
+    }
+    if (sort.gender) {
+      filteredData = sortedData.filter(user => {
+        return user.gender === sort.gender;
+      });
+    }
+    render(filteredData);
+  };
+
   document.getElementById("nav").addEventListener("change", evt => {
-    if (evt.target.name === "order by") {
-      sortData(evt.target.value);
-    }
+    orderingData(evt.target.name === "order by" ? evt.target.value : ``);
     if (evt.target.name === "gender") {
-      sort.gender = evt.target.value;
+      sort.gender = evt.target.value ==="all" ? null: evt.target.value;
     }
-    dynamicSearch(sort);
+    filteringData();
   });
 
   document.getElementById("myInput").addEventListener("keyup", evt => {
     sort.text = evt.target.value;
-    dynamicSearch(sort);
+    filteringData();
   });
-
-  const dynamicSearch = sort => {
-    let filter = sort.text.toUpperCase();
-    let hideGender = (sort.gender === `male`)? `female`:(sort.gender === `female`)? `male` : null;
-    let cards = document.querySelectorAll(`.card`);
-    cards.forEach(card => {
-      card.textContent.toUpperCase().indexOf(filter) > -1 && !card.classList.contains(hideGender)
-        ? (card.style.display = "")
-        : (card.style.display = "none");
-    });
-  };
 };
