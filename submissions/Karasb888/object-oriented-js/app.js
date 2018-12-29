1d49cb0ac8bc2bb052c350df75a9ab4809c43c3a
karasb888 <Ivan.mazurenko99@gmail.com>
2018-12-30 00:42:19 +0200

    object-oriented-js(frogger) re OPP changes V2

--- submissions/Karasb888/object-oriented-js/app.js
+++ submissions/Karasb888/object-oriented-js/app.js
@@ -9,6 +9,7 @@ function setScore(){
   }else{
   table.innerHTML = 'Lives: ' + lives + ' | Wins: ' + wins;
   }
+
 };
 
 document.addEventListener('DOMContentLoaded', function(){
@@ -18,70 +19,90 @@ document.addEventListener('DOMContentLoaded', function(){
 
 });
 
-//Starting values
-const CANVAS_WIDTH = 505;
-const CANVAS_HEIGHT = 606;
-const ROW_HEIGHT = 83;
-const COLL_WIDTH = 101;
-const COLL_NUMB = CANVAS_HEIGHT / COLL_WIDTH;
-
-const INFO = {
-  player_start_x: CANVAS_WIDTH/2 - COLL_WIDTH/2,
-  player_start_y: CANVAS_HEIGHT - ROW_HEIGHT*2.5, 
+//MAIN constans
+var GAME_PARAMS = {
+  canvas_width: 505,
+  canvas_height: 606,
+  row_height: 83,
+  coll_width: 101,
+  coll_numb: 6,
+  player_start_x: 202,
+  player_start_y: 398
 };
 
+//sprites
+const SPRITES = {
+  player: 'images/char-boy.png',
+  enemy: 'images/enemy-bug.png'
+}
+//Generate random
 function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-//Enemy class
-var Enemy = function(row, player) {
-   //Initial location x and y
-    this.start();
-    this.y = (ROW_HEIGHT - 10) * row;
-    this.width = COLL_WIDTH;
-    this.height = ROW_HEIGHT;
-    this.row = COLL_NUMB - row;
-    this.sprite = 'images/enemy-bug.png';
-    this.player = player;
+//SUPERCLASS
+var Character = function(sprite, x, y){
+  this.sprite = sprite;
+  this.x = x;
+  this.y = y;
 };
 
+Character.prototype.render = function(){
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+//Enemy class
+var Enemy = function(row, player) {
+    Character.call(this, SPRITES.enemy , -GAME_PARAMS.coll_width , (GAME_PARAMS.row_height - 10) * row );
+    this.width = GAME_PARAMS.coll_width;
+    this.height = GAME_PARAMS.row_height;
+    this.row = GAME_PARAMS.coll_numb - row;
+    this.player = player;
+    this.start();
+};
+
+Enemy.prototype = Object.create(Character.prototype);
+
 Enemy.prototype.start = function() {
-  this.x = -COLL_WIDTH;
+  this.x = -GAME_PARAMS.coll_width;
   this.speed = getRandomArbitrary(1, 6);
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+Enemy.prototype.collision = function() {
+  //On collision with player
+  if(this.x  >= player.x - 70 && this.x - 70 <= player.x && this.row === player.row ){
+    this.player.start();
+    lives--;
+    setScore();
+  }
+  //when enemy out of game board
+  if(this.x + this.speed >= GAME_PARAMS.canvas_width + this.width){
+    this.start();
+  }
+};
+
 Enemy.prototype.update = function(dt) {
     this.x += this.speed;
-
-    if(this.x  >= player.x - 70 && this.x - 70 <= player.x && this.row === player.row ){
-      this.player.start();
-      lives--;
-      setScore();
-    }
-    if(this.x + this.speed >= CANVAS_WIDTH + this.width){
-      this.start();
-    }
+    this.collision();
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-var Player = function(x, y){
-    this.x = x;
-    this.y = y;
-    this.sprite = 'images/char-boy.png';
+//player class
+var Player = function(){
+    Character.call(this, SPRITES.player , GAME_PARAMS.player_start_x, GAME_PARAMS.player_start_y );
     this.start();
 };
 
-Player.prototype.update = function(){
+Player.prototype = Object.create(Character.prototype);
+
+Player.prototype.start = function() {
+  this.row = 1;
+  this.x = GAME_PARAMS.player_start_x;
+  this.y = GAME_PARAMS.player_start_y;
+}
+
+Player.prototype.update = function() {
+};
+
+Player.prototype.checkStatus = function() {
   if(this.row === 6){
     this.start();
     wins++;
@@ -89,47 +110,40 @@ Player.prototype.update = function(){
   }
 };
 
-Player.prototype.start = function() {
-  this.row = 1;
-  this.x = INFO.player_start_x;
-  this.y = INFO.player_start_y;
-}
-Player.prototype.render = function(){
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
 Player.prototype.handleInput = function(dir){
+
   switch(dir){
 
     case 'left':
       if(this.x > 0){
-        this.x -= COLL_WIDTH;
+        this.x -= GAME_PARAMS.coll_width;
       }
     break;
 
     case 'right':
-      if(this.x + COLL_WIDTH < CANVAS_WIDTH){
-        this.x += COLL_WIDTH;
+      if(this.x + GAME_PARAMS.coll_width < GAME_PARAMS.canvas_width){
+        this.x += GAME_PARAMS.coll_width;
       }
     break;
 
     case 'up':
-        this.y -= ROW_HEIGHT;
+        this.y -= GAME_PARAMS.row_height;
         this.row++;
     break;
 
     case 'down':
       if(this.row > 1){
-        this.y += ROW_HEIGHT;
+        this.y += GAME_PARAMS.row_height;
         this.row--;
       }
     break;
 
   };
+
+  this.checkStatus();
+
 };
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+
 var player = new Player();
 
 var allEnemies = [];
