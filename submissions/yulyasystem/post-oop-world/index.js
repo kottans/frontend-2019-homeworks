65d36ac921b6bc0e369e52a53d19cf45293a4ede
yulyasystem <yulyasystem@gmail.com>
2019-01-01 19:49:57 +0200

    make changes

--- submissions/yulyasystem/post-oop-world/index.js
+++ submissions/yulyasystem/post-oop-world/index.js
@@ -1,10 +1,10 @@
 class Entity {
-  constructor(species, name, gender, legs) {
+  constructor(species, name, gender, legs, saying) {
     this.species = species;
     this.name = name;
     this.gender = gender;
     this.legs = legs;
-    this.saying = ` My name is ${this.name}`;
+    this.saying = saying || `My name is ${this.name}`;
   }
   toString() {
     return [this.species, this.name, this.gender, this.legs, this.saying].join(
@@ -14,54 +14,49 @@ class Entity {
 }
 
 class Human extends Entity {
-  constructor(name, gender,saying) {
-    super("human", name, gender, 2  );
+  constructor(name, gender, saying) {
+    super("human", name, gender, 2, saying);
     this.hands = 2;
-    this.saying = saying;
-    
   }
   toString() {
-    return super.toString() + ` I say ${this.saying} I have ${this.hands} hands `;
+    return super.toString() + ` I have ${this.hands} hands `;
   }
 }
 
 class Animal extends Entity {
-  constructor(species, name, gender) {
-    super(species, name, gender, 4);
+  constructor(species, name, gender, saying) {
+    super(species, name, gender, 4, saying);
   }
   toString() {
-    return super.toString() + ` I have ${this.legs} legs`;
+    return super.toString();
   }
 }
 
 class Cat extends Animal {
   constructor(name, gender) {
-    super("cat", name, gender);
+    super("cat", name, gender, "meow");
   }
 }
 
 class Dog extends Animal {
   constructor(name, gender) {
-    super("dog", name, gender);
+    super("dog", name, gender, "woof");
   }
 }
 
 class CatWoman extends Human {
-  constructor(name, gender,saying) {
-    super(name, gender,saying);
+  constructor(name, gender, saying) {
+    super(name, gender, saying);
     this.species = "cat-women";
-  }
-  toString() {
-    return super.toString() + ` I am ${this.species} `;
   }
 }
 
 const objects = [
-  new Human("George", "male",'hello'),
-  new Human("Alisa", "female",'I love Kottans'),
+  new Human("George", "male", "hello"),
+  new Human("Alisa", "female", "I love Kottans"),
   new Cat("Velik", "male"),
   new Dog("Hugo", "male"),
-  new CatWoman("Hinata", "female","learn me ow")
+  new CatWoman("Hinata", "female", "hi")
 ];
 
 objects.forEach(obj => print(obj));
