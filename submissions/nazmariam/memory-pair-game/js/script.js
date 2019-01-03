8644d3494d9477e5977cd3644263706bd34c4367
nazmariam <fedirko.mary@gmail.com>
2019-01-03 12:25:03 +0200

    fixed bugs after review

--- submissions/nazmariam/memory-pair-game/js/script.js
+++ submissions/nazmariam/memory-pair-game/js/script.js
@@ -1,53 +1,79 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const cards = [
-        "images/1.png",
-        "images/2.png",
-        "images/3.png",
-        "images/4.png",
-        "images/5.png",
-        "images/6.png"
-    ];
-    cards.forEach(function (item) {
-        cards.push(item);
-    });
-    let newArr = cards.sort(function() { return 0.5 - Math.random() });
+    const cardsImg = [1,2,3,4,5,6];
     const main = document.querySelector('.main');
-    const back = document.querySelectorAll('.back');
 
-    for (let i=0; i<newArr.length; i++){
-            back[i].innerHTML = '<img src=' + cards[i] + '>';
+    let randomImg = [];
+    function createCards() {
+        let images = cardsImg;
+        let cardContainer = document.createDocumentFragment();
+        images = images.concat(images);
+        randomImg = images.sort(() => 0.5 - Math.random());
+        randomImg.forEach(function (item) {
+            let card = document.createElement('div');
+            card.classList.add('flip-container');
+            card.dataset.item = item;
+            // console.log(card.dataset);
+            card.innerHTML = `<div class="flipper"><div class="front"></div>
+                      <div class="back"></div></div>`;
+            cardContainer.append(card);
+        });
+        return cardContainer;
     }
-    main.addEventListener('click', (e) => {
+    function insertImg(){
+        const back = document.querySelectorAll('.back');
+
+        for (let i=0; i<randomImg.length; i++){
+            back[i].innerHTML = '<img src="images/' + randomImg[i] + '.png">';
+        }
+    }
+    function doActive({target}){
         let activeCards = document.querySelectorAll('.flip-container.active');
-        console.log(activeCards);
-        let card = e.target;
-        if(activeCards.length===2){
+
+        if(isAction(activeCards)){
             activeCards.forEach(function (card) {
                 card.classList.remove('active');
+
             })
         }
-        card.closest('.flip-container').classList.add('active');
-        activeCards = document.querySelectorAll('.flip-container.active');
+        target.closest('.flip-container').classList.add('active');
+    }
+    function isAction(activeCards){
+        let action = false;
         if(activeCards.length===2){
+            action = true;
+        }
+        return action;
+    }
+    function compare(){
+        let activeCards = document.querySelectorAll('.flip-container.active');
+        if(isAction(activeCards)){
             let etalon = '', same = false;
             activeCards.forEach(function (card) {
-                if (etalon === card.querySelector('img').src){
+                if (etalon === card.dataset.item){
                     same = true;
                 }
-                etalon = card.querySelector('img').src;
-                console.log(card.querySelector('img').src);
+                etalon = card.dataset.item;
             });
             if (same){
-                console.log(same);
+
                 activeCards.forEach(function (card) {
                     card.classList.add('hidden');
                 })
             }
         }
+    }
+    function showWinner(){
         let hiddenCards = document.querySelectorAll('.hidden');
-        if(hiddenCards.length===cards.length){
+        if(hiddenCards.length===randomImg.length){
             let win = document.querySelector('h1');
             win.classList.add('winner');
         }
+    }
+    main.appendChild(createCards());
+    insertImg();
+    main.addEventListener('click', ({ target }) => {
+        doActive({target});
+        compare();
+        showWinner();
     });
 });
