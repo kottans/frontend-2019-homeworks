a447854479702032ef62d59783a4e9b1652716f6
Oleksiy Derkach <oleksiy.derkach@perfectial.com>
2019-01-05 00:36:50 +0200

    Changes after PR review

--- submissions/olek557/memory-pair-game/script.js
+++ submissions/olek557/memory-pair-game/script.js
@@ -1,24 +1,65 @@
 // BOARD CLASS
-let Board = function(id) {
+const Board = function(squareSize) {
   this.sequence = [];
+  this.squareSize = squareSize;
+  this.size = squareSize * squareSize;
+  this.selectedCard = '';
+}
+
+Board.prototype.startGame = function() {
+  this.generateHTMLBoard();
+  this.generateDataBoard();
+  this.randomizeDataBoard();
+  this.addEventListenerToCards();
+}
+
+Board.prototype.generateHTMLBoard = function() {
+  let board = document.createElement('div');
+  board.classList.add('wrapper');
+  board.id = 'board';
+  for (let i = 0; i < this.size; i++) {
+    let cardWrapper = document.createElement('div'),
+        card = document.createElement('div'),
+        card__front = document.createElement('div'),
+        card__back = document.createElement('div');
+    card__front.classList.add('card__front');
+    card__back.classList.add('card__back');
+    card.classList.add('card');
+    card.setAttribute('data-cell', i);
+    card.appendChild(card__back);
+    card.appendChild(card__front);
+    cardWrapper.classList.add('card__wrapper');
+    cardWrapper.style.width = `calc( 100% / ${this.squareSize} - 20px)`;
+    cardWrapper.appendChild(card);
+    board.appendChild(cardWrapper);
+  }
+  document.body.appendChild(board);
   this.boardElement = document.getElementById('board');
 }
 
-Board.prototype.generate = function() {
-  for(i = 0; i < 8; i++) {
+Board.prototype.generateDataBoard = function() {
+  for (let i = 0; i < this.size / 2; i++) {
     this.sequence.push(new Card(i), new Card(i));
   }
-  this.sequence.sort(function() { return 0.5 - Math.random() });
+}
+
+Board.prototype.randomizeDataBoard = function() {
+  this.sequence.sort(() => { return 0.5 - Math.random() });
+}
+
+Board.prototype.addEventListenerToCards = function() {
   this.boardElement.addEventListener('click', event => {
     if(event.target.matches('.card__front')) {
       let cardElement = event.target.parentNode,
           index = cardElement.getAttribute('data-cell');
       cardElement.parentNode.classList.add('open');
       cardElement.querySelector('.card__back').innerHTML = this.sequence[index].value;
-      if(selectedCard) {
-        if(this.sequence[index].value == selectedCard.value) {
-          this.fixCards();
-          this.closeAllCards();
+      if(this.selectedCard) {
+        if(this.sequence[index].value == this.selectedCard.value) {
+          setTimeout(() => {
+            this.hideCards();
+            this.closeAllCards();
+          }, 600);
         }
         else {
           setTimeout(() => {
@@ -27,22 +68,22 @@ Board.prototype.generate = function() {
         }
       }
       else {
-        selectedCard = this.sequence[index];
+        this.selectedCard = this.sequence[index];
       }
     }
   });
 }
 
 Board.prototype.closeAllCards = function() {
-  selectedCard = '';
+  this.selectedCard = '';
   Array.from(document.getElementsByClassName('card__wrapper')).forEach(i => {
     i.classList.remove('open');
   });
 }
 
-Board.prototype.fixCards = function() {
+Board.prototype.hideCards = function() {
   Array.from(document.getElementsByClassName('card__wrapper open')).forEach(i => {
-    i.classList.add('fixed');
+    i.classList.add('hide');
   });
 }
 
@@ -52,6 +93,5 @@ let Card = function(value) {
 }
 
 // INIT
-let board = new Board(),
-    selectedCard = '';
-board.generate();
+const board = new Board(4);
+board.startGame();
