6539e165f8f7bd5366c3d2b28bb7c2688f601dff
Bohdan Kovalchuk <bohdan.babay@gmail.com>
2019-01-04 03:58:57 +0200

    Tried to implement object hierarchy

--- submissions/babayK0/OOP-Exercise/index.js
+++ submissions/babayK0/OOP-Exercise/index.js
@@ -7,28 +7,57 @@
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
+const MALE = 'male';
+const FEMALE = 'female';
 class Organism{
-   constructor(species,gender,name,legs,hands,saying,friends){
-   this.species=species;
-   this.gender=gender;
-   this.name=name;
-   this.legs=legs;
-   this.hands=hands;
-   this.saying=saying;
-   this.friends=undefined;
+   constructor(gender,name,saying,friends,species,legs,hands){
+      this.species=species;
+      this.gender=gender;
+      this.name=name;
+      this.saying=saying;
+      this.friends=null;
+      this.legs=legs;
+      this.hands=hands;
    }
-   get Info(){
-      return `I am ${this.species}-${this.gender}. My name is ${this.name}.I have ${this.legs} legs and ${this.hands} hands.`
-      +(this.friends==undefined ? '...oh, i haven\'t friends' : `My friends are ${this.friends}`)
-      +`.My favorite phrase is: <strong>${this.saying}</strong>`;
+   get info(){
+      return `I am ${this.species}-${this.gender}.My name is <strong>${this.name} </strong>.`
+         +(this.hands=='0' ? `I have not hands but have ${this.legs} legs!`:`I have ${this.hands} hands and ${this.legs} legs!`)
+         +(this.friends==null ? '...oh, i haven\'t friends:(':`My friends are ${this.friends}.`)
+         +`My favorite phrase is: <strong>${this.saying}</strong>`;
    }
 }
-
-let dog = new Organism('dog','male','Wolf','4','0','arf-aarf!');
-let cat = new Organism('cat','female','Serafina','4','0','meeeow!');
-let woman = new Organism('human','female','Oprah','2','2','Failure is another steppingstone to greatness.');
-let man = new Organism('human','male','John','2','2','Your well-being depends on your own decisions.');
-let catwoman = new Organism(woman.species+cat.species,woman.gender,'Vi','2','2',cat.saying);
+class Human extends Organism{
+   constructor(gender,name,saying,friends,species='human',legs='2',hands='2'){
+      super(gender,name,saying,friends,species,legs,hands);
+   }
+}
+class Animal extends Organism{
+   constructor(gender,name,saying,friends,species,legs='4',hands){
+      super(gender,name,saying,friends,species,legs,hands);
+   }
+}
+class Dog extends Animal{
+   constructor(gender,name,saying,friends,species='dog',legs,hands='0'){
+      super(gender,name,saying,friends,species,legs,hands);
+      this.saying='argf-argf!';
+   }
+}
+class Cat extends Animal{
+   constructor(gender,name,saying,friends,species='cat',legs,hands='0'){
+      super(gender,name,saying,friends,species,legs,hands);
+      this.saying='meeoow!';
+   }
+}
+class CatWoman extends Cat{
+   constructor(gender,name,saying,friends,species='cat-woman',legs='2',hands='2'){
+      super(gender,name,saying,friends,species,legs,hands);
+   }
+}
+let dog = new Dog(MALE,'Wolf');
+let cat = new Cat(FEMALE,'Serafina');
+let woman = new Human(FEMALE,'Oprah','Failure is another steppingstone to greatness.');
+let man = new Human(MALE,'John','Your well-being depends on your own decisions.');
+let catwoman = new CatWoman(FEMALE,'Vi');
 
 dog.friends = `${cat.name} and ${man.name}`;
 cat.friends = `${dog.name} and ${woman.name}`;
@@ -41,11 +70,11 @@ catwoman.friends = `${woman.name} and ${woman.friends}`
    Message can contain HTML markup. You may also tweak index.html and/or styles.css.
    However, please, REFRAIN from improving visuals at least until your code is reviewed
    so code reviewers might focus on a single file that is index.js.*/
-print(dog.Info);
-print(cat.Info);
-print(woman.Info);
-print(man.Info);
-print(catwoman.Info);
+print(dog.info);
+print(cat.info);
+print(woman.info);
+print(man.info);
+print(catwoman.info);
 /* Print examples:
    print('ABC');
    print('<strong>ABC</strong>');
