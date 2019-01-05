81f6c438174193402d6974d565c55f7391e87272
vv2529 <vv02529@gmail.com>
2019-01-05 12:03:22 +0200

    DOM task: separate content from markup
    Lists and text formatting tags are left, because they depend on the content itself

--- submissions/vv2529/js-dom/script.js
+++ submissions/vv2529/js-dom/script.js
@@ -32,8 +32,8 @@ const savedData = {};
 function displayData(title){
 	if(title == currentPage) return;
 	const data = savedData[title];
-	if(data.content){
-		main.innerHTML = data.content;
+	if(data){
+		main.innerHTML = data;
 		currentPage = title;
 	}
 	else showError();
@@ -45,7 +45,8 @@ function loadData(title, callback){
 	else fetch(`data/${title}.json`)
 		.then(response => response.json())
 		.then(data => {
-			savedData[title] = data;
+			// Now we save a string with ready content
+			savedData[title] = parseData(data);
 			callback(title);
 		}, () => {
 			showError(title);
@@ -53,7 +54,21 @@ function loadData(title, callback){
 }
 
 function showError(title){
-	main.innerHTML = `<h1>No content!</h1><p>No data could be displayed for <q>${title}</q> query, sorry :(</p>`;
+	main.innerHTML = parseData({
+		heading: 'No content!',
+		paragraphs: [
+			`No data could be displayed for <q>${title}</q> query, sorry :(`
+		]
+	});
+}
+
+function parseData(data){
+	if(!data) return;
+	let str = `<h1>${data.heading}</h1>`;
+	data.paragraphs.forEach(item => {
+		str += `<p>${item}</p>`;
+	});
+	return str;
 }
 
 /*
