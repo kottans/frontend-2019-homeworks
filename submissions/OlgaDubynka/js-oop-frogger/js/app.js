26815b7f50b683324c9e4a6c98ec383ef56bd4c1
OlgaFrontend <odnews10@gmail.com>
2019-01-05 22:14:20 +0200

    fix frogger-game

--- submissions/OlgaDubynka/js-oop-frogger/js/app.js
+++ submissions/OlgaDubynka/js-oop-frogger/js/app.js
@@ -1,3 +1,17 @@
+const BOARD_WIDTH = 505;
+const PLAYER_WIDTH = 60;
+const PLAYER_HEIGHT = 80;
+const PLAYER_X = 202;
+const PLAYER_Y = 405;
+const PLAYER_STEP_X = 102;
+const PLAYER_STEP_Y = 83;
+const ENEMY_LOCATION = [60, 145, 225];
+const ENEMY_START = -70;
+const DELAY_TIME = 800;
+const RANDOM_INDEX = 235;
+const DEFAULT_SPEED = 200;
+const BLOCK_WIDTH = 100;
+
 class Enemy {
     constructor (x, y, speed) {
         this.x = x;
@@ -13,17 +27,17 @@ class Enemy {
     update (dt) {
         this.x += this.speed * dt;
     
-        if (this.x > 505) {
-            this.x = -70;
-            this.speed = 100 + Math.floor(Math.random() * 235);
+        if (this.x > BOARD_WIDTH) {
+            this.x = ENEMY_START;
+            this.speed = BLOCK_WIDTH + Math.floor(Math.random() * RANDOM_INDEX);
         };
     
-        if (player.x < this.x + 80 &&
-            player.x + 80 > this.x &&
-            player.y < this.y + 60 &&
-            60 + player.y > this.y) {
-            player.x = 202;
-            player.y = 405;
+        if (player.x < this.x + PLAYER_HEIGHT &&
+            player.x + PLAYER_HEIGHT > this.x &&
+            player.y < this.y + PLAYER_WIDTH &&
+            PLAYER_WIDTH + player.y > this.y) {
+            player.x = PLAYER_X;
+            player.y = PLAYER_Y;
         };
     }
 
@@ -43,53 +57,55 @@ class Player {
     update (dt) {
         if (this.y < 0) {
             setTimeout(() => {
-                this.x = 202;
-                this.y = 405;
-            }, 800);
+                this.x = PLAYER_X;
+                this.y = PLAYER_Y;
+            }, DELAY_TIME);
         };
     }
 
     handleInput (keyPress) {
-        
-        if (keyPress == 'up' && this.y > 0) {
-            this.y -= 83;
-        };
-    
-        if (keyPress == 'down' && this.y < 405) {
-            this.y += 83;
-        };
-    
-        if (keyPress == 'right' && this.x < 405) {
-            this.x += 101;
-        };
-    
+
         if (keyPress == 'left' && this.x > 0) {
-            this.x -= 101;
+            this.x -= PLAYER_STEP_X;
+        };
+
+        if (keyPress == 'right' && this.x < PLAYER_Y) {
+            this.x += PLAYER_STEP_X;
         };
     
+        if (keyPress == 'up' && this.y > 0) {
+            this.y -= PLAYER_STEP_Y;
+        };
+
+        if (keyPress == 'down' && this.y < PLAYER_Y) {
+            this.y += PLAYER_STEP_Y;
+        };
     }
 }
 
 const allEnemies = [];
 
-const enemyLocation = [60, 145, 225];
-
-enemyLocation.forEach(function (locationY) {
-    enemy = new Enemy(0, locationY, 200);
+ENEMY_LOCATION.forEach(function (locationY) {
+    let enemy = new Enemy(0, locationY, DEFAULT_SPEED);
     allEnemies.push(enemy);
 });
 
-const player = new Player(202, 405);
+const player = new Player(PLAYER_X, PLAYER_Y);
 
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+
+    const allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    let key = e.keyCode;
+
+    if (key in allowedKeys) {
+        player.handleInput(allowedKeys[e.keyCode]);
+    }
 });
 
 
