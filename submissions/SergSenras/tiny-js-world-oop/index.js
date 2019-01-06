36c8305030626d933250c76f580481d7059e8079
serg <s.sliusarenko@fozzy.ua>
2019-01-06 13:54:57 +0200

    PR review changes

--- submissions/SergSenras/tiny-js-world-oop/index.js
+++ submissions/SergSenras/tiny-js-world-oop/index.js
@@ -8,41 +8,38 @@
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
 class Dweller {
-  constructor (species,name,gender,saying,friends) {
+  constructor (species,name,gender,legs,saying,friends) {
     this.species = species;
     this.name = name;
     this.gender = gender;
+    this.legs = legs;
     this.saying = saying;
     this.friends = friends;
   }
 
   getInfo() {
-    var str = '';
-    for (var key in this) {
-      str += key + ': ' + this[key] + '; ';
-    }
-    return str;
+    return ['species','name','gender','hands','legs','saying','friends']
+      .map( x => '<b>' + x + '</b>: ' + this[x] )
+      .join('; ');
   }
 }
 
 class Animal extends Dweller {
   constructor(species,name,gender,legs,saying,friends) {
-    super(species,name,gender,saying,friends);
-    this.legs = legs;
+    super(species,name,gender,legs,saying,friends);
   }
 }
 
 class Human extends Dweller {
-  constructor(species,name,gender,legs, hands,saying,friends) {
-    super(species,name,gender,saying,friends);
-    this.legs = legs;
+  constructor(name,gender,legs,hands,saying,friends) {
+    super('human',name,gender,legs,saying,friends);
     this.hands = hands;
   }
 }
 
 
-var man = new Human ('human','John','male',2,2,'Hi',['Toby', 'Selina Kyle']);
-var woman = new Human ('human','Mary','female',2,2,'Hello',['Mrs. Whiskerson', 'John']);
+var man = new Human ('John','male',2,2,'Hi',['Toby', 'Selina Kyle']);
+var woman = new Human ('Mary','female',2,2,'Hello',['Mrs. Whiskerson', 'John']);
 var dog = new Animal ('dog','Toby','male',4,'woof-woof!',['John']);
 var cat = new Animal ('cat','Mrs. Whiskerson','female',4,'meow',['Mary', 'Selina Kyle']);
 var Catwoman = new Human ('Catwoman','Selina Kyle','female',2,2,cat.saying,['John', 'Mrs. Whiskerson']);
