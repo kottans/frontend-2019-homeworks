8bdadcb78b68d672750ad44036fe3e268df2d327
webdevagent <44728742+webdevagent@users.noreply.github.com>
2019-01-07 00:41:27 +0200

    Add setResidentData overloading

--- submissions/webdevagent/advanced-oop/index.js
+++ submissions/webdevagent/advanced-oop/index.js
@@ -2,43 +2,50 @@
 /*Resident is a parent class and all future classes
 will inherit from this class*/
 class Resident {
-  constructor(species, name, gender, legs, hands, voice, friends) {
+  constructor(species, name, gender, voice, friends) {
     this.species = species;
     this.name = name;
     this.gender = gender;
-    this.legs = legs;
-    this.hands = hands;
     this.voice = voice;
     this.friends = friends;
+    this.residentData;
   }
-  showResidentInfo() {
-    let residentData = Object.values(this);
-    residentData = residentData.filter(num => num != 'paws');
-    print(residentData.join('; '));
+  setResidentData() {
+    this.residentData = Object.values(this);
+  }
+  getResidentInfo() {
+    this.setResidentData();
+    print(this.residentData.join('; '));
   }
 }
 class Animal extends Resident {
-  constructor(species, name, gender, legs, hands, voice, friends) {
-    super(species, name, gender, legs, hands, voice, friends);
+  constructor(species, name, gender, voice, friends) {
+    super(species, name, gender, voice, friends);
+    this.paws = 4;
+  }
+  setResidentData() {
+    super.setResidentData();
+    this.residentData.splice(3, 0, this.residentData.pop());
   }
 }
 class Human extends Resident {
-  constructor(species, name, gender, legs, hands, voice, friends) {
-    super(species, name, gender, legs, hands, voice, friends);
+  constructor(species, name, gender, voice, friends) {
+    super(species, name, gender, voice, friends);
+    this.legs = 2;
+    this.hands = 2;
   }
-}
-class Miracle extends Resident {
-  constructor(species, name, gender, paws, hands, voice, friends) {
-    super(species, name, gender, paws, hands, voice, friends);
+  setResidentData() {
+    super.setResidentData();
+    let limbs = this.residentData.splice(5, 2);
+    this.residentData.splice(3, 0, limbs[0], limbs[1]);
   }
 }
 //Defining residents objects
-const Dog = new Animal('dog', 'Princess', 'female', 4, 'paws', 'woof', 'Garry, Patrick, Wendy');
-const Cat = new Animal('cat', 'Garry', 'male', 4, 'paws', 'meow', 'Princess, Patrick, Wendy, Kitty');
-const Man = new Human('human', 'Patrick', 'male', 2, 2, 'How do you do?', 'Garry, Princess, Wendy, Kitty');
-const Woman = new Human('human', 'Wendy', 'female', 2, 2, 'Hello my friends', 'Garry, Patrick, Princess');
-const catWoman = new Miracle('human', 'Kitty', 'female', 2, 2, 'meow', 'Garry, Patrick');
+const Dog = new Animal('dog', 'Princess', 'female', 'woof', 'Garry, Patrick, Wendy');
+const Cat = new Animal('cat', 'Garry', 'male', 'meow', 'Princess, Patrick, Wendy, Kitty');
+const Man = new Human('human', 'Patrick', 'male', 'How do you do?', 'Garry, Princess, Wendy, Kitty');
+const Woman = new Human('human', 'Wendy', 'female', 'Hello my friends', 'Garry, Patrick, Princess');
 //Group residents object in Array for future manipulations
 const residents = [Dog, Cat, Man, Woman];
 //call showResidentInfo method for each residents object
-residents.forEach(num => num.showResidentInfo());
+residents.forEach(num => num.getResidentInfo());
