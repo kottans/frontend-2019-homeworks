445cc8858cc4fcd0e1c9e0b9deff51b37db9f13f
vv2529 <vv02529@gmail.com>
2019-01-06 13:50:50 +0200

    OOP: Frogger game - improve code

--- submissions/vv2529/frogger/js/app.js
+++ submissions/vv2529/frogger/js/app.js
@@ -4,87 +4,95 @@ var Entity = function(x, y){
 	this.y = y;
 }
 Entity.prototype.render = function(){
-	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	// Every instance of child class of Entity should have its sprite
+	if('sprite' in this){
+		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	}
 };
 
 // Enemies our player must avoid
-var Enemy = function(x, y, speed){
+var Enemy = function(x, y, speed, target){
 	Entity.call(this, x, y);
 	this.speed = speed;
+	this.target = target;
+	this.sprite = 'images/enemy-bug.png';
+	this.width = 98;
+	this.height = 66;
 };
 Enemy.prototype = Object.create(Entity.prototype);
-// I refuse to write `Enemy.prototype` on every single line!
-Enemy.prototype = Object.assign(Enemy.prototype, {
-	constructor: Enemy,
-	sprite: 'images/enemy-bug.png',
-	width: 98,
-	height: 66,
-	update: function(dt){
-		this.x += this.speed * dt;
+Enemy.prototype.constructor = Enemy;
+Enemy.prototype.update = function(dt){
+	this.x += this.speed * dt;
 
-		// Get an enemy back on track when it is gone
-		if(this.x > 800) this.restart();
+	this.checkOffscreen();
+	this.checkForCollision();
+}
 
-		// Check for collision
-		if((((this.x > player.x) && (this.x < player.x + player.width)) ||
-			((this.x + this.width > player.x) && (this.x + this.width < player.x + player.width))) &&
-			(((this.y > player.y) && (this.y < player.y + player.height)) ||
-			((this.y + this.height > player.y) && (this.y + this.height < player.y + player.height))))
-			player.restart('lose');
-	},
-	restart: function(){
-		this.x = -this.width;
-		this.y = random(3) * 80 + 60 // = 60, 140 or 220
-		this.speed = random(500, 100);
-	}
-});
+Enemy.prototype.restart = function(){
+	this.x = -this.width;
+	this.y = random(3) * 80 + 65 // = 65, 145 or 225 - rows on the road
+	this.speed = random(500, 100);
+}
+
+Enemy.prototype.checkOffscreen = function(){
+	if(this.x > 800) this.restart();
+}
+
+Enemy.prototype.checkForCollision = function(){
+	const t = this.target;
+	if((((this.x > t.x) && (this.x < t.x + t.width)) ||
+		((this.x + this.width > t.x) && (this.x + this.width < t.x + t.width))) &&
+		(((this.y > t.y) && (this.y < t.y + t.height)) ||
+		((this.y + this.height > t.y) && (this.y + this.height < t.y + t.height))))
+		t.restart('lose');
+}
 
 // Player class
 var Player = function(x, y){
 	Entity.call(this, x, y);
+	this.spawnX = x;
+	this.spawnY = y;
+	this.sprite = 'images/char-boy.png';
+	this.width = 67;
+	this.height = 77;
 }
 Player.prototype = Object.create(Entity.prototype);
-Player.prototype = Object.assign(Player.prototype, {
-	constructor: Player,
-	sprite: 'images/char-boy.png',
-	width: 67,
-	height: 77,
-	update: () => {},
-	handleInput: function(dir){
-		if(!dir) return;
-		const shifts = {
-			left: [-1,0],
-			up: [0,-1],
-			right: [1,0],
-			down: [0,1]
-		};
-		const shift = shifts[dir];
-		const newX = this.x + shift[0] * 100;
-		const newY = this.y + shift[1] * 85;
-		// Check if the player is within the screen; values were picked empirically
-		if(0 <= newX && newX <= 400) this.x = newX;
-		if(0 <= newY && newY <= 450) this.y = newY;
-		// Check if the player reached the water
-		if(newY < 0){
-			this.restart('win');
-		}
-	},
-	restart: function(msg){
-		console.log(msg+'!');
-		if(msg == 'win') getId('wins').textContent++;
-		else if(msg == 'lose') getId('losses').textContent++;
-
-		this.x = 200;
-		this.y = 400;
+Player.prototype.constructor = Player;
+Player.prototype.update = function(){};
+Player.prototype.handleInput = function(dir){
+	if(!dir) return;
+	const shifts = {
+		left: [-1,0],
+		up: [0,-1],
+		right: [1,0],
+		down: [0,1]
+	};
+	const shift = shifts[dir];
+	const newX = this.x + shift[0] * 100;
+	const newY = this.y + shift[1] * 85;
+	// Check if the player is within the screen; values were picked empirically
+	if(0 <= newX && newX <= 400) this.x = newX;
+	if(0 <= newY && newY <= 450) this.y = newY;
+	// Check if the player reached the water
+	if(newY < 0){
+		this.restart('win');
 	}
-});
+};
+Player.prototype.restart = function(msg){
+	console.log(msg+'!');
+	if(msg == 'win') getId('wins').textContent++;
+	else if(msg == 'lose') getId('losses').textContent++;
+
+	this.x = this.spawnX;
+	this.y = this.spawnY;
+}
 
 // Now instantiate your objects.
 const player = new Player(200, 400);
 const allEnemies = [
-	new Enemy(100, 60, 200),
-	new Enemy(0, 140, 100),
-	new Enemy(-100, 220, 400)
+	new Enemy(100, 65, 200, player),
+	new Enemy(0, 145, 100, player),
+	new Enemy(-100, 225, 400, player)
 ];
 
 /*
