5b3dbbbe7c44937569789d55f01b1536066baf68
Alex Nugda <alexnugda@gmail.com>
2019-01-06 22:38:05 +0200

    add share props and method to base class

--- submissions/AlexNugda/a-tiny-JS-world-oop/index.js
+++ submissions/AlexNugda/a-tiny-JS-world-oop/index.js
@@ -1,56 +1,55 @@
 class Inhabitant {
-  constructor(name, gender) {
+  constructor(name, gender, species, saying) {
     this.name = name;
     this.gender = gender;
+	this.species = species;
+	this.saying = saying || `Hello from ${this.name}`;
+  }
+  showInhabitant(){
+	  return `<strong>${this.species}</strong>, ${this.name}, ${this.gender}, saying: ${this.saying}, `;
   }
 }
 
 class Human extends Inhabitant {
-	constructor(name, gender){
-		super(name, gender);
-		this.species = "Human";
+	constructor(name, gender, species){
+		super(name, gender, species || "Human");
 		this.hands = 2;
 		this.legs = 2;
-		this.saying = `Hello from ${this.name}`;
+	}
+	showInhabitant(){
+		return super.showInhabitant() + `hands: ${this.hands}, legs: ${this.legs}`;
 	}
 }
 
 class Animal extends Inhabitant {
-	constructor(name, gender){
-		super(name, gender);
+	constructor(name, gender, species, saying){
+		super(name, gender, species, saying);
 		this.paws = 4;
+	}
+	showInhabitant(){
+		return super.showInhabitant() + `paws: ${this.paws}`;
 	}
 }
 
 class Cat extends Animal {
 	constructor(name, gender){
-		super(name, gender);
-		this.species = "Cat";
-		this.saying = "Meow";
+		super(name, gender, "Cat", "Meow");
 	}
 }
 
 class Dog extends Animal {
 	constructor(name, gender){
-		super(name, gender);
-		this.species = "Dog";
-		this.saying = "Woff";
+		super(name, gender, "Dog", "Woff");
 	}
 }
 
 class CatWoman extends Human {
 	constructor(name, gender){
-		super(name, gender);
-		this.species = "CatWoman";
+		super(name, gender, "CatWoman");
 		this.saying = new Cat().saying;
 	}
 }
 
 let inhabitants = [new Human("Alex", "male"), new Human("Jenny", "female"), new Cat("Murka", "female"), new Dog("Rex", "male"), new CatWoman("LadyCat", "female")];
 
-const showInhabitant = ( inhabitant ) => {
-	let{species, name, gender, hands, legs, paws, saying} = inhabitant;
-	return `<strong>${species}</strong>, ${name}, ${gender}, hands: ${hands || "0"}, legs: ${legs || "0"}, paws: ${paws || "0"}, saying: ${saying}`;
-}
-
-inhabitants.forEach(inhabitant => print(showInhabitant(inhabitant)));
+inhabitants.forEach(inhabitant => print(inhabitant.showInhabitant()));
