91b87890f691d400200085d63de2b4f907c8d00a
Roman Halenko <owlbro.popping@gmail.com>
2019-01-08 20:53:01 +0200

    Add js-post-oop task

--- submissions/Roman-Halenko/a-tiny-js-world/index.js
+++ submissions/Roman-Halenko/a-tiny-js-world/index.js
@@ -6,35 +6,55 @@
    */
 
 // ======== OBJECTS DEFINITIONS ========
+class Inhab {
+    constructor(nature, gender, name, says, legs, hands) {
+        this.nature = nature;
+        this.gender = gender;
+        this.name = name;
+        this.says = says;
+        this.legs = legs;
+        this.hands = hands;
+        this.friends = [];
+    }
 
-function createInhab(nature, gender, name, says, legs, hands, friendlyTo) {
-  return {
-    nature: nature,
-    gender: gender,
-    name: name,
-    says: says,
-    legs: legs,
-    hands: hands,
-    friendlyTo: friendlyTo.join(' and '),
-    about: about
-  }
+    addFriend() {
+        let arr = Array.prototype.slice.call(arguments);
+        arr.forEach(e => this.friends.push(e));
+    }
+
+    about() {
+        return [
+          `${this.name} is a `,
+          `${this.nature}. Sometime says: `,
+          `${this.says} Has `,
+          `${this.legs} legs, `,
+          `${this.hands} hands. Friendly with: `,
+          `${this.friends.map(e => e.name).join(', ')}. `,
+          `${this.gender}, as you may guess.`
+        ].join('');
+    }
 };
 
-const cat = createInhab('cat', 'female', 'Jessy', 'Wanna eat!', 4, 0, ['Justin']);
-const dog = createInhab('dog', 'male', 'Kim', 'Awa-waw!', 4, 0, ['Justin', 'Kate']);
-const woman = createInhab('woman', 'female', 'Kate', 'I need hugs!', 2, 2, ['Kim', 'Justin']);
-const man = createInhab('man', 'male', 'Justin', 'I love comics.', 2, 2, ['Kim', 'Jessy', 'Kate']);
-
-function about() {
-  return [
-    `${this.name} is a `,
-    `${this.nature}. Sometime says: `,
-    `${this.says} has `,
-    `${this.legs} legs, `,
-    `${this.hands} hands. Friendly with `,
-    `${this.friendlyTo}. `,
-    `${this.gender}, as you may guess.`
-  ].join('');
+class Human extends Inhab {
+    constructor(nature, gender, name, says, legs = 2, hands = 2) {
+        super(nature, gender, name, says, legs, hands);
+    }
 };
+
+class Animal extends Inhab {
+    constructor(nature, gender, name, says, legs = 4, hands = 0) {
+        super(nature, gender, name, says, legs, hands);
+    }
+};
+
+const cat = new Animal('cat', 'female', 'Jessy', 'Wanna eat!');
+const dog = new Animal('dog', 'male', 'Kim', 'Awa-waw!');
+const woman = new Human('woman', 'female', 'Kate', 'I need hugs!');
+const man = new Human('man', 'male', 'Justin', 'I love comics.');
+
+cat.addFriend(man);
+dog.addFriend(woman, man);
+man.addFriend(woman, dog, cat);
+woman.addFriend(man, dog);
 
 [cat, dog, man, woman].forEach( e => { print(e.about()) });
