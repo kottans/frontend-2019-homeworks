2fe21e80847644d76a3d41e74dbad0b097528a72
vv2529 <vv02529@gmail.com>
2019-01-08 21:00:51 +0200

    Memory pair - remove comments, `getId()` and run Prettier

--- submissions/vv2529/memory-pair/script.js
+++ submissions/vv2529/memory-pair/script.js
@@ -1,43 +1,33 @@
-/*
- * Helper functions
- */
+const arrayShuffle = arr => arr.sort(() => 0.5 - Math.random());
 
- getId = (id) => document.getElementById(id);
- arrayShuffle = (arr) => arr.sort(() => 0.5 - Math.random());
+const board = document.getElementById('board'),
+	modal = document.getElementById('modal'),
+	movesBestElem = document.getElementById('moves-best'),
+	movesCurrentElem = document.getElementById('moves-current'),
+	winMsg = document.getElementById('win-msg');
 
-/*
- * Events
- */
-
-const board = getId('board'),
-modal = getId('modal'),
-movesBestElem = getId('moves-best'),
-movesCurrentElem = getId('moves-current'),
-winMsg = getId('win-msg');
-
-board.addEventListener('click', function(event){
+board.addEventListener('click', function(event) {
 	const elem = event.target.closest('.tile');
-	if(!elem) return;
+	if (!elem) return;
 
 	checkForMatch(elem);
 });
-getId('btn-restart').addEventListener('click', function(){
+document.getElementById('btn-restart').addEventListener('click', function() {
 	modal.classList.add('hidden');
 	startGame();
 });
 
-/*
- * Main part
- */
-
 const images = ['1', '2', '5', '10', '25', '50'],
-boardWidth = 4, boardHeight = 3,
-flipDuration = 500, delayAfterFlip = 500,
-flipped = [];
+	boardWidth = 4,
+	boardHeight = 3,
+	flipDuration = 500,
+	delayAfterFlip = 500,
+	flipped = [];
 let matched = 0,
-movesBest = movesCurrent = 0;
+	movesBest = 0,
+	movesCurrent = 0;
 
-function startGame(){
+function startGame() {
 	matched = movesCurrent = 0;
 
 	let shuffled = arrayShuffle(formArray());
@@ -45,17 +35,17 @@ function startGame(){
 	board.append(...shuffled.map(img => makeTile(img)));
 }
 
-function formArray(){
+function formArray() {
 	// Assuming the board is 12 tiles
 	return images.concat(images);
 }
 
-function makeElem(className, tagName = 'div'){
+function makeElem(className, tagName = 'div') {
 	const elem = document.createElement(tagName);
 	elem.className = className;
 	return elem;
 }
-function makeTile(img){
+function makeTile(img) {
 	const tile = makeElem('tile');
 	const flipper = makeElem('flipper');
 	const front = makeElem('front');
@@ -71,19 +61,24 @@ function makeTile(img){
 	return tile;
 }
 
-function getImg(tile){
+function getImg(tile) {
 	return tile.querySelector('.tile-img').src;
 }
 
-function checkForMatch(elem){
-	if((flipped.length >= 2) || (flipped[0] == elem) || elem.classList.contains('matched')) return;
+function checkForMatch(elem) {
+	if (
+		flipped.length >= 2 ||
+		flipped[0] == elem ||
+		elem.classList.contains('matched')
+	)
+		return;
 	flipped.push(elem);
 	elem.classList.add('flipped');
 
-	if(flipped.length == 2){
+	if (flipped.length == 2) {
 		movesCurrentElem.textContent = ++movesCurrent;
 
-		if(getImg(flipped[0]) == getImg(flipped[1])){
+		if (getImg(flipped[0]) == getImg(flipped[1])) {
 			flipped.forEach(elem => {
 				elem.classList.add('matched');
 			});
@@ -91,34 +86,28 @@ function checkForMatch(elem){
 
 			matched++;
 			checkForWin();
-		}
-		else {
+		} else {
 			setTimeout(() => {
 				flipped.forEach(elem => {
 					elem.classList.remove('flipped');
 				});
 				flipped.length = 0;
-			}, flipDuration + delayAfterFlip)
+			}, flipDuration + delayAfterFlip);
 		}
 	}
 }
-function checkForWin(){
-	if(matched * 2 === boardWidth * boardHeight){
+function checkForWin() {
+	if (matched * 2 === boardWidth * boardHeight) {
 		modal.classList.remove('hidden');
 
 		let msg = '';
-		if(!movesBest || movesCurrent < movesBest){
+		if (!movesBest || movesCurrent < movesBest) {
 			movesBest = movesCurrent;
 			msg = 'New best!';
-		}
-		else msg = 'You win!';
+		} else msg = 'You win!';
 		winMsg.textContent = msg;
 		movesBestElem.textContent = movesBest;
 	}
 }
-
-/*
- * Initialization
- */
 
 startGame();
