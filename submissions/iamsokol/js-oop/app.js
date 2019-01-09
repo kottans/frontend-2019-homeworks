5498515de26b9b85857acd364c60641dca679caf
iamsokol <vladyslav.sokolov@lifecell.com.ua>
2019-01-09 21:21:05 +0200

    update app.js

--- submissions/iamsokol/js-oop/app.js
+++ submissions/iamsokol/js-oop/app.js
@@ -1,58 +1,59 @@
 let allEnemies = [];
 
-const ENEMIES_POSITION = [50, 130, 220],
-    WIDTH_PLAYER = 65,
-    HEIGHT_PLAYER = 75,
+const ENEMIES_POSITION = [60, 140, 220],
+    WIDTH_PLAYER = 60,
+    HEIGHT_PLAYER = 70,
     WIDTH_BLOCK = 150,
     WIDTH_FIELD = 500,
+    RANDOM_INDEX = 125,
+    COEFFICIENT = 100,
+    TIME_DELAY = 1000,
     PLAYER_X = 200,
-    PLAYER_Y = 380;
+    PLAYER_Y = 380,
+    PLAYER_STEP_X = 102,
+    PLAYER_STEP_Y = 83;
 
-const Enemy = function(x, y, player) {
+let Character = function(x,y,sprite){
     this.x = x;
     this.y = y;
+    this.sprite = sprite;
+};
+
+Character.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+let Enemy = function(x, y, player) {
+    Character.call(this, x, y, 'images/Gem Blue.png');
     this.player = player;
-    this.sprite = 'images/Gem Blue.png';
-    this.motion = WIDTH_BLOCK + Math.floor(Math.random() * 125);
+    this.motion = WIDTH_BLOCK + Math.floor(Math.random() * RANDOM_INDEX);
 };
 
-const Player = function(x, y, motion) {
-    this.x = x;
-    this.y = y;
+let Player = function(x, y, motion) {
+    Character.call(this, x, y, 'images/char-horn-girl.png');
     this.motion = motion;
-    this.sprite = 'images/char-horn-girl.png';
 };
+
+Player.prototype = Object.create(Character.prototype);
+Enemy.prototype = Object.create(Character.prototype);
+Player.prototype.constructor = Character;
+Enemy.prototype.constructor = Character;
 
 Enemy.prototype.update = function(dt) {
     this.x += this.motion * dt;
     if (this.x > WIDTH_FIELD) {
         this.x = -WIDTH_BLOCK;
     }
-    this.checkCollisions();
+    this.checkCollisions(player);
 }
 
 Enemy.prototype.checkCollisions = function(dt) {
-    let position1 = player.x < this.x + WIDTH_PLAYER,
-        position2 = player.x + WIDTH_PLAYER > this.x,
-        position3 = player.y < this.y + HEIGHT_PLAYER,
-        position4 = player.y + HEIGHT_PLAYER > this.y;
+    let position1 = this.player.x < this.x + WIDTH_PLAYER,
+        position2 = this.player.x + WIDTH_PLAYER > this.x,
+        position3 = this.player.y < this.y + HEIGHT_PLAYER,
+        position4 = this.player.y + HEIGHT_PLAYER > this.y;
     if (position1 && position2 && position3 && position4) {
-        player.restartPlayer();
-    };
-};
-
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-Player.prototype.update = function() {
-    if (this.x < 0) this.x = 0;
-    if (this.x > PLAYER_X * 2) this.x = PLAYER_X * 2;
-    if (this.y > PLAYER_Y) this.y = PLAYER_Y;
-    if (this.y < 0) {
-        setTimeout(()=> {
-            this.restartPlayer()
-        }, 1000)
+        this.player.restartPlayer();
     };
 };
 
@@ -61,31 +62,41 @@ Player.prototype.restartPlayer = function () {
     player.y = PLAYER_Y
 };
 
-Player.prototype.handleInput = function(pressedKey) {
-    switch (pressedKey) {
-        case 'left':
-            this.x -= this.motion + 50;
-            break;
-        case 'up':
-            this.y -= this.motion + 30;
-            break;
-        case 'right':
-            this.x += this.motion + 50;
-            break;
-        case 'down':
-            this.y += this.motion + 30;
-            break;
-    }
+Player.prototype.update = function() {
+    if (this.x < 0) this.x = 0;
+    if (this.x > PLAYER_X * 2) this.x = PLAYER_X * 2;
+    if (this.y > PLAYER_Y) this.y = PLAYER_Y;
 };
+
+Player.prototype.handleInput = function(pressedKey) {
+    if (pressedKey == 'left' && this.x > 0) {
+        this.x -= PLAYER_STEP_X;
+    };
+    if (pressedKey == 'right' && this.x < PLAYER_Y) {
+        this.x += PLAYER_STEP_X;
+    };
+    if (pressedKey == 'up' && this.y > 0) {
+        this.y -= PLAYER_STEP_Y;
+        if (this.y < 0) {
+            setTimeout(()=> {
+                alert("You win!");
+                this.restartPlayer()
+            }, TIME_DELAY)
+        };
+    };
+    if (pressedKey == 'down' && this.y < PLAYER_Y) {
+        this.y += PLAYER_STEP_Y;
+    };
+};
+
+const player = new Player(PLAYER_X, PLAYER_Y, 100);
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-let player = new Player(PLAYER_X, PLAYER_Y, 50);
-
 ENEMIES_POSITION.map((enemyCoordinate) => {
-	let enemy = new Enemy(0, enemyCoordinate, 100 + Math.floor(Math.random() * 125));
+	let enemy = new Enemy(0, enemyCoordinate, player);
 	allEnemies.push(enemy);
 });
 
@@ -96,6 +107,5 @@ document.addEventListener('keyup', function (e) {
 		39: 'right',
 		40: 'down'
 	};
-
 	player.handleInput(allowedKeys[e.keyCode]);
 });
