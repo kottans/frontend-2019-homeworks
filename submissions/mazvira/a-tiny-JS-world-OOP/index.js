f6aad1e90dc575960939596f4508d50eb0d5cc02
Vira Mazurkevych <mazvira@ukr.net>
2019-01-09 20:38:19 +0200

    Updated a-tiny-JS-world

--- submissions/mazvira/a-tiny-JS-world-OOP/index.js
+++ submissions/mazvira/a-tiny-JS-world-OOP/index.js
@@ -6,34 +6,55 @@
    */
 
 // ======== OBJECTS DEFINITIONS ========
-var Inhabitant = function (species, name, gender, legs, hands, saying, friends) {
+var Inhabitant = function (species, name, gender, legs, saying, friends) {
     this.species = species;
     this.name = name;
     this.gender = gender;
     this.legs = legs;
-    this.hands = hands;
     this.saying = saying;
     this.friends = friends;
 }
 
 Inhabitant.prototype.printInhabitants =  function() {
-    return [this.species, this.name, this.gender, this.legs, this.hands, this.saying, this.friends].join(" - ");
+    return [this.species, this.name, this.gender, this.legs, this.saying, this.friends].join(" - ");
 }
 
 var Human = function(name, gender, saying, friends){
-    var inhabitant = new Inhabitant('human', name, gender, 2, 2, saying, friends);
-    return inhabitant;
+    Inhabitant.apply(this, arguments);
+    this.species = 'human';
+    this.legs = 2;
+    this.hands = 2;
 }
+
+Human.prototype = Object.create(Inhabitant.prototype);
+Human.prototype.constructor = Human;
+Human.prototype.printInhabitants = function () {
+    return Inhabitant.prototype.printInhabitants.apply(this) + this.hands;
+}
+
+var FourLeggedAnimal = function(species, name, gender, saying, friends){
+    Inhabitant.apply(this, arguments);
+    this.legs = 4;
+}
+
+FourLeggedAnimal.prototype = Object.create(Inhabitant.prototype);
+FourLeggedAnimal.prototype.constructor = FourLeggedAnimal;
 
 var Dog = function(name, gender, saying, friends){
-    var inhabitant = new Inhabitant('dog', name, gender, 4, 0, saying, friends);
-    return inhabitant;
+    FourLeggedAnimal.apply(this, arguments);
+    this.species = 'dog';
 }
 
+Dog.prototype = Object.create(FourLeggedAnimal.prototype);
+Dog.prototype.constructor = Dog;
+
 var Cat = function(name, gender, saying, friends){
-    var inhabitant = new Inhabitant('cat', name, gender, 4, 0, saying, friends);
-    return inhabitant;
+    FourLeggedAnimal.apply(this, arguments);
+    this.species = 'cat';
 }
+
+Cat.prototype = Object.create(FourLeggedAnimal.prototype);
+Cat.prototype.constructor = Cat;
 
 var name = 'Toby';
 var gender = 'male';
