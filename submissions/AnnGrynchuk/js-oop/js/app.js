894d868f9add84ece31a619b68e5b1c40274b99f
ann grynchuk <ann.grynchuk7@gmail.com>
2019-01-10 13:47:17 +0200

    add checkCollisions function

--- submissions/AnnGrynchuk/js-oop/js/app.js
+++ submissions/AnnGrynchuk/js-oop/js/app.js
@@ -4,7 +4,7 @@ const playerHeight = 80;
 const moveX = 101;
 const moveY = 85;
 
-var Enemy = function(x, y, speed) {
+const Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
      this.x = x;
@@ -21,16 +21,18 @@ var Enemy = function(x, y, speed) {
 Enemy.prototype.update = function(dt) {
     this.x += this.speed * dt; 
 
-    if(this.x >500){
+    if(this.x >400){
       this.x = -30;
       this.speed = Math.floor(Math.random() * 200) + 70;
     };
+};
+
+Enemy.prototype.checkCollisions = function(){
 
     if( this.x + playerWidth > player.x && playerWidth + player.x > this.x && this.y + playerHeight > player.y && playerHeight + player.y > this.y ){
         player.x = 200;
         player.y = 400;
     }
-    
 };
 
 // Draw the enemy on the screen, required method for game
@@ -42,7 +44,7 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
-var Player = function(x, y) {
+const Player = function(x, y) {
     this.x = x;
     this.y = y;
     this.sprite = 'images/char-boy.png';
@@ -85,13 +87,13 @@ Player.prototype.handleInput = function (key){
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [];
-var enemy1 = new Enemy(50,80,100);
-var enemy2 = new Enemy(30,220,50);
+let allEnemies = [];
+const enemy1 = new Enemy(50,80,100);
+const enemy2 = new Enemy(30,220,50);
 
 allEnemies.push(enemy1, enemy2);
 
-var player = new Player(200, 400);
+const player = new Player(200, 400);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
