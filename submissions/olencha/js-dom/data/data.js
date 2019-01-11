2e588896a0910b138ea6a8c3913de9446932d402
olencha <chazova.olenk@gmail.com>
2019-01-11 15:38:14 +0200

    add js-dom task

---
+++ submissions/olencha/js-dom/data/data.js
@@ -0,0 +1,32 @@
+const booksData = [
+    {
+        title: 'Eloquent JavaScript',
+        author: 'Marijn Haverbeke',
+        description: 'Eloquent JavaScript goes beyond the cut-and-paste scripts of the recipe books and teaches you to write code that\'s elegant and effective. You\'ll start with the basics of programming, and learn to use variables, control structures, functions, and data structures. Then you\'ll dive into the real JavaScript artistry: higher-order functions, closures, and object-oriented programming. Along the way you\'ll learn to Master basic programming techniques and best practices Harness the power of functional and object-oriented programming Use regular expressions to quickly parse and manipulate strings Gracefully deal with errors and browser incompatibilities Handle browser events and alter the DOM structure Most importantly, Eloquent JavaScript will teach you to express yourself in code with precision and beauty. After all, great programming is an art, not a science—so why settle for a killer app when you can create a masterpiece?',
+        imgPath: 'img/eloquent_js.png'
+    },
+    {
+        title: 'You Don’t Know JS',
+        author: 'Kyle Simpson',
+        description: 'Even if you have been programming in JavaScript for a long time and have studied multiple versions of the JavaScript specifications, and followed the development of new versions of the specification closely, yet Kyle’s deep fascination with exploring all the little nooks and crannies manages to uncover many bits of the language that you had not yet discovered or explored. What you won’t find in this series is a lot of talk about software architecture, deep programmer wisdom, or an abundance of software design principles that will easily span many different languages. But it will certainly help you gain a new appreciation and a deeper understanding of JavaScript. Kyle’s singular focus on deep diving into language features is a unique treasure and it is one of the reasons that this book is listed in Top 5 JavaScript books our experts recommend.',
+        imgPath: 'img/you_dont_know_js.png'
+    },
+    {
+        title: 'Secrets of the JavaScript Ninja',
+        author: 'John Resig, Bear Bibeault, and Josip Maras',
+        description: 'Secrets of the JavaScript Ninja uses practical examples to clearly illustrate each core concept and technique. This completely revised edition shows you how to master key JavaScript concepts such as functions, closures, objects, prototypes, and promises. It covers APIs such as the DOM, events, and timers. You’ll discover best practice techniques such as testing, and cross-browser development, all taught from the perspective of skilled JavaScript practitioners. This book is highly popular in Simply library and almost all of our developers read it and recommended. This can give you trust that you need to consider this book whenever have time for reading.',
+        imgPath: 'img/secrets_of_the_javascript_ninja.png'
+    },
+    {
+        title: 'JavaScript: The Good Parts',
+        author: 'Douglas Crockford',
+        description: 'Most programming languages contain good and bad parts, but JavaScript has more than its share of the bad, having been developed and released in a hurry before it could be refined. This authoritative book scrapes away these bad features to reveal a subset of JavaScript that\'s more reliable, readable, and maintainable than the language as a whole--a subset you can use to create truly extensible and efficient code. With JavaScript: The Good Parts, you\'ll discover a beautiful, elegant, lightweight and highly expressive language that lets you create effective code, whether you\'re managing object libraries or just trying to get Ajax to run fast. If you develop sites or applications for the Web, this book is an absolute must.',
+        imgPath: 'img/javascript_the_good_parts.png'
+    },
+    {
+        title: 'The Principles of Object-Oriented JavaScript',
+        author: 'Nicholas C. Zakas ',
+        description: 'In The Principles of Object-Oriented JavaScript, Nicholas C. Zakas thoroughly explores JavaScript\'s object-oriented nature, revealing the language\'s unique implementation of inheritance and other key characteristics. he Principles of Object-Oriented JavaScript will leave even experienced developers with a deeper understanding of JavaScript. Unlock the secrets behind how objects work in JavaScript so you can write clearer, more flexible, and more efficient code.',
+        imgPath: 'img/the_principles_of_oojs.png'
+    }
+];
