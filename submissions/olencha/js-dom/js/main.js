752468b7d7848b580090517be09128ca129c9813
olencha <chazova.olenk@gmail.com>
2019-01-11 17:14:31 +0200

    fix changes requested

--- submissions/olencha/js-dom/js/main.js
+++ submissions/olencha/js-dom/js/main.js
@@ -1,31 +1,30 @@
 //select main fragments
 const nav = document.querySelector('#books-list'),
-    content = document.querySelector('#book-content');
+  content = document.querySelector('#book-content');
 
 //create parent container for menu items
 const booksList = document.createElement('UL');
 booksList.classList.add('books');
 
 for (let i = 0; i < booksData.length; i++) {
-    const listElement = document.createElement('LI'),
-        bookTitle = document.createTextNode(booksData[i].title);
-    listElement.dataset.index = i;
-    listElement.appendChild(bookTitle);
-    booksList.appendChild(listElement);
+  const listElement = document.createElement('LI'),
+    bookTitle = document.createTextNode(booksData[i].title);
+  listElement.classList.add('book-item');
+  listElement.dataset.index = i;
+  listElement.appendChild(bookTitle);
+  booksList.appendChild(listElement);
 }
 
 //add books list to nav tag and add 'active' class to first(default) element
 nav.appendChild(booksList).firstChild.classList.add('active');
 
 //create content
-const addContent = (book) => {
-    content.innerHTML = `
+const addContent = book => {
+  content.innerHTML = `
 <h2>${book.title}</h2>
 <p class="book-author">written by ${book.author}</p>
-<div>
 <p>${book.description}</p>
 <img src="${book.imgPath}" alt="book ${book.title}" class="book-img">
-</div>
 `;
 };
 
@@ -33,18 +32,18 @@ const addContent = (book) => {
 addContent(booksData[0]);
 
 //set another book by click
-nav.addEventListener('click', function (event) {
-    let navElement = event.target;
+nav.addEventListener('click', function(event) {
+  let navElement = event.target;
 
-    if (document.querySelector('.active')) {
-        document.querySelector('.active').classList.remove('active');
-    }
+  if (document.querySelector('.active')) {
+    document.querySelector('.active').classList.remove('active');
+  }
 
-    if (navElement.tagName != 'LI') {
-        return;
-    };
+  if (navElement.tagName != 'LI') {
+    return;
+  }
 
-    navElement.classList.add('active');
-    let book = booksData[navElement.dataset.index];
-    addContent(book);
+  navElement.classList.add('active');
+  let book = booksData[navElement.dataset.index];
+  addContent(book);
 });
