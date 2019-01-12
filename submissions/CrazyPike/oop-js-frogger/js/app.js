8ea21c6b75939881d61824234c7f4047d075b529
Serhiy Danylenko <serhiy.danylenko@gmail.com>
2019-01-12 16:13:05 +0200

    Frogger added #8

---
+++ submissions/CrazyPike/oop-js-frogger/js/app.js
@@ -0,0 +1,92 @@
+// Enemies our player must avoid
+var coordinatesYBugs = [60,145,230];
+var Enemy = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
+    this.speed = 100 + Math.floor(Math.random()*50);
+    this.y = coordinatesYBugs[Math.floor(Math.random()*3)];
+    this.x = -100;
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.x =+ this.x+dt*this.speed;
+    if (this.x > 600) {this.x = -100;
+                        this.y = coordinatesYBugs[Math.floor(Math.random()*3)];
+                      this.speed = 100 + Math.floor(Math.random()*50);}
+    if (Math.abs(this.x - player.x) < 50 && Math.abs(this.y - player.y) < 30) {
+        console.log('busted');
+        player.x = 200;
+        player.y = 300;
+        player.render();
+    }
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+
+var Player = function () {
+    this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y = 300;
+}
+
+Player.prototype.update = function () {
+    if (this.y <= 40) setTimeout(()=>{this.x = 200; this.y = 300;},500)
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function (key) {
+
+    switch (key) {
+        case 'up': {if (this.y>40) this.y -=80;
+                    break;}
+        case 'left': {if (this.x > 60)this.x -=100;
+            break;}
+        case 'down': {if (this.y < 360)this.y +=80;
+            break;}
+        case 'right': {if (this.x < 400)this.x +=100;
+            break;}
+    }
+    this.render();
+}
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+var allEnemies = [new Enemy(), new Enemy(), new Enemy(), new Enemy()]
+var player = new Player();
+
+
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
+
