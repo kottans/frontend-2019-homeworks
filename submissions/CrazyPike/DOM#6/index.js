eedee7002bc0dc1c1815b31b05ae2fbc77512325
Serhiy Danylenko <serhiy.danylenko@gmail.com>
2019-01-13 01:50:40 +0200

    Fixes added

--- submissions/CrazyPike/DOM#6/index.js
+++ submissions/CrazyPike/DOM#6/index.js
@@ -4,13 +4,14 @@ let menuItems = [{name:'topic1', text: 'text1'},
     {name:'topic3', text: 'text3'},
     {name:'topic4', text:   'text4'}];
 let elementMenu = document.getElementById('menu');
-elementMenu.innerHTML = menuItems.map((item,index) => `<div class="itemMenu" id="item${index}">${item.name}</div>`).join(' ');
+let menuList = menuItems.map((item,index) => `<li class="itemMenu" id="item${index}" itemNumber=${index}>${item.name}</li>`).join(' ');
+elementMenu.innerHTML = menuList;
 elementMenu.addEventListener('click', (event) =>{
     let target = event.target;
     if (!(target.className !== 'itemMenu' || target.className !== 'itemMenu active')) return;
     document.getElementById(`item${previousItem}`).className = 'itemMenu';
     target.className = 'itemMenu active';
-    previousItem = target.id.match(/\d+/g)[0];
-    document.getElementById('main').innerText = menuItems[target.id.match(/\d+/g)[0]]['text'];
+    previousItem = target.getAttribute('itemNumber');
+    document.getElementById('main').innerText = menuItems[previousItem]['text'];
 });
 document.getElementById('item0').click();
