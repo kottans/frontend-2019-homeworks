c5d6404c176893b6d623d9b91a1bc7eb81ebedfe
Oleksiy Derkach <oleksiy.derkach@perfectial.com>
2019-01-13 18:33:59 +0200

    Change after PR review

--- submissions/olek557/friends-app-task/common.js
+++ submissions/olek557/friends-app-task/common.js
@@ -1,25 +1,39 @@
 'use strict';
 
-const searchInput = document.getElementById('search'),
+const API_URL = 'https://randomuser.me/api/?results=200',
+      ASC = 'asc',
+      DESC = 'desc',
+      FEMALE = 'female',
+      MALE = 'male',
+      ALL_GENDERS = 'all',
+      container = document.getElementById('users-table'),
+      searchInput = document.getElementById('search'),
       filterAgeFromInput = document.getElementById('filter-user-age-from'),
       filterAgeToInput = document.getElementById('filter-user-age-to');
+
 let usersArr = [],
     currentUserArr = [],
     userModal = document.getElementById('user-info-modal'),
     modalOverflow = document.querySelector('.modal-overflow'),
     sortBtn = document.getElementById('sorting').getElementsByClassName('btn'),
-    sortUsersAscBtn = document.getElementById('sort-users-asc'),
-    sortUsersDescBtn = document.getElementById('sort-users-desc'),
-    sortUsersAgeAscBtn = document.getElementById('sort-users-age-asc'),
-    sortUsersAgeDescBtn = document.getElementById('sort-users-age-desc'),
+    sortUsersByNameAscBtn = document.getElementById('sort-users-asc'),
+    sortUsersByNameDescBtn = document.getElementById('sort-users-desc'),
+    sortUsersByAgeAscBtn = document.getElementById('sort-users-age-asc'),
+    sortUsersByAgeDescBtn = document.getElementById('sort-users-age-desc'),
     filterUsersByGenderMan = document.getElementById('filter-gender-man'),
-    filterUsersByGenderWoman = document.getElementById('filter-gender-woman');
+    filterUsersByGenderWoman = document.getElementById('filter-gender-woman'),
+    genderFilterParam = ALL_GENDERS,
+    ageFilterParam = {from: 0, to: 150},
+    nameSortParam = false,
+    ageSortParam = false,
+    searchKey = '';
 
-
-// GET USER LIST FUNCTION
 function getUserList() {
-  fetch('https://randomuser.me/api/?results=200')
+  fetch(API_URL)
     .then(function(response) {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
       return response.json();
      })
     .then(function(users) {
@@ -29,14 +43,14 @@ function getUserList() {
         userElem.generateElement();
         usersArr.push(userElem);
       });
-      generateUserTable(usersArr, '#users-table');
+      generateUserTable(usersArr);
       currentUserArr = usersArr;
     })
-    .catch(console.log);
+    .catch(function(error) {
+        console.log(error);
+    });
 }
 
-
-// USER CLASS
 let User = function(user, i) {
   this.id = i;
   this.fullName = `${user.name.first} ${user.name.last}`;
@@ -49,40 +63,30 @@ User.prototype.generateElement = function() {
       userInfo = document.createElement('div'),
       userName = document.createElement('div'),
       userAge = document.createElement('div');
-
-  user.classList.add('user');
-  user.classList.add(`user--${this.info.gender}`);
+  user.classList.add('user', `user--${this.info.gender}`);
   user.setAttribute('id', this.id);
   userImage.classList.add('user__image');
   userInfo.classList.add('user__info');
   userName.classList.add('user__name');
   userAge.classList.add('user__age');
-
   userImage.style.backgroundImage = `url(${this.info.picture.large})`;
-  userName.innerHTML = capitalizeFirstLetter(this.info.name.first) 
-                       + ' ' 
-                       + capitalizeFirstLetter(this.info.name.last);
+  userName.innerHTML = this.fullName;
   userAge.innerHTML = this.info.dob.age;
-  userInfo.appendChild(userName);
-  userInfo.appendChild(userAge);
-  user.appendChild(userImage);
-  user.appendChild(userInfo);
+  userInfo.append(userName, userAge);
+  user.append(userImage, userInfo);
   this.htmlElement = user;
 }
 
-
-// GENERATING HTML CONTENT FUNCTIONS
-function generateUserTable(users, parentElementSelector) {
-  const wrapperElement = document.querySelector(parentElementSelector);
-  wrapperElement.innerHTML = '';
+function generateUserTable(users) {
+  container.innerHTML = '';
   let userWrapperElement = document.createElement('div');
   userWrapperElement.classList.add('wrapper');
   users.forEach((user) => {
     userWrapperElement.appendChild(user.htmlElement);
   });
-  wrapperElement.appendChild(userWrapperElement);
-  wrapperElement.addEventListener('click', function(event) {
-    let target = checkParentTarget(this, event.target, '.user');
+  container.appendChild(userWrapperElement);
+  container.addEventListener('click', function(event) {
+    let target = event.target.closest('.user');
     if(target) {
       userModal.classList.add('open');
       generateModalInfo(target.id, userModal);
@@ -91,15 +95,23 @@ function generateUserTable(users, parentElementSelector) {
 }
 
 function generateModalInfo(userId, modal) {
-  let user = usersArr.filter((userObj) => {
+  let user = usersArr.find((userObj) => {
     return userObj.id == userId;
-  })[0].info;
+  }).info;
+  setInfoToModal(user, modal);
+  generateModalInfoRows(user);
+}
+
+function setInfoToModal(user, modal) {
   modal.querySelector('.modal__header').innerHTML = `${user.name.first} ${user.name.last}`;
   modal.querySelector('.modal__image').style.backgroundImage = `url(${user.picture.large})`;
   modal.querySelector('.modal__action--location').href =
     `https://maps.google.com/?q=${user.location.coordinates.latitude},${user.location.coordinates.longitude}`;
   modal.querySelector('.modal__action--email').href = `mailto:${user.email}`;
   modal.querySelector('.modal__action--phone').href = `tel:${user.phone}`;
+}
+
+function generateModalInfoRows(user) {
   let modalBodyElement = document.querySelector('.modal__body'),
       infoWrapper = document.createElement('div');
   infoWrapper.classList.add('info');
@@ -116,24 +128,19 @@ function generateModalInfo(userId, modal) {
     row.classList.add('info-row');
     title.classList.add('info-row__title');
     value.classList.add('info-row__value');
-    title.innerHTML = Object.keys(userInfo)[0];
-    value.innerHTML = userInfo[Object.keys(userInfo)[0]];
-    row.appendChild(title);
-    row.appendChild(value);
+    title.innerHTML = Object.entries(userInfo)[0][0];
+    value.innerHTML = Object.entries(userInfo)[0][1];
+    row.append(title, value);
     infoWrapper.appendChild(row);
   });
   modalBodyElement.innerHTML = '';
   modalBodyElement.appendChild(infoWrapper);
 }
 
-
-// MODAL
-modalOverflow.addEventListener('click', (event) => {
-  event.target.parentNode.classList.remove('open');
+modalOverflow.addEventListener('click', ({target}) => {
+  target.closest('.modal-wrapper').classList.remove('open');
 });
 
-
-// FILTERING AND SORTING FUNCTIONS
 function searchUser(userList, keyword) {
   return userList.filter((user) => {
     return user.info.name.first.toLowerCase().match(keyword.toLowerCase())
@@ -141,130 +148,168 @@ function searchUser(userList, keyword) {
   })
 }
 
-function filterUserList(userList, key, value){
-  return userList.filter((user) => {
-    return getObjectValueFromPath(user, key) == value;
-  });
+function filterUsersByGender(usersList, genderFilterParam) {
+  if (genderFilterParam != ALL_GENDERS) {
+    return usersList.filter((user) => {
+      return user.info.gender == genderFilterParam;
+    });
+  }
+  else {
+    return usersList;
+  }
 }
 
-function filterUsersByAge(userList, fromAge, toAge){
-  return userList.filter((user) => {
-    return user.info.dob.age >= fromAge && user.info.dob.age <= toAge;
+function filterUsersByAge(usersList, ageFilterParam) {
+  return usersList.filter((user) => {
+    return user.info.dob.age >= ageFilterParam.from && 
+           user.info.dob.age <= ageFilterParam.to;
   })
 }
 
-function sortUserList(userList, key, orderDirection){
-  return userList.sort((user1, user2) => {
-    if (getObjectValueFromPath(user1, key) > getObjectValueFromPath(user2, key)) {
-      return (orderDirection == 'asc') ? 1 : -1;
+function sortUsersByName(usersList, nameSortParam) {
+  return usersList.sort((user1, user2) => {
+    if (user1.fullName > user2.fullName) {
+      return (nameSortParam == ASC) ? 1 : -1;
     }
-    if (getObjectValueFromPath(user1, key) < getObjectValueFromPath(user2, key)) {
-      return (orderDirection == 'asc') ? -1 : 1;
+    if (user1.fullName < user2.fullName) {
+      return (nameSortParam == ASC) ? -1 : 1;
     }
   });
 }
 
-function genderFilter() {
-  if(filterUsersByGenderMan.checked && filterUsersByGenderWoman.checked
-     || !filterUsersByGenderMan.checked && !filterUsersByGenderWoman.checked) {
-    currentUserArr = usersArr;
-    generateUserTable(currentUserArr, '#users-table');
-  }
-  else if(filterUsersByGenderMan.checked && !filterUsersByGenderWoman.checked) {
-    currentUserArr = filterUserList(usersArr, 'info.gender', 'male');
-    generateUserTable(currentUserArr, '#users-table');
-  }
-  else if(!filterUsersByGenderMan.checked && filterUsersByGenderWoman.checked) {
-    currentUserArr = filterUserList(usersArr, 'info.gender', 'female');
-    generateUserTable(currentUserArr, '#users-table');
-  }
-}
-
-// ADD EVENT LISTENERS FOR BTNS
-sortUsersAscBtn.addEventListener('click', (event) => {
-  removeBtnActiveStates(sortBtn);
-  sortUsersAscBtn.classList.add('active');
-  let sortedUserTable = sortUserList(currentUserArr, 'fullName', 'asc');
-  generateUserTable(sortedUserTable, '#users-table');
-});
-
-sortUsersDescBtn.addEventListener('click', (event) => {
-  removeBtnActiveStates(sortBtn);
-  sortUsersDescBtn.classList.add('active');
-  let sortedUserTable = sortUserList(currentUserArr, 'fullName', 'desc');
-  generateUserTable(sortedUserTable, '#users-table');
-});
-
-sortUsersAgeAscBtn.addEventListener('click', (event) => {
-  removeBtnActiveStates(sortBtn);
-  sortUsersAgeAscBtn.classList.add('active');
-  let sortedUserTable = sortUserList(currentUserArr, 'info.dob.age', 'asc');
-  generateUserTable(sortedUserTable, '#users-table');
-});
-
-sortUsersAgeDescBtn.addEventListener('click', (event) => {
-  removeBtnActiveStates(sortBtn);
-  sortUsersAgeDescBtn.classList.add('active');
-  let sortedUserTable = sortUserList(currentUserArr, 'info.dob.age', 'desc');
-  generateUserTable(sortedUserTable, '#users-table');
-});
-
-searchInput.addEventListener('keyup', (event) => {
-  removeBtnActiveStates(sortBtn);
-  currentUserArr = searchUser(usersArr, event.target.value);
-  generateUserTable(currentUserArr, '#users-table')
-});
-
-filterAgeFromInput.addEventListener('input', (event) => {
-  removeBtnActiveStates(sortBtn);
-  let filteredArr = filterUsersByAge(usersArr, filterAgeFromInput.value, filterAgeToInput.value);
-  generateUserTable(filteredArr, '#users-table');
-});
-
-filterAgeToInput.addEventListener('input', (event) => {
-    removeBtnActiveStates(sortBtn);
-    let filteredArr = filterUsersByAge(usersArr, filterAgeFromInput.value, filterAgeToInput.value);
-    generateUserTable(filteredArr, '#users-table')
-});
-
-filterUsersByGenderMan.addEventListener('input', (event) => {
-  genderFilter();
-});
-filterUsersByGenderWoman.addEventListener('input', (event) => {
-  genderFilter();
-});
-
-
-// HELPERS
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-function getObjectValueFromPath(object, pathString) {
-  let path = pathString.split('.'),
-      value = object;
-  for (let i = 0; i < path.length; i++){
-    value = value[path[i]];
-  };
-  return value;
-}
-
-function checkParentTarget(parentElement, eventTarget, elementClass) {
-  let target = eventTarget;
-  while(target != parentElement) {
-    if(target.matches(elementClass)) {
-      return target;
+function sortUsersByAge(usersList, ageSortParam) {
+  return usersList.sort((user1, user2) => {
+    if (user1.info.dob.age > user2.info.dob.age) {
+      return (ageSortParam == ASC) ? 1 : -1;
     }
-    target = target.parentNode;
-  }
+    if (user1.info.dob.age < user2.info.dob.age) {
+      return (ageSortParam == ASC) ? -1 : 1;
+    }
+  });
 }
+
+sortUsersByNameAscBtn.addEventListener('click', () => {
+  if (nameSortParam == ASC) {
+    removeBtnActiveStates(sortBtn);
+    nameSortParam = false;
+  }
+  else {
+    nameSortParam = ASC;
+    removeBtnActiveStates(sortBtn);
+    sortUsersByNameAscBtn.classList.add('active');
+  }
+  allFilters();
+});
+
+sortUsersByNameDescBtn.addEventListener('click', () => {
+  if (nameSortParam == DESC) {
+    removeBtnActiveStates(sortBtn);
+    nameSortParam = false;
+  }
+  else {
+    nameSortParam = DESC;
+    removeBtnActiveStates(sortBtn);
+    sortUsersByNameDescBtn.classList.add('active');
+  }
+  allFilters();
+});
+
+sortUsersByAgeAscBtn.addEventListener('click', () => {
+  if (ageSortParam == ASC) {
+    removeBtnActiveStates(sortBtn);
+    ageSortParam = false;
+  }
+  else {
+    ageSortParam = ASC;
+    removeBtnActiveStates(sortBtn);
+    sortUsersByAgeAscBtn.classList.add('active');
+  }
+  allFilters();
+});
+
+sortUsersByAgeDescBtn.addEventListener('click', () => {
+  if (ageSortParam == DESC) {
+    removeBtnActiveStates(sortBtn);
+    ageSortParam = false;
+  }
+  else {
+    ageSortParam = DESC;
+    removeBtnActiveStates(sortBtn);
+    sortUsersByAgeDescBtn.classList.add('active');
+  }
+  allFilters();
+});
+
+searchInput.addEventListener('keyup', ({target}) => {
+  searchKey = target.value;
+  allFilters();
+});
+
+filterAgeFromInput.addEventListener('input', ({target}) => {
+  ageFilterParam.from = +target.value;
+  allFilters();
+});
+
+filterAgeToInput.addEventListener('input', ({target}) => {
+  ageFilterParam.to = +target.value;
+  allFilters();
+});
+
+filterUsersByGenderMan.addEventListener('input', () => {
+  const womanCheckboxChecked = filterUsersByGenderWoman.checked;
+  if (genderFilterParam == FEMALE) {
+    genderFilterParam = ALL_GENDERS;
+  }
+  else if (genderFilterParam == ALL_GENDERS && !womanCheckboxChecked) {
+    genderFilterParam = MALE;
+  }
+  else if (womanCheckboxChecked) {
+    genderFilterParam = FEMALE;
+  }
+  else {
+    genderFilterParam = ALL_GENDERS;
+  }
+  allFilters();
+});
+
+filterUsersByGenderWoman.addEventListener('input', (event) => {
+  const manCheckboxChecked = filterUsersByGenderMan.checked;
+  if (genderFilterParam == MALE) {
+    genderFilterParam = ALL_GENDERS;
+  }
+  else if (genderFilterParam == ALL_GENDERS && !manCheckboxChecked) {
+    genderFilterParam = FEMALE;
+  }
+  else if (manCheckboxChecked) {
+    genderFilterParam = MALE;
+  }
+  else {
+    genderFilterParam = ALL_GENDERS;
+  }
+  allFilters();
+});
 
 function removeBtnActiveStates(btnsArr) {
  Array.from(btnsArr).forEach((btn) => {
   btn.classList.remove('active');
- })
+ });
 }
 
+function allFilters() {
+  let currentUserArr = usersArr;
+  currentUserArr = filterUsersByGender(currentUserArr, genderFilterParam);
+  currentUserArr = filterUsersByAge(currentUserArr, ageFilterParam);
+  if (nameSortParam) {
+    currentUserArr = sortUsersByName(currentUserArr, nameSortParam);
+  }
+  if (ageSortParam) {
+    currentUserArr = sortUsersByAge(currentUserArr, ageSortParam);
+  }
+  if(searchKey) {
+    currentUserArr = searchUser(currentUserArr, searchKey);
+  }
+  generateUserTable(currentUserArr);
+}
 
 // INIT
 getUserList();
