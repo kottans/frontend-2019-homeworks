60707078533457783bd22014475f7a5227b22d98
Serhiy Danylenko <serhiy.danylenko@gmail.com>
2019-01-13 20:25:51 +0200

    Move all the numbers to consts, eliminate confusion with global vars

--- submissions/CrazyPike/oop-js-frogger/js/app.js
+++ submissions/CrazyPike/oop-js-frogger/js/app.js
@@ -1,23 +1,32 @@
 // Enemies our player must avoid
-const coordinatesYBugs = [60,145,230];
+const COORDINATES_Y_ENEMIES = [60,145,230];
 const LEFT_BORDER_CANVAS = -100;
 const RIGHT_BORDER_CANVAS = 600;
 const START_X_PLAYER = 200;
 const START_Y_PLAYER = 300;
 const STEP_X_PLAYER = 100;
 const STEP_Y_PLAYER = 80;
+const SPEED_ENEMY = 100;
+const SPEED_VARIANCE_ENEMY = 50;
+const COLLISION_DIFF_X = 50;
+const COLLISION_DIFF_Y = 30;
+const TOP_ROW_Y = 40;
+const BOTTOM_ROW_Y = 360;
+const LEFT_COLUMN_X = 60;
+const RIGHT_COLUMN_Y = 400;
 
-var Enemy = function(player) {
+
+var Enemy = function(gamer) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.speed = 100 + Math.floor(Math.random()*50);
-    this.y = coordinatesYBugs[Math.floor(Math.random()*3)];
+    this.speed = SPEED_ENEMY + Math.floor(Math.random()*SPEED_VARIANCE_ENEMY);
+    this.y = COORDINATES_Y_ENEMIES[Math.floor(Math.random()*COORDINATES_Y_ENEMIES.length)];
     this.x = LEFT_BORDER_CANVAS;
-    this.player = player;
+    this.player = gamer;
 };
 
 // Update the enemy's position, required method for game
@@ -27,15 +36,19 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x =+ this.x+dt*this.speed;
-    if (this.x > RIGHT_BORDER_CANVAS) { this.x = LEFT_BORDER_CANVAS;
-                        this.y = coordinatesYBugs[Math.floor(Math.random()*3)];
-                        this.speed = 100 + Math.floor(Math.random()*50);}
+    if (this.x > RIGHT_BORDER_CANVAS) {this.reset();}
     if (this.isCollision(this,this.player)) this.player.reset();
 
 };
 
-Enemy.prototype.isCollision = function(enemy,player){
-    return (Math.abs(enemy.x - player.x) < 50 && Math.abs(enemy.y - player.y) < 30)
+Enemy.prototype.reset = function(){
+    this.x = LEFT_BORDER_CANVAS;
+    this.y = COORDINATES_Y_ENEMIES[Math.floor(Math.random()*COORDINATES_Y_ENEMIES.length)];
+    this.speed = SPEED_ENEMY + Math.floor(Math.random()*SPEED_VARIANCE_ENEMY);
+}
+
+Enemy.prototype.isCollision = function(enemy,gamer){
+    return (Math.abs(enemy.x - gamer.x) < COLLISION_DIFF_X && Math.abs(enemy.y - gamer.y) < COLLISION_DIFF_Y)
 }
 
 // Draw the enemy on the screen, required method for game
@@ -54,7 +67,7 @@ var Player = function () {
 }
 
 Player.prototype.update = function () {
-    if (this.y <= 40) setTimeout(()=>{this.x = START_X_PLAYER; this.y = START_Y_PLAYER;},500)
+    if (this.y <= TOP_ROW_Y) setTimeout(()=>{this.x = START_X_PLAYER; this.y = START_Y_PLAYER;},500)
 }
 
 Player.prototype.render = function() {
@@ -69,16 +82,16 @@ Player.prototype.reset = function() {
 Player.prototype.handleInput = function (key) {
 
     switch (key) {
-        case 'up': {if (this.y > 40) this.y -= STEP_Y_PLAYER;
+        case 'up': {if (this.y > TOP_ROW_Y) this.y -= STEP_Y_PLAYER;
                     break;
                     }
-        case 'left': {if (this.x > 60) this.x -= STEP_X_PLAYER;
+        case 'left': {if (this.x > LEFT_COLUMN_X) this.x -= STEP_X_PLAYER;
                     break;
                     }
-        case 'down': {if (this.y < 360) this.y += STEP_Y_PLAYER;
+        case 'down': {if (this.y < BOTTOM_ROW_Y) this.y += STEP_Y_PLAYER;
                     break;
                     }
-        case 'right': {if (this.x < 400) this.x += STEP_X_PLAYER;
+        case 'right': {if (this.x < RIGHT_COLUMN_Y) this.x += STEP_X_PLAYER;
                     break;
                     }
     }
