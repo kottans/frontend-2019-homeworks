ddf1a26933177bc6eefdb99dcedcf3ba770f9d18
OlgaFrontend <odnews10@gmail.com>
2019-01-13 06:31:47 +0200

    fix friends-app

--- submissions/OlgaDubynka/friends-app/js/main.js
+++ submissions/OlgaDubynka/friends-app/js/main.js
@@ -4,6 +4,10 @@ const USER_LIST = document.querySelector('.user-list');
 const INPUT_NAME = document.querySelector('.search-by-name');
 const INPUT_AGE = document.querySelector('.search-by-age');
 const RESET = document.querySelector('.btn-reset');
+const INPUT_UP = document.querySelector('#up-name');
+const INPUT_DOWN = document.querySelector('#down-name');
+const INPUT_MALE = document.querySelector('#male');
+const INPUT_FEMALE = document.querySelector('#female');
 
 const ENDPOINT = 'https://randomuser.me/api/?';
 const RES = 'results=20';
@@ -26,33 +30,32 @@ const fetchUsers = () =>
 
 //render users
 const renderUsers = (data, parent) => {
-  let htmlString = '';
-  data.map(user => {
-    return htmlString += `<div class="user-list__item">
-                          <div class="user-card">
-                            <div class="user-card__img-wrap">
-                              <img  class="user-card__img" src="${user.img}" alt="user-photo">
-                            </div>
-                            <div class="user-card__name">${user.name}</div>
-                            <div class="user-card__btns">
-                              <button class="search-btn btn-flw btn-card">follow</button>
-                              <button class="search-btn btn-msg btn-card">message</button>
-                            </div>
-                            <div class="user-info">
-                              <p class="user-info__age">Age: ${user.age}</p>
-                              <p class="user-info__gender">Gender: ${user.gender}</p>
-                              <p class="user-info__location">City: ${user.location}</p>	
-                          </div>
-                          </div>
-                        </div>`
-  });
+  const htmlString = data.map(user => {
+    return `<div class="user-list__item">
+              <div class="user-card">
+                <div class="user-card__img-wrap">
+                  <img  class="user-card__img" src="${user.img}" alt="user-photo">
+                </div>
+                <div class="user-card__name">${user.name}</div>
+                <div class="user-card__btns">
+                  <button class="search-btn btn-flw btn-card">follow</button>
+                  <button class="search-btn btn-msg btn-card">message</button>
+                </div>
+                <div class="user-info">
+                  <p class="user-info__age">Age: ${user.age}</p>
+                  <p class="user-info__gender">Gender: ${user.gender}</p>
+                  <p class="user-info__location">City: ${user.location}</p>	
+              </div>
+              </div>
+            </div>`
+  }).join(" ");
   parent.innerHTML = htmlString;
 };
 
-const users = fetchUsers();
+let users;
 
-users.then(data => { 
-  const users = data.map(user => ({
+fetchUsers().then(data => { 
+  users = data.map(user => ({
       gender: user.gender,
       name: `${user.name.first} ${user.name.last}`,
       img: user.picture.thumbnail,
@@ -63,45 +66,47 @@ users.then(data => {
   );
 
   renderUsers(users, USER_LIST);
-
-  FORM_FILTER.addEventListener('click', function(e) {
-    const target = e.target;
-    const inputChecked = target.checked;
-    let getUsers = users;
-    let inputVal;
-    switch ((target === 'INPUT' !== null && inputChecked) || (target.classList.contains('search-btn'))) {
-      case target.value === 'up-age':
-        getUsers = users.sort((a, b) => a.age - b.age);
-        break;
-      case target.value === 'down-age':
-        getUsers = users.sort((a, b) => b.age - a.age);
-        break;
-      case target.value === 'up-name': 
-        getUsers = users.sort((a, b) => ((a.name > b.name) - (a.name < b.name)));
-        break;
-      case target.value === 'down-name':
-        getUsers = users.sort((a, b) => ((a.name < b.name) - (a.name > b.name)));
-        break;
-      case target.value === 'male' || target.value === 'female':
-        getUsers = users.filter(item => item.gender === target.value);
-        break;
-      case target.classList.contains('search-name-btn'):
-        inputVal = INPUT_NAME.value;
-        getUsers = users.filter(item => item.name.toLowerCase().includes(inputVal));
-        break;
-      case target.classList.contains('search-age-btn'):
-        inputVal = INPUT_AGE.value;
-        getUsers = users.filter(item => String(item.age) === inputVal);
-        break;
-      case target.classList.contains('btn-reset'):
-        INPUT_NAME.value = '';
-        INPUT_AGE.value = '';
-        [...INPUTS_FORM].forEach((item) => {
-          item.checked = false;
-        });
-        break;
-    }
-    renderUsers(getUsers, USER_LIST);
-  });
   
 });
+
+const makeSearch = ({ target }) => {
+  const inputChecked = target.checked;
+  let getUsers = users;
+  let inputVal;
+  switch ((target !== null && inputChecked) || (target.classList.contains('search-btn'))) {
+    case target.value === 'male' || target.value === 'female':
+      getUsers = users.filter(item => item.gender === target.value);
+      break;
+    case target.value === 'up-age':
+      getUsers = users.sort((a, b) => a.age - b.age);
+      break;
+    case target.value === 'down-age':
+      getUsers = users.sort((a, b) => b.age - a.age);
+      break;
+    case target.value === 'up-name': 
+      getUsers = users.sort((a, b) => ((a.name > b.name) - (a.name < b.name)));
+      break;
+    case target.value === 'down-name':
+      getUsers = users.sort((a, b) => ((a.name < b.name) - (a.name > b.name)));
+      break;
+    case target.classList.contains('search-name-btn'):
+      inputVal = INPUT_NAME.value;
+      getUsers = users.filter(item => item.name.toLowerCase().includes(inputVal));
+      break;
+    case target.classList.contains('search-age-btn'):
+      inputVal = INPUT_AGE.value;
+      getUsers = users.filter(item => String(item.age) === inputVal);
+      break;
+    case target.classList.contains('btn-reset'):
+      INPUT_NAME.value = '';
+      INPUT_AGE.value = '';
+      [...INPUTS_FORM].forEach((item) => {
+        item.checked = false;
+      });
+      break;
+  }
+
+  renderUsers(getUsers, USER_LIST);
+};
+
+FORM_FILTER.addEventListener('click', makeSearch);
