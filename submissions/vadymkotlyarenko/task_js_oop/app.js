a0a772a9912d63edfe99098cade9361a43112dd1
Vadym Kotlyarenko <vadymkotyarenko@gmail.com>
2019-01-14 22:36:52 +0200

    change after review

--- submissions/vadymkotlyarenko/task_js_oop/app.js
+++ submissions/vadymkotlyarenko/task_js_oop/app.js
@@ -1,4 +1,41 @@
-var Enemy = function(x, y, speed) {
+const PLAYER_START_X = 200;
+const PLAYER_START_Y = 400;
+const DISTANCE_FOR_LOSE_X = 70;
+const DISTANCE_FOR_LOSE_Y = 50;
+const END_GAME_FIELD_FOR_ENEMY = 650;
+const ENEMY_START_SPEED = 100;
+const PLAYER_STEP_LENGTH = 30;
+const PLAYER_GAME_FIELD = {
+    left : -40,
+    up : 450,
+    right : 450,
+    down : 40
+}
+const ENEMY_START_LIMITS = {
+    left : -400,
+    right : -100
+}
+const ENEMY_BOOST_LIMITS = {
+    min : 1,
+    max : 10
+}
+const ENEMYS_START_GAME_POSITION = {
+    first : {
+        x : -50,
+        y : 60
+    },
+    second : {
+        x : -150,
+        y : 140
+    },
+    third  : {
+        x : -100,
+        y : 230
+    }
+}
+
+
+let Enemy = function(x, y, speed) {
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
@@ -7,13 +44,13 @@ var Enemy = function(x, y, speed) {
 
 Enemy.prototype.update = function(dt) {
     this.x += dt * this.speed;    
-    if(Math.abs(this.x - player.x) < 70 && Math.abs(this.y - player.y) < 50) {
-        player.x = 200; 
-        player.y = 400;
+    if (Math.abs(this.x - player.x) < DISTANCE_FOR_LOSE_X && Math.abs(this.y - player.y) < DISTANCE_FOR_LOSE_Y) {
+        player.x = PLAYER_START_X; 
+        player.y = PLAYER_START_Y;
     }
-    if (this.x > 650) {
-        this.x = Math.floor(Math.random(-400, -100));
-        this.speed = this.speed * Math.floor(Math.round(1, 10));
+    if (this.x > END_GAME_FIELD_FOR_ENEMY) {
+        this.x = Math.floor(Math.random(ENEMY_START_LIMITS.left, ENEMY_START_LIMITS.right));
+        this.speed = this.speed * Math.floor(Math.round(ENEMY_BOOST_LIMITS.min, ENEMY_BOOST_LIMITS.max));
     }
 };
 
@@ -21,16 +58,17 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-var Player = function() {
+let Player = function() {
     this.sprite = 'images/char-boy.png';
-    this.x = 200;
-    this.y = 400;
+    this.x = PLAYER_START_X;
+    this.y = PLAYER_START_Y;
 };
 
 Player.prototype.update = function(dt) {
-    if (this.y <= 40  || this.y >= 450 || this.x <= -40  || this.x >= 440) {
-        this.x = 200; 
-        this.y = 400;
+    if (this.y <= PLAYER_GAME_FIELD.down  || this.y >= PLAYER_GAME_FIELD.right 
+        || this.x <= PLAYER_GAME_FIELD.left  || this.x >= PLAYER_GAME_FIELD.up) {
+            this.x = PLAYER_START_X; 
+            this.y = PLAYER_START_Y;
     }
 };
 
@@ -41,26 +79,28 @@ Player.prototype.render = function() {
 Player.prototype.handleInput = function(allowedKey) {
     switch (allowedKey){
         case 'left':
-            this.x -= 30;
+            this.x -= PLAYER_STEP_LENGTH;
             break;
         case 'up':
-            this.y -= 30;
+            this.y -= PLAYER_STEP_LENGTH;
             break;
         case 'right':
-            this.x += 30;
+            this.x += PLAYER_STEP_LENGTH;
             break;
         case 'down':
-            this.y += 30;
+            this.y += PLAYER_STEP_LENGTH;
             break;
     }
 };
 
 var player = new Player();
 
-var allEnemies = [new Enemy(-50, 60, 100), new Enemy(-150, 140, 100), new Enemy(-100, 230, 100)]
+var allEnemies = [new Enemy(ENEMYS_START_GAME_POSITION.first.x, ENEMYS_START_GAME_POSITION.first.y, ENEMY_START_SPEED), 
+    new Enemy(ENEMYS_START_GAME_POSITION.second.x, ENEMYS_START_GAME_POSITION.second.y, ENEMY_START_SPEED), 
+    new Enemy(ENEMYS_START_GAME_POSITION.third.x, ENEMYS_START_GAME_POSITION.third.y, ENEMY_START_SPEED)]
 
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    let allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
