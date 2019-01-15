49252133d9029c95535515817a6e0833b0a3534d
Vadym Kotlyarenko <vadymkotyarenko@gmail.com>
2019-01-15 20:02:35 +0200

    change getTextForPrint

--- submissions/vadymkotlyarenko/a-tiny-js-world/index.js
+++ submissions/vadymkotlyarenko/a-tiny-js-world/index.js
@@ -1,8 +1,4 @@
-function capitalizeFirstLetter(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-}
-
-function Inhabitant(species, name, gender, legs, hands, saying, friends) {
+function Inhabitant(species, name, gender, legs, hands, saying, ...friends) {
     this.species = species;
     this.name = name;
     this.gender = gender;
@@ -10,41 +6,34 @@ function Inhabitant(species, name, gender, legs, hands, saying, friends) {
     this.hands = hands;
     this.saying = saying;
     this.friends = friends;
-    this.getTextForPrint = function() {
-        var textToPrint = '';
-        for (const key in this) {
-            if (!this[key] || typeof(this[key]) === 'function') {
-                continue;
-            }
-            if (this[key] === friends) {
-                if (this[key] && this[key].length){
-                    textToPrint += capitalizeFirstLetter(key) + '=>';
-                    for (var index = 0; index < this[key].length; index++) {
-                        textToPrint +=  this[key][index].name + ','
-                    }
-                    textToPrint += ';';
-                } else {
-                    textToPrint += capitalizeFirstLetter(key) + '=>' + this[key].name + ';'
-                }
-                continue;
-            }
-            if (this[key] === saying) {
-                textToPrint += capitalizeFirstLetter(key) + '=>' + (typeof(this[key]) === 'object' ? this[key].saying : this[key]) + ';'
-                continue;
-            }
-            textToPrint += capitalizeFirstLetter(key) + '=>' + this[key] + ';'
-        }
-        return textToPrint;
-    };
 };
-  
-const dog = new Inhabitant('dog', 'Buddy','male', 4, 0 , 'woof-woof');
+
+Inhabitant.prototype.getTextForPrint = function() {
+    return [
+        `Species => ${this.species}`,
+        `Name => ${this.name}`,
+        `Gender => ${this.gender}`,
+        `Legs => ${this.legs}`,
+        `Hands => ${this.hands}`,
+        `Saying => ${typeof(this.saying) === "object" ? this.saying.saying : this.saying}`,
+        `Friends => ${this.friends.length > 0 ? this.getFriendsName() : "No one"}`
+    ].join("; ")
+};
+Inhabitant.prototype.getFriendsName = function() {
+    let names = "";
+    this.friends.forEach(friend => {
+        names += friend.name + ", ";
+    });
+    return names;
+};
+
+const dog = new Inhabitant("dog", "Buddy", "male", 4, 0, "woof-woof!");
 print(dog.getTextForPrint());
-const cat = new Inhabitant('cat', 'Garfield','male', 4, 0 , 'mew', dog);
+const cat = new Inhabitant("cat", "Garfield", "male", 4, 0, "meu", dog);
 print(cat.getTextForPrint());
-const man = new Inhabitant('human', 'Jonathan','male', 2, 2 , 'Hello!',[dog,dog]);
+const man = new Inhabitant("human", "Jonathan", "male", 2, 2, "Hello!", dog, cat);
 print(man.getTextForPrint());
-const woman = new Inhabitant('human', 'Megan','female', 2, 2 , 'Hi!');
+const woman = new Inhabitant("human", "Megan", "female", 2, 2, "Hi!");
 print(woman.getTextForPrint());
-const catWomen = new Inhabitant('cat', 'Joe','female', 4, 0 , cat, dog);
+const catWomen = new Inhabitant("cat", "Jane", "female", 4, 0, cat);
 print(catWomen.getTextForPrint());
