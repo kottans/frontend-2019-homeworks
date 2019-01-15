6ed9ed59f83a7d0f8ff0b2057bd0a0755f9d5b59
Vadym Kotlyarenko <vadymkotyarenko@gmail.com>
2019-01-15 20:10:51 +0200

    change using es6 classes

--- submissions/vadymkotlyarenko/OOP-exercise/index.js
+++ submissions/vadymkotlyarenko/OOP-exercise/index.js
@@ -1,109 +1,77 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
+class Inhabitant {
+    constructor(species, name, gender, legs, hands, ...friends) {
+        this.species = species;
+        this.name = name;
+        this.gender = gender;
+        this.legs = legs;
+        this.hands = hands;
+        this.friends = friends;
+    }
+    get textToPrint() {
+        return [
+            `Species => ${this.species}`,
+            `Name => ${this.name}`,
+            `Gender => ${this.gender}`,
+            `Legs => ${this.legs}`,
+            `Hands => ${this.hands}`,
+            `Friends => ${this.friends.length > 0 ? this.getFriendsName(this.friends) : "No one"}`,
+        ].join("; ")
+    }
+    getFriendsName(friends) {
+        let names = "";
+        friends.forEach(friend => {
+            names += friend.name + ", ";
+        });
+        return names;
+    }
+};
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
-
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
-
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
-
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
-
-function capitalizeFirstLetter(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
+class Human extends Inhabitant {
+    constructor(species, name, gender, legs, hands, saying, ...friends) {
+        super(species, name, gender, legs, hands, ...friends)
+        this.saying = saying;
+    }
+    get textToPrint() {
+        return [
+            super.textToPrint,
+            `Saying => ${this.saying}`
+        ].join("; ")
+    }
 }
 
-var Inhabitant = function (species, name, gender, legs, hands, saying, friends) {
-    this.species = species;
-    this.name = name;
-    this.gender = gender;
-    this.legs = legs;
-    this.hands = hands;
-    this.saying = saying;
-    this.friends = friends;
-};
-
-Inhabitant.prototype.printInformation = function() {
-    var textToPrint = '';
-    for (const key in this) {
-        if (!this[key] || typeof(this[key]) === 'function') {
-            continue;
-        }
-        if (key === 'friends') {
-            if (this[key] && this[key].length){
-                textToPrint += capitalizeFirstLetter(key) + '=>';
-                for (var index = 0; index < this[key].length; index++) {
-                    textToPrint +=  this[key][index].name + ','
-                }
-                textToPrint += ';';
-            } else {
-                textToPrint += capitalizeFirstLetter(key) + '=>' + this[key].name + ';'
-            }
-            continue;
-        }
-        if (key === 'saying') {
-            textToPrint += capitalizeFirstLetter(key) + '=>' + (typeof(this[key]) === 'object' ? this[key].saying : this[key]) + ';'
-            continue;
-        }
-        textToPrint += capitalizeFirstLetter(key) + '=>' + this[key] + ';'
+class Dog extends Inhabitant {
+    constructor(species, name, gender, legs, hands, saying, ...friends) {
+        super(species, name, gender, legs, hands, ...friends)
+        this.saying = saying;
     }
-    print(textToPrint);
-};
-  
-const dog = new Inhabitant('dog', 'Buddy','male', 4, 0 , 'woof-woof');
-dog.printInformation();
-const cat = new Inhabitant('cat', 'Garfield','male', 4, 0 , 'mew', dog);
-cat.printInformation();
-const man = new Inhabitant('human', 'Jonathan','male', 2, 2 , 'Hello!',[dog,dog]);
-man.printInformation();
-const woman = new Inhabitant('human', 'Megan','female', 2, 2 , 'Hi!');
-woman.printInformation();
-const catWomen = new Inhabitant('cat', 'Joe','female', 4, 0 , cat, dog);
-catWomen.printInformation();
+    get textToPrint() {
+        return [
+            super.textToPrint,
+            `Saying => ${this.saying}`
+        ].join("; ")
+    }
+}
 
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
+class Cat extends Inhabitant {
+    constructor(species, name, gender, legs, hands, saying, ...friends) {
+        super(species, name, gender, legs, hands, ...friends)
+        this.saying = saying;
+    }
+    get textToPrint() {
+        return [
+            super.textToPrint,
+            `Saying => ${typeof(this.saying) === "object" ? this.saying.saying : this.saying}`
+        ].join("; ")
+    }
+}
 
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-
-
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-
-
+const dog = new Dog("dog", "Buddy", "male", 4, 0, "woof-woof!");
+print(dog.textToPrint);
+const cat = new Cat("cat", "Garfield", "male", 4, 0, "meu", dog);
+print(cat.textToPrint);
+const man = new Human("human", "Jonathan", "male", 2, 2, "Hello!", dog, cat);
+print(man.textToPrint);
+const woman = new Human("human", "Megan", "female", 2, 2, "Hi!");
+print(woman.textToPrint);
+const catWomen = new Cat("cat", "Jane", "female", 4, 0, cat);
+print(catWomen.textToPrint);
