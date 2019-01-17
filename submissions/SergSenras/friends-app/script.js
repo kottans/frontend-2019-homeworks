8bab6e69148dfd8ff2d9f0a138c4ec59c4ac527e
serg <s.sliusarenko@fozzy.ua>
2019-01-17 15:51:05 +0200

    changes according to review

--- submissions/SergSenras/friends-app/script.js
+++ submissions/SergSenras/friends-app/script.js
@@ -5,23 +5,22 @@ const URL = 'https://randomuser.me/api/?results=40';
 
 let persons = [];
 let displayList = [];
-let searchStr = '';
 
-async function loadXMLDoc() {
-  let response = await fetch(URL)
-                  .then(data => data.json())
-                  .then(function(data){
-                    persons = data.results;
-                    displayList = persons;
-                    displayCards();
-                  })
-                  .catch(function(error){
-                    console.log('error');
-                  })
+function getPeopleData() {
+	let response = fetch(URL)
+		.then(data => data.json())
+		.then(function (data) {
+			persons = data.results;
+			displayList = persons;
+			displayCards();
+		})
+		.catch(function (error) {
+			console.log('error');
+		})
 }
 
 function getTemplate(el) {
-  return `<div class="card">
+	return `<div class="card">
             <div class="picture">
               <img src="${el.picture.large}">
             </div>
@@ -35,54 +34,55 @@ function getTemplate(el) {
 }
 
 function displayCards() {
-  CONTAINER.innerHTML = '';
-  let cardMarkup = '';
-  cardMarkup += displayList.reduce((accumulator, currentValue) => accumulator.concat(getTemplate(currentValue)), '');
-  CONTAINER.insertAdjacentHTML('afterbegin',cardMarkup);
+	CONTAINER.innerHTML = '';
+	let cardMarkup = '';
+	cardMarkup += displayList.reduce((accumulator, currentValue) => accumulator.concat(getTemplate(currentValue)), '');
+	CONTAINER.insertAdjacentHTML('afterbegin', cardMarkup);
 }
 
-document.addEventListener('DOMContentLoaded', function(){  
-  loadXMLDoc();
+document.addEventListener('DOMContentLoaded', function () {
+	getPeopleData();
 
-  SIDEBAR.addEventListener('click', function(event){
-    let radio = event.target.closest('.rdBtn');
-    let btn = event.target.closest('button');
-    if(radio){
-      switch(radio.id){
-        case 'genderAll':
-          displayList = persons;
-          break;
-        case 'genderMale':
-          displayList = persons.filter( el => el.gender == 'male' );
-          break;
-        case 'genderFemale':
-          displayList = persons.filter( el => el.gender == 'female' );
-          break;
-        case 'nameAsc':
-          displayList = displayList.sort((b, a) => a.name.last > b.name.last ? -1 : 1);
-          break;
-        case 'nameDesc':
-          displayList = displayList.sort((b, a) => a.name.last < b.name.last ? -1 : 1);
-          break;
-        case 'ageLow':
-          displayList = displayList.sort((a, b) => a.dob.age - b.dob.age);
-          break;
-        case 'ageHigh':
-          displayList = displayList.sort((b, a) => a.dob.age - b.dob.age);
-          break;
-      }
-    }
-    else if(btn){
-      displayList = persons;
-    }
-    displayCards();
-  })
+	SIDEBAR.addEventListener('click', function (event) {
+		let radio = event.target.closest('.rdBtn');
+		let btn = event.target.closest('button');
+		if (radio) {
+			switch (radio.id) {
+				case 'genderAll':
+					displayList = persons;
+					break;
+				case 'genderMale':
+					displayList = persons.filter(el => el.gender == 'male');
+					break;
+				case 'genderFemale':
+					displayList = persons.filter(el => el.gender == 'female');
+					break;
+				case 'nameAsc':
+					displayList = displayList.sort((b, a) => a.name.last > b.name.last ? -1 : 1);
+					break;
+				case 'nameDesc':
+					displayList = displayList.sort((b, a) => a.name.last < b.name.last ? -1 : 1);
+					break;
+				case 'ageLow':
+					displayList = displayList.sort((a, b) => a.dob.age - b.dob.age);
+					break;
+				case 'ageHigh':
+					displayList = displayList.sort((b, a) => a.dob.age - b.dob.age);
+					break;
+			}
+		}
+		else if (btn) {
+			displayList = persons;
+		}
+		displayCards();
+	})
 
-  SEARCH_FIELD.addEventListener('keyup', function(event){
-    searchStr = SEARCH_FIELD.value;
-    displayList = persons.filter(el => el.name.last.indexOf(searchStr) != -1 || el.name.first.indexOf(searchStr) != -1)
-    displayCards();
-  })
+	SEARCH_FIELD.addEventListener('input', function (event) {
+		let searchStr = '';
+		searchStr = SEARCH_FIELD.value.trim();
+		displayList = persons.filter(el => el.name.last.includes(searchStr));
+		displayCards();
+	})
 });
 
 
