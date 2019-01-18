6e02754d42202441477883071cb0e066cbe416b7
Peter Trofimov <ptrofimov7@gmail.com>
2019-01-18 21:12:02 +0200

    Fixed bugs

--- submissions/patrofimov/memory-pair-game/js/script.js
+++ submissions/patrofimov/memory-pair-game/js/script.js
@@ -1,92 +1,113 @@
-(function () {
-    const cards = [
-    "img/head001.png",
-    "img/head002.png",
-    "img/head003.png",
-    "img/head004.png",
-    "img/head005.png",
-    "img/head006.png",
-    "img/head001.png",
-    "img/head002.png",
-    "img/head003.png",
-    "img/head004.png",
-    "img/head005.png",
-    "img/head006.png"
-  ];
+let makeElement = (tagName, className, textInfo, srcInfo) => {
+  let element = document.createElement(tagName);
+  element.classList.add(className);
+  if (textInfo) {
+    element.textContent = textInfo;
+  }
+  if (srcInfo) {
+    element.src = srcInfo;
+  }
+  return element;
+};
+let createContent = item => {
+  let content = makeElement("div", "flip-box");
 
-    const main = document.getElementById("parent");
+  let flipper = makeElement("div", "flipper");
 
-    function SortCards() {
-        cards.sort(function () {
-            return 0.5 - Math.random();
-        });
-    }
+  let flipper_front = makeElement("div", "flip-box__front");
+  let flipper_back = makeElement("div", "flip-box__back");
 
-    var makeElement = function (tagName, className, textInfo) {
-        var element = document.createElement(tagName);
-        element.classList.add(className);
-        if (textInfo) {
-            element.textContent = textInfo;
-        }
-        return element;
-    };
-    var createContent = function (item) {
-        var content = makeElement("div", "flip-box");
+  let image = makeElement("img", "flip-box__img", "", item);
 
-        var flipper = makeElement("div", "flipper");
+  flipper_back.appendChild(image);
 
-        var flipper_front = makeElement("div", "flip-box__front");
-        var flipper_back = makeElement("div", "flip-box__back");
+  flipper.appendChild(flipper_front);
+  flipper.appendChild(flipper_back);
+  content.appendChild(flipper);
 
-        var image = makeElement("img", "flip-box__img");
-        image.src = item;
-        image.alt = '';
-        flipper_back.appendChild(image);
+  return content;
+};
 
-        flipper.appendChild(flipper_front);
-        flipper.appendChild(flipper_back);
-        content.appendChild(flipper);
+class Game {
+  constructor() {
+    this.main = document.getElementById("parent");
+    this.cards = [
+      "img/head001.png",
+      "img/head002.png",
+      "img/head003.png",
+      "img/head004.png",
+      "img/head005.png",
+      "img/head006.png",
+      "img/head001.png",
+      "img/head002.png",
+      "img/head003.png",
+      "img/head004.png",
+      "img/head005.png",
+      "img/head006.png"
+    ];
+  }
 
-        return content;
-    };
-
-    function ShowCards() {
-        var fragment = document.createDocumentFragment();
-
-        cards.forEach(item => {
-            fragment.appendChild(createContent(item));
-        });
-        main.appendChild(fragment);
-    }
-
-    function ToggleCard(element) {
-        let activeCards = [...document.querySelectorAll(".active")]
-
-        if (activeCards.length > 1) activeCards.forEach(item => {
-            item.classList.remove("active");
-        });
-
-        if (element.classList.contains("flip-box")) {
-            element.classList.toggle("active");
-        }
-    }
-
-    function DisableCards() {
-        if (SameCards())[...document.querySelectorAll(".active")].forEach(item => {
-            item.classList.add("hidden");
-        });
-    }
-
-    function SameCards() {
-        let images = [...document.querySelectorAll(".active .flip-box__img")];
-        return images.length === 2 && images[0].src === images[1].src;
-    }
-
-    main.addEventListener("click", function (event) {
-        ToggleCard(event.target);
-        DisableCards();
+  addListeners() {
+    this.main.addEventListener("click", event => {
+      this.toggleCard(event.target);
+      this.disableCards();
     });
+  }
 
-    SortCards();
-    ShowCards();
-})();
+  sortCards() {
+    this.cards.sort(() => {
+      return 0.5 - Math.random();
+    });
+  }
+
+  showCards() {
+    let fragment = document.createDocumentFragment();
+
+    this.cards.forEach(item => {
+      fragment.appendChild(createContent(item));
+    });
+    this.main.appendChild(fragment);
+  }
+
+  toggleCard(element) {
+    let activeCards = [...document.querySelectorAll(".active")];
+
+    if (activeCards.length > 1) {
+      activeCards.forEach(item => {
+        item.classList.remove("active");
+      });
+    }
+
+    if (element.classList.contains("flip-box")) {
+      element.classList.toggle("active");
+    }
+  }
+
+  disableCards() {
+    if (this.sameCards()) {
+      let activeElements = document.querySelectorAll(".active");
+      [...activeElements].forEach(item => {
+        item.classList.add("hidden");
+      });
+    }
+  }
+
+  sameCards() {
+    let activeImageElements = document.querySelectorAll(
+      ".active .flip-box__img"
+    );
+    let images = [...activeImageElements];
+    return images.length === 2 && images[0].src === images[1].src;
+  }
+
+  init() {
+    this.sortCards();
+    this.showCards();
+    this.addListeners();
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const game = new Game();
+  game.init();
+});
