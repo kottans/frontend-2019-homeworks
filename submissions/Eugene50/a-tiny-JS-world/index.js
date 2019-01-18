7094d672943eb3e61b0f1be901e28c4cb9485028
Eugene50 <nova_alf@ukr.net>
2019-01-18 23:56:39 +0200

    add OOP features

--- submissions/Eugene50/a-tiny-JS-world/index.js
+++ submissions/Eugene50/a-tiny-JS-world/index.js
@@ -1,4 +1,4 @@
-class Citizens {
+class Citizen {
     constructor (species, gender, name, say, legs = 0, hands = null ){
      this.species = species;
      this.gender = gender;
@@ -7,14 +7,46 @@ class Citizens {
      this.hands = hands;
      this.say = say;
     }
-};
-
-let dog = new Citizens('dog','male','Sharik','Gaf!!!', 4);
-let cat = new Citizens('cat', 'male', 'Murzik','Mjau)', 4 );
-let man = new Citizens('human','male','Alex','Hello to All!!!', 2, 2);
-let woman = new Citizens('human','female','Maria','I want coffee!!!', 2, 2);
-
-const all = [man, woman, cat, dog];
-all.forEach(element => {
+ };
+ 
+ class Human extends Citizen {
+      constructor(species, gender, name, say, legs, hands ) {
+         super(species, gender, name, say, 2, 2)
+      }
+ }
+ class Animal extends Citizen {
+      constructor(species, gender, name, say, legs ) {
+          super(species, gender, name, say, 4)
+      }
+ }
+ class Dog extends Animal {
+      constructor(gender, name, say){
+         super('dog', gender, name, say)
+      }
+ }
+ class Cat extends Animal {
+      constructor(gender, name, say){
+         super('cat', gender, name, say)
+      }
+ }
+ class Man extends Human {
+      constructor(gender, name, say){
+         super('human', gender, name, say)
+      }
+ }
+ class Woman extends Human {
+      constructor(gender,  name, say){
+         super('human', gender, name, say )
+      }
+ }
+ 
+ const dog = new Dog('male','Sharik','Gaf-gaf!!!');
+ const cat = new Cat('male', 'Murzik','Mjau)');
+ const man = new Man('male','Alex','Hello to All!!!');
+ const woman = new Woman('female','Maria','I want coffee!!!');
+ 
+ const all = [man, woman, cat, dog];
+ all.forEach(element => {
     print([element.species, element.gender, element.name, element.say, element.legs, element.hands].join('; '));
-})
+ })
+ 
