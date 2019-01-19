1f1223116817b872ac4d33b24b08c42459a10b45
Konstantin Makarevich <krash1408@gmail.com>
2019-01-19 14:17:48 +0200

    Remove console.log Add restoreSortedArray function Partially fix a bug with sorting

--- submissions/krash1408/friends-app/src/index.js
+++ submissions/krash1408/friends-app/src/index.js
@@ -5,8 +5,15 @@ fieldWrapper.classList.add('main-field__wrapper');
 fieldWrapper.innerHTML = '';
 let usersCurrentArr = [];
 let usersSortedArr = [];
+const restoreSortedArray = function () {
+  usersSortedArr = [];
+  usersCurrentArr.forEach( val => {
+    usersSortedArr.push(val);
+  })
+  
+}
 
-function getUsersList() {
+const request = function() {
   fetch(url)
     .then(function ifError(response) {
       if (response.ok) {
@@ -18,81 +25,89 @@ function getUsersList() {
     })
     .then(function (data) {
       data.results.forEach(element => {
-        createCard(element);
-        usersCurrentArr.push(element);
-        usersSortedArr.push(element);
+        let card = new createCard(element);
+        usersCurrentArr.push(card);
+        usersSortedArr.push(card);
       })
     })
     .then(function () {
-      createFiled( usersCurrentArr );
+      createFiled( usersSortedArr );
     })
-    .catch(error => console.error(error));
-}
-//добавляет в массив к каждому обьекту ключ element со значением DOM елемента card
-function createCard ( value ) {
-  card = document.createElement('div');
-  card.classList.add('card');
-  cardWrapper = document.createElement('div');
-  cardWrapper.classList.add('card__wrapper');
-
-  cardFront = document.createElement('div');
-  cardFront.classList.add('card__front');
-  cardBack = document.createElement('div');
-  cardBack.classList.add('card__back');
-
-  cardPhoto = document.createElement('img');
-  cardPhoto.classList.add('card__photo');
-  cardPhoto.setAttribute('src', `${value.picture.large}`);
-  cardName = document.createElement('p');
-  cardName.classList.add('card__name');
-  cardName.innerHTML = `Name: ${value.name.first} ${value.name.last}`;
-  cardAge = document.createElement('p');
-  cardAge.classList.add('card__age');
-  cardAge.innerHTML = `Age: ${value.dob.age}`;
-
-  cardBirthDay = document.createElement('p');
-  cardBirthDay.classList.add('card__birthday');
-  cardBirthDay.innerHTML = `Date of birth:<br>${value.dob.date}`;
-  cardEmail = document.createElement('p');
-  cardEmail.classList.add('card__email');
-  cardEmail.innerHTML = `Email:<br>${value.email}`;
-  cardID = document.createElement('p');
-  cardID.classList.add('card__id');
-  cardID.innerHTML = `Accaunt ID:<br>${value.id.value}`;
-  cardPhone = document.createElement('p');
-  cardPhone.classList.add('card__phont');
-  cardPhone.innerHTML = `Phone number:<br>${value.phone}`;
-
-  card.appendChild(cardWrapper);
-
-  cardWrapper.appendChild(cardFront);
-  cardWrapper.appendChild(cardBack);
-
-  cardFront.appendChild(cardPhoto);
-  cardFront.appendChild(cardName);
-  cardFront.appendChild(cardAge);
-
-  cardBack.appendChild(cardBirthDay);
-  cardBack.appendChild(cardEmail);
-  cardBack.appendChild(cardID);
-  cardBack.appendChild(cardPhone);
-
-  value.element = card;
+    .catch(error => error);
 }
 
-function createFiled ( array ) {
+class createCard {
+  constructor(value) {
+    this.value = value;
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.appendChild(this.Wrapper());
+    this.element = card;
+  }
+  Wrapper() {
+    const cardWrapper = document.createElement('div');
+    cardWrapper.classList.add('card__wrapper');
+
+    cardWrapper.appendChild(this.Front());
+    cardWrapper.appendChild(this.Back());
+
+    return cardWrapper;
+  }
+  Front() {
+    const cardFront = document.createElement('div');
+    cardFront.classList.add('card__front');
+    const cardPhoto = document.createElement('img');
+    cardPhoto.classList.add('card__photo');
+    cardPhoto.setAttribute('src', `${this.value.picture.large}`);
+    const cardName = document.createElement('p');
+    cardName.classList.add('card__name');
+    cardName.innerHTML = `Name: ${this.value.name.first} ${this.value.name.last}`;
+    const cardAge = document.createElement('p');
+    cardAge.classList.add('card__age');
+    cardAge.innerHTML = `Age: ${this.value.dob.age}`;
+    cardFront.appendChild(cardPhoto);
+    cardFront.appendChild(cardName);
+    cardFront.appendChild(cardAge);
+
+    return cardFront;
+  }
+  Back() {
+    const cardBack = document.createElement('div');
+    cardBack.classList.add('card__back');
+    const cardBirthDay = document.createElement('p');
+    cardBirthDay.classList.add('card__birthday');
+    cardBirthDay.innerHTML = `Date of birth:<br>${this.value.dob.date}`;
+    const cardEmail = document.createElement('p');
+    cardEmail.classList.add('card__email');
+    cardEmail.innerHTML = `Email:<br>${this.value.email}`;
+    const cardID = document.createElement('p');
+    cardID.classList.add('card__id');
+    cardID.innerHTML = `Accaunt ID:<br>${this.value.id.value}`;
+    const cardPhone = document.createElement('p');
+    cardPhone.classList.add('card__phont');
+    cardPhone.innerHTML = `Phone number:<br>${this.value.phone}`;
+    cardBack.appendChild(cardBirthDay);
+    cardBack.appendChild(cardEmail);
+    cardBack.appendChild(cardID);
+    cardBack.appendChild(cardPhone);
+
+    return cardBack;
+  }
+}
+
+const createFiled = function( array ) {
   array.forEach(value => fieldWrapper.appendChild(value.element))
   field.appendChild(fieldWrapper);
 }
-// сортировщики
-let sortByAge = function( a, b ) {
-    if ( a.dob.age < b.dob.age ) return -1;
-    if ( a.dob.age > b.dob.age) return 1;
+
+const sortByAge = function( a, b ) {
+    if ( a.value.dob.age < b.value.dob.age ) return -1;
+    if ( a.value.dob.age > b.value.dob.age) return 1;
     return 0;
 }
 const sortByName = function ( a, b ) {
-  let nameA = a.name.first.toLowerCase(), 
-      nameB = b.name.first.toLowerCase()
+  let nameA = a.value.name.first.toLowerCase(), 
+      nameB = b.value.name.first.toLowerCase()
   if (nameA < nameB) return -1
   if (nameA > nameB) return 1
   return 0
@@ -100,20 +115,19 @@ const sortByName = function ( a, b ) {
 const sortByGenderMale = function  ( array ) {
   let card = document.querySelectorAll('.card');
   array.forEach((value, i) => {
-    if ( value.gender !== 'male') {card[i].style.display = 'none'}
+    if ( value.value.gender !== 'male') {card[i].style.display = 'none'}
     else {card[i].style.display = ''}
   })
 }
 const sortByGenderFemale = function  ( array ) {
   let card = document.querySelectorAll('.card');
   array.forEach((value, i) => {
-    if ( value.gender !== 'female') {card[i].style.display = 'none'}
+    if ( value.value.gender !== 'female') {card[i].style.display = 'none'}
     else {card[i].style.display = ''}
   })
 }
 
-// поиск по имени
-function search(  ) {
+const search = function() {
   let names = document.getElementsByClassName('card__name');
   const search = document.querySelector('#search');
   const filter = search.value.toUpperCase();
@@ -128,7 +142,7 @@ function search(  ) {
   }
 
 }
-//сортировка по имени и возрасту
+
 document.querySelector('.main-navigation').addEventListener('click', () => {
   const inputNodeList = document.querySelectorAll('.hidden'); 
   const male = document.querySelector('.male');
@@ -141,33 +155,38 @@ document.querySelector('.main-navigation').addEventListener('click', () => {
 
   switch (event.target) {
     case nameAZ :
-      usersCurrentArr.sort(sortByName);
-      createFiled( usersCurrentArr );
+      usersSortedArr.sort(sortByName);
+      createFiled( usersSortedArr );
+      restoreSortedArray();
       break;
     case nameZA :
-    usersCurrentArr.sort(sortByName).reverse();
-      createFiled( usersCurrentArr );
+    usersSortedArr.sort(sortByName).reverse();
+      createFiled( usersSortedArr );
+      restoreSortedArray();
       break;
     case ageZA :
-      usersCurrentArr.sort(sortByAge);
-      createFiled( usersCurrentArr );
+      usersSortedArr.sort(sortByAge);
+      createFiled( usersSortedArr );
+      restoreSortedArray();
       break;
     case ageAZ :
-      usersCurrentArr.sort(sortByAge).reverse();
-      createFiled( usersCurrentArr );
+      usersSortedArr.sort(sortByAge).reverse();
+      createFiled( usersSortedArr );
+      restoreSortedArray();
       break;
     case male :
-      sortByGenderMale( usersCurrentArr );
+      sortByGenderMale( usersSortedArr );
       break;
     case female :
-        sortByGenderFemale( usersCurrentArr );
-        break;
+      sortByGenderFemale( usersSortedArr );
+      break;
     case reset :
       inputNodeList.forEach(val => val.checked = false);
       document.getElementById('search').value = '';
       document.querySelectorAll('.card').forEach( val => val.style.display = "")
       field.removeChild(fieldWrapper);
-      createFiled( usersSortedArr );
+      createFiled( usersCurrentArr );
+      restoreSortedArray();
       break;
   }
 })
@@ -175,12 +194,4 @@ document.querySelector('#search').addEventListener('input', () => {
   search( usersCurrentArr );
 })
 
-document.querySelectorAll('.card__front').forEach( val => {
-  val.addEventListener('click', () => {
-    console.log(target);
-    console.log(click)
-    val.classList.toggle('rotate');
-  })
-}) 
-
-getUsersList();
+request();
