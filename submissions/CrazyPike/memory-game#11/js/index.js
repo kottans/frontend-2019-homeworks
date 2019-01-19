9db34b45af5906b6e213e36d3521aabb1a90ea0b
Serhiy Danylenko <serhiy.danylenko@gmail.com>
2019-01-19 14:31:46 +0200

    Refactored code without flipping

--- submissions/CrazyPike/memory-game#11/js/index.js
+++ submissions/CrazyPike/memory-game#11/js/index.js
@@ -1,40 +1,56 @@
-const input = [1,5,4,8,3,2,6,4,7,3,1,6,8,5,2,7]
-var firstCard = {
+const cardItemsArray = [];
+const SHOW_SECOND_CARD_TIMEOUT = 300;
+const CLASS_STYLE_DISPLAYED = 'displayed';
+const CLASS_STYLE_CARD = 'card';
+const CLASS_STYLE_NOBORDER = 'noborder';
+const MEMORY_BOARD_SIZE = 16;
+
+let firstCard = {
     id : 'none',
     card: 'none'
 }
 
-let images = {
-    1: 'veg1',
-    2: 'veg2',
-    3: 'veg3',
-    4: 'veg4',
-    5: 'veg5',
-    6: 'veg6',
-    7: 'veg7',
-    8: 'veg8',
-}
+let images = ['veg1', 'veg2', 'veg3', 'veg4', 'veg5', 'veg6', 'veg7', 'veg8']
 
+fillArray(cardItemsArray)
+shuffle(cardItemsArray);
 const container = document.getElementById('container');
-container.innerHTML = input.map((item,index) => `<div class="card" id="card${index}" card="${item} "><img src="./img/${images[item]}.png"></div>`).join(' ');
-container.addEventListener('click', (event) =>{
-    var target = event.target;
-    if (target.className !== 'card') return;
+container.innerHTML = cardItemsArray.map((item, index) => `<div class="${CLASS_STYLE_CARD}" id="card${index}" card="${item} "><img src="./img/${images[item]}.png"></div>`).join(' ');
+container.addEventListener('click', ({target}) =>{
+    if (!target.classList.contains(CLASS_STYLE_CARD) || target.classList.contains(CLASS_STYLE_DISPLAYED)) return;
     let targetCard = target.getAttribute('card');
-    target.className = 'card displayed';
+    target.classList.toggle(CLASS_STYLE_DISPLAYED);
     if (firstCard.id === 'none') {
         firstCard.id = target.id;
         firstCard.card = targetCard;
     }
     else if (firstCard.card === targetCard) {
-        document.getElementById(firstCard.id).className = 'card displayed noborder';
-        target.className = 'card displayed noborder ';
+        document.getElementById(firstCard.id).classList.toggle(CLASS_STYLE_NOBORDER);
+        target.classList.toggle(CLASS_STYLE_NOBORDER);
         firstCard.id = 'none';
 
     }   else {
         let timerid = setTimeout(function() {
-            target.className = 'card';}, 300)
-        document.getElementById(firstCard.id).className = 'card';
+            target.classList.toggle(CLASS_STYLE_DISPLAYED);}, SHOW_SECOND_CARD_TIMEOUT);
+        document.getElementById(firstCard.id).classList.toggle(CLASS_STYLE_DISPLAYED);
         firstCard.id = 'none';
     }
 } )
+
+
+function fillArray(array){
+    let arrayItem = 0;
+    for (let i = 0; i < MEMORY_BOARD_SIZE; i+=2 ){
+        array.push(arrayItem);
+        array.push(arrayItem);
+        arrayItem = (arrayItem === MEMORY_BOARD_SIZE/2) ? 0 : ++arrayItem;
+    }
+}
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
