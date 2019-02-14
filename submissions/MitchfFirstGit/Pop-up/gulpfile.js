cca5eb346c39c843c089dc34ac82eed55d90318c
Mitch <artfixmitch@gmail.com>
2019-02-14 20:44:00 +0300

    Pop-up complite

---
+++ submissions/MitchfFirstGit/Pop-up/gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const nano = require('gulp-cssnano');
+const watch = require('gulp-watch');
+gulp.task('sass-compile', function() {
+    return gulp.src('./scss/main.scss')
+        .pipe(sass())
+        .pipe(nano())
+        .pipe(gulp.dest('./css/'))
+})
+
+gulp.task('watch', function() {
+    gulp.watch('./scss/main.scss', gulp.series('sass-compile'))
+})
