3fe6aeba126345b8cb2e59ddf3ad15c44c809c11
Mitch <artfixmitch@gmail.com>
2019-02-15 22:50:39 +0300

    Add new changes

--- submissions/MitchfFirstGit/Pop-up/gulpfile.js
+++ submissions/MitchfFirstGit/Pop-up/gulpfile.js
@@ -1,11 +1,11 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-// const nano = require('gulp-cssnano');
 const watch = require('gulp-watch');
+const nano = require('gulp-cssnano');
 gulp.task('sass-compile', function() {
     return gulp.src('./scss/main.scss')
         .pipe(sass())
-        // .pipe(nano())
+        .pipe(nano())
         .pipe(gulp.dest('./css/'))
 })
 
