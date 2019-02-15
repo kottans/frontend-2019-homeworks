309b898255763f196d3e3a0f55d5d1ab434c7189
Mitch <artfixmitch@gmail.com>
2019-02-15 15:44:27 +0300

    Fix all issues

--- submissions/MitchfFirstGit/Pop-up/gulpfile.js
+++ submissions/MitchfFirstGit/Pop-up/gulpfile.js
@@ -1,11 +1,11 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const nano = require('gulp-cssnano');
+// const nano = require('gulp-cssnano');
 const watch = require('gulp-watch');
 gulp.task('sass-compile', function() {
     return gulp.src('./scss/main.scss')
         .pipe(sass())
-        .pipe(nano())
+        // .pipe(nano())
         .pipe(gulp.dest('./css/'))
 })
 
